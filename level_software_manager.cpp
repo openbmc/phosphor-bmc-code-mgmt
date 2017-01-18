0eafe7a5a112a5ad716e6a105227d4b2fcc15268
Gunnar Mills <gmills@us.ibm.com>
2017-01-18 09:48:13 -0600

    Initial phosphor-bmc-code-mgmt commit
    This is the initial makefile and building of a basic application
    that provides dbus introspection and properties of
    the xyz/openbmc_project/Software/Level.interface.yaml
    
    Change-Id: Ied9beee1ea4f12805622c17c99c01b89c3f74d50
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ level_software_manager.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "level_software_manager.hpp"
+
+namespace phosphor
+{
+namespace software
+{
+namespace manager
+{
+
+} // namespace manager
+} // namespace software
+} // namepsace phosphor

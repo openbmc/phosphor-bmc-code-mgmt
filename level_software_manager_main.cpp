5b298fc34b4610a85f9bea5a5563bc9f4f8d14b0
Gunnar Mills <gmills@us.ibm.com>
2017-01-18 09:48:13 -0600

    Add base support for BMC code management
    This is the starting code needed to build the application.
    
    Change-Id: Ied9beee1ea4f12805622c17c99c01b89c3f74d50
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ level_software_manager_main.cpp
@@ -0,0 +1,54 @@
+#include <fstream>
+#include <cstdlib>
+#include <iostream>
+#include <exception>
+#include <sdbusplus/bus.hpp>
+#include "config.h"
+#include "level_software_manager.hpp"
+
+int main(int argc, char *argv[])
+{
+    auto bus = sdbusplus::bus::new_default();
+
+    // Use hash of /etc/os-release to determine id.
+    std::ifstream efile;
+    std::string line;
+    std::string version;
+    efile.open("/etc/os-release");
+
+    while(!efile.eof()) {
+        getline(efile, line);
+        if (line.find("VERSION=") != std::string::npos) {
+            version = line.substr(9,11);
+            break;
+        }
+    }
+    efile.close();
+
+    if(version.empty())
+    {
+        return -1;
+    }
+
+    // Only want 8 digits.
+    auto id = std::hash<std::string>{}(version) % 100000000;
+
+    auto objPathInst = std::string{LEVEL_OBJPATH} + '/' +
+        std::to_string(id);
+
+    // Add sdbusplus ObjectManager.
+    sdbusplus::server::manager::manager objManager(bus,
+                                                   objPathInst.c_str());
+
+    phosphor::software::manager::Level manager(bus,
+                                               objPathInst.c_str());
+
+    bus.request_name(LEVEL_BUSNAME);
+
+    while(true)
+    {
+        bus.process_discard();
+        bus.wait();
+    }
+    return 0;
+}

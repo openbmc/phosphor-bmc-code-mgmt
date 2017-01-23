9e0e94e7da4859a65033e253a7fa97cccf604bda
Gunnar Mills <gmills@us.ibm.com>
2017-01-23 16:45:03 -0600

    Determine Level properties on application startup
    Change-Id: I38c323aa9dd0ec37c5ae7a1f60f61da207ce7c1c
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- version_software_manager.hpp
+++ version_software_manager.hpp
@@ -21,19 +21,71 @@ class Version : public sdbusplus::server::object::object <
     public:
         /** @brief Constructs Version Software Manager
          *
-         * @param[in] bus       - The Dbus bus object
-         * @param[in] objPath   - The Dbus object path
+         * @param[in] version - The version identifier
+         * @param[in] purpose - The purpose of the version
+         */
+        struct Properties
+        {
+            std::string version;
+            VersionPurpose purpose;
+        };
+
+        /** @brief Constructs Version Software Manager
+         *
+         * @note This constructor passes 'true' to the base class in order to
+         *       defer dbus object registration until we can run
+         *       determineInitialProperties() and set our properties
+         *
+         * @param[in] bus        - The Dbus bus object
+         * @param[in] objPath    - The Dbus object path
+         * @param[in] properties - Desired Version properties.
          */
         Version(sdbusplus::bus::bus& bus,
-                const char* objPath) :
+                const char* objPath,
+                const Properties& properties) :
             sdbusplus::server::object::object <
             sdbusplus::xyz::openbmc_project::Software::server::Version >
-            (bus, objPath) {};
+            (bus, objPath, true)
+        {
+            purpose(properties.purpose);
+            version(properties.version);
+
+            // Will throw exception on fail
+            setInitialProperties(properties.version, properties.purpose);
+
+            // Emit deferred signal.
+            emit_object_added();
+        }
 
         /**
-         * @brief Get the code version
+         * @brief Set initial properties
          *
-         * @return The version
+         * @param[in] version - The version identifier
+         * @param[in] purpose - The purpose of the version
+         *
+         * @return Will throw exceptions on failure
+         **/
+        void setInitialProperties(const std::string& version,
+                                  VersionPurpose purpose);
+
+        /**
+         * @brief Set value of purpose
+         *
+         * @param[in] value - The purpose of the version
+         **/
+        VersionPurpose purpose(VersionPurpose value) override;
+
+        /**
+         * @brief Set value of version
+         *
+         * @param[in] value - The version identifier
+         **/
+        std::string version(const std::string value) override;
+
+        /**
+         * @brief Get the code version identifier
+         *
+         * @return The version identifier
          **/
         static const std::string getVersion();
 };

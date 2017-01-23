aca4d8ad70c2364c86e0a21ca5a8f3f6d8a1c3cc
Gunnar Mills <gmills@us.ibm.com>
2017-01-23 16:45:03 -0600

    Determine Level properties on application startup
    Change-Id: I38c323aa9dd0ec37c5ae7a1f60f61da207ce7c1c
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- level_software_manager_main.cpp
+++ level_software_manager_main.cpp
@@ -28,15 +28,22 @@ int main(int argc, char *argv[])
     // Only want 8 digits.
     auto id = std::hash<std::string>{}(version) % 100000000;
 
-    auto objPathInst = std::string{LEVEL_OBJPATH} + '/' +
-        std::to_string(id);
+    // For now, we only have one instance of the level
+    auto objPathInst = std::string(LEVEL_OBJPATH) + '/' + std::to_string(id);
 
     // Add sdbusplus ObjectManager.
     sdbusplus::server::manager::manager objManager(bus,
                                                    objPathInst.c_str());
 
+    phosphor::software::manager::Level::Properties properties;
+    properties.version = version;
+
+    // For now, we only support the BMC code level
+    properties.purpose = phosphor::software::manager::Level::LevelPurpose::BMC;
+
     phosphor::software::manager::Level manager(bus,
-                                               objPathInst.c_str());
+                                               objPathInst.c_str(),
+                                               properties);
 
     bus.request_name(LEVEL_BUSNAME);
 

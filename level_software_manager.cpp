eef3795b60fa4c114f9d7a3725f9ecaf4e40883a
Gunnar Mills <gmills@us.ibm.com>
2017-01-23 16:45:03 -0600

    Determine Level properties on application startup
    Change-Id: I38c323aa9dd0ec37c5ae7a1f60f61da207ce7c1c
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- level_software_manager.cpp
+++ level_software_manager.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <log.hpp>
 #include "level_software_manager.hpp"
 
 namespace phosphor
@@ -9,6 +10,40 @@ namespace software
 {
 namespace manager
 {
+
+using namespace phosphor::logging;
+
+// When you see server:: you know we're referencing our base class
+namespace server = sdbusplus::xyz::openbmc_project::Software::server;
+
+void Level::setInitialProperties(const std::string& version,
+    const LevelPurpose& purpose)
+{
+    server::Level::purpose(purpose);
+
+    server::Level::version(version);
+
+    return;
+}
+
+std::string Level::version(const std::string value)
+{
+    log<level::INFO>("Change to Software Version",
+                     entry("VERSION=%s",
+                           value));
+
+    return server::Level::version(value);
+}
+
+
+Level::LevelPurpose Level::purpose(const LevelPurpose value)
+{
+    log<level::INFO>("Change to Software Purpose",
+                     entry("PURPOSE=%s",
+                           convertForMessage(value).c_str()));
+
+    return server::Level::purpose(value);
+}
 
 std::string Level::getVersion()
 {
@@ -27,7 +62,6 @@ std::string Level::getVersion()
     }
     efile.close();
     return version;
-
 }
 
 } // namespace manager

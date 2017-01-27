18f4ec8a8950dea1797b1bc17c639de17fee1ba7
Gunnar Mills <gmills@us.ibm.com>
2017-01-27 10:41:49 -0600

    Add Software Activation Interface
    Change-Id: I922d6cfe31b6a20bd228167c5e12def69d5ef113
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ activation_software_manager.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include "xyz/openbmc_project/Software/Activation/server.hpp"
+
+namespace phosphor
+{
+namespace software
+{
+namespace manager
+{
+
+/** @class Activation
+ *  @brief OpenBMC activation software management implementation.
+ *  @details A concrete implementation for
+ *           xyz.openbmc_project.Software.Activation
+ *  DBus API.
+ */
+class Activation : public sdbusplus::server::object::object<
+                sdbusplus::xyz::openbmc_project::Software::server::Activation>
+{
+    public:
+        /** @brief Constructs Activation Software Manager
+         *
+         * @param[in] bus        - The Dbus bus object
+         * @param[in] objPath    - The Dbus object path
+         */
+        Activation(sdbusplus::bus::bus& bus,
+                const char* objPath) :
+                sdbusplus::server::object::object<
+                    sdbusplus::xyz::openbmc_project::Software::server::
+                        Activation>(bus, objPath, true)
+        {
+            activation(phosphor::software::manager::Activation::
+                    Activations::Active);
+            // requestedActivation default is "None", so no need to set.
+
+            // Emit deferred signal.
+            emit_object_added();
+        }
+};
+
+} // namespace manager
+} // namespace software
+} // namespace phosphor

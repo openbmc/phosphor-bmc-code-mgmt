e266ef0375a017daf71a4f380768b5e084b45171
Gunnar Mills <gmills@us.ibm.com>
2017-01-27 11:26:51 -0600

    Set Activation properties on application startup
    Change-Id: I8a7f2be4dc3321b70d55a4c8b624a36800ff48b2
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- activation_software_manager.cpp
+++ activation_software_manager.cpp
@@ -1,3 +1,5 @@
+#include "activation_software_manager.hpp"
+#include <log.hpp>
 
 namespace phosphor
 {
@@ -5,6 +7,30 @@ namespace software
 {
 namespace manager
 {
+
+using namespace phosphor::logging;
+
+// When you see server:: you know we're referencing our base class
+namespace server = sdbusplus::xyz::openbmc_project::Software::server;
+
+Activation::Activations Activation::activation(Activations value)
+{
+    log<level::INFO>("Change to Software Activation",
+                     entry("ACTIVATION=%s",
+                           convertForMessage(value).c_str()));
+
+    return server::Activation::activation(value);
+}
+
+Activation::RequestedActivations Activation::requestedActivation(
+    RequestedActivations value)
+{
+    log<level::INFO>("Change to Software Requested Activation",
+                     entry("REQUESTEDACTIVATION=%s",
+                           convertForMessage(value).c_str()));
+
+    return server::Activation::requestedActivation(value);
+}
 
 } // namespace manager
 } // namespace state
--- activation_software_manager.hpp
+++ activation_software_manager.hpp
@@ -20,16 +20,57 @@ class Activation : public sdbusplus::server::object::object <
     sdbusplus::xyz::openbmc_project::Software::server::Activation >
 {
     public:
+        /** @brief Activation properties
+         *
+         * @param[in] activation - The current Activation state of the
+         *                         Software.Version.
+         */
+        struct Properties
+        {
+            Activations activation;
+            RequestedActivations requestedActivation;
+        };
+
         /** @brief Constructs Activation Software Manager
          *
-         * @param[in] bus       - The Dbus bus object
-         * @param[in] objPath   - The Dbus object path
+         * @note This constructor passes 'true' to the base class in order to
+         *       defer dbus object registration until we can run
+         *       setInitialProperties() and set our properties
+         *
+         * @param[in] bus        - The Dbus bus object
+         * @param[in] objPath    - The Dbus object path
+         * @param[in] properties - Desired Activation properties.
          */
         Activation(sdbusplus::bus::bus& bus,
-                   const char* objPath) :
+                   const char* objPath,
+                   const Properties& properties) :
             sdbusplus::server::object::object <
             sdbusplus::xyz::openbmc_project::Software::server::Activation > (
-                bus, objPath) {};
+                bus, objPath, true)
+        {
+            activation(properties.activation);
+            requestedActivation(properties.requestedActivation);
+
+            // Emit deferred signal.
+            emit_object_added();
+        }
+
+        /**
+         * @brief Set value of activation
+         *
+         * @param[in] value - The current Activation state of the
+         *                    Software.Version.
+         **/
+        Activations activation(Activations value) override;
+
+        /**
+         * @brief Set value of requestedActivation
+         *
+         * @param[in] value - The desired Activation state of the
+         *                    Software.Version.
+         **/
+        RequestedActivations requestedActivation(
+            RequestedActivations value) override;
 };
 
 } // namespace manager

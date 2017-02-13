7bc260429324e336e5f20304ac7e3929c7e8502e
Gunnar Mills <gmills@us.ibm.com>
2017-02-13 16:50:16 -0600

    Optimize namespace with using VersionInherit alias
    Change-Id: Ie82359b1cc2eb042672743a1336da044e70ac1d9
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- version_software_manager.hpp
+++ version_software_manager.hpp
@@ -11,14 +11,16 @@ namespace software
 namespace manager
 {
 
+using VersionInherit = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Software::server::Version,
+    sdbusplus::xyz::openbmc_project::Software::server::Activation>;
+
 /** @class Version
  *  @brief OpenBMC version software management implementation.
  *  @details A concrete implementation for xyz.openbmc_project.Software.Version
  *  DBus API.
  */
-class Version : public sdbusplus::server::object::object<
-                sdbusplus::xyz::openbmc_project::Software::server::Version,
-                sdbusplus::xyz::openbmc_project::Software::server::Activation>
+class Version : public VersionInherit
 {
     public:
         /** @brief Constructs Version Software Manager
@@ -31,13 +33,8 @@ class Version : public sdbusplus::server::object::object<
          * @param[in] objPath    - The Dbus object path
          */
         Version(sdbusplus::bus::bus& bus,
-                const char* objPath) :
-                sdbusplus::server::object::object<
-                    sdbusplus::xyz::openbmc_project::Software::server::Version,
-                    sdbusplus::xyz::openbmc_project::Software::
-                        server::Activation>
-                            (bus, (std::string{objPath} + '/' +
-                                getId()).c_str(), true)
+                const char* objPath) : VersionInherit(
+                    bus, (std::string{objPath} + '/' + getId()).c_str(), true)
         {
             // Set properties.
             purpose(VersionPurpose::BMC);

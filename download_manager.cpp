701e021ce90be9b14489b3395d53598c7a7965aa
Gunnar Mills <gmills@us.ibm.com>
2017-04-03 11:21:27 -0500

    Initial Download manager
    This is the basic download manager that implements
    xyz/openbmc_project/Common/TFTP.interface.yaml
    
    Change-Id: I429b0f2fbce3864dee5051af727b88b3a3fe1227
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ download_manager.cpp
@@ -0,0 +1,19 @@
+#include "download_manager.hpp"
+
+namespace phosphor
+{
+namespace software
+{
+namespace manager
+{
+
+void Download::downloadViaTFTP(const  std::string fileName,
+                               const  std::string serverAddress)
+{
+    return;
+}
+
+} // namespace manager
+} // namespace software
+} // namespace phosphor
+

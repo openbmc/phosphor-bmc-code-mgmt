362e2a5872c4d88d16a58b79b3664b00b86c6452
Gunnar Mills <gmills@us.ibm.com>
2017-04-03 11:21:27 -0500

    Initial Download manager
    This is the basic download manager that implements
    xyz/openbmc_project/Common/TFTP.interface.yaml
    
    Change-Id: I429b0f2fbce3864dee5051af727b88b3a3fe1227
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ download_manager.cpp
@@ -0,0 +1,13 @@
+#include "download_manager.hpp"
+
+namespace phosphor
+{
+namespace software
+{
+namespace manager
+{
+
+} // namespace manager
+} // namespace software
+} // namepsace phosphor
+
---
+++ download_manager.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include "xyz/openbmc_project/Common/TFTP/server.hpp"
+
+namespace phosphor
+{
+namespace software
+{
+namespace manager
+{
+
+using DownloadInherit = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Common::server::TFTP>;
+
+/** @class Download
+ *  @brief OpenBMC download software management implementation.
+ *  @details A concrete implementation for xyz.openbmc_project.Common.TFTP
+ *  DBus API.
+ */
+class Download : public DownloadInherit
+{
+    public:
+        /** @brief Constructs Download Software Manager
+         *
+         * @param[in] bus       - The Dbus bus object
+         * @param[in] objPath   - The Dbus object path
+         */
+        Download(sdbusplus::bus::bus& bus,
+                const std::string& objPath) : DownloadInherit(
+                    bus, (objPath).c_str()) {};
+
+
+};
+
+} // namespace manager
+} // namespace software
+} // namespace phosphor
+

005d5bec6ad91e53231cfe97b303694d0505b1fc
Gunnar Mills <gmills@us.ibm.com>
2017-04-03 11:21:27 -0500

    Initial Download manager
    This is the basic download manager that implements
    xyz/openbmc_project/Common/TFTP.interface.yaml
    
    Change-Id: I429b0f2fbce3864dee5051af727b88b3a3fe1227
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ download_manager_main.cpp
@@ -0,0 +1,24 @@
+#include <sdbusplus/bus.hpp>
+#include "config.h"
+#include "download_manager.hpp"
+
+int main(int argc, char* argv[])
+{
+    auto bus = sdbusplus::bus::new_default();
+
+    // Add sdbusplus ObjectManager.
+    sdbusplus::server::manager::manager objManager(bus,
+            SOFTWARE_OBJPATH);
+
+    phosphor::software::manager::Download manager(bus,
+            SOFTWARE_OBJPATH);
+
+    bus.request_name(DOWNLOAD_BUSNAME);
+
+    while (true)
+    {
+        bus.process_discard();
+        bus.wait();
+    }
+    return 0;
+}

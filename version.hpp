ef4781feeaba9037289ad8c6a0bcc574cb1be4fe
Gunnar Mills <gmills@us.ibm.com>
2017-04-19 11:28:24 -0500

    Add Common.FilePath interface
    Change-Id: I4de5b1a4d16e427de84b88365c0136ddd2b42deb
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- version.hpp
+++ version.hpp
@@ -2,6 +2,7 @@
 
 #include <sdbusplus/bus.hpp>
 #include "xyz/openbmc_project/Software/Version/server.hpp"
+#include "xyz/openbmc_project/Common/FilePath/server.hpp"
 
 namespace phosphor
 {
@@ -11,7 +12,8 @@ namespace manager
 {
 
 using VersionInherit = sdbusplus::server::object::object<
-    sdbusplus::xyz::openbmc_project::Software::server::Version>;
+    sdbusplus::xyz::openbmc_project::Software::server::Version,
+    sdbusplus::xyz::openbmc_project::Common::server::FilePath>;
 
 /** @class Version
  *  @brief OpenBMC version software management implementation.
@@ -27,16 +29,19 @@ class Version : public VersionInherit
          * @param[in] objPath        - The Dbus object path
          * @param[in] versionId      - The version identifier
          * @param[in] versionPurpose - The version purpose
+         * @param[in] filePath       - The image filesystem path
          */
         Version(sdbusplus::bus::bus& bus,
                 const std::string& objPath,
                 const std::string& versionId,
-                VersionPurpose versionPurpose) : VersionInherit(
+                VersionPurpose versionPurpose,
+                const std::string& filePath) : VersionInherit(
                     bus, (objPath).c_str(), true)
         {
             // Set properties.
             purpose(versionPurpose);
             version(versionId);
+            path(filePath);
 
             // Emit deferred signal.
             emit_object_added();

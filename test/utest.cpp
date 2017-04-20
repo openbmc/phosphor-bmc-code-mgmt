e3e9f4f9775fd5319f093f02f6bdfe9215902733
Gunnar Mills <gmills@us.ibm.com>
2017-04-20 10:52:15 -0500

    Move version gtests to phosphor-bmc-code-mgmt
    Moving the version gtests from openpower-pnor-code-mgmt to
    phosphor-bmc-code-mgmt. This code has been reviewed before,
    the only changes are renaming to "namespace phosphor" and
    changes related to renaming the files.
    
    Change-Id: Id799fcfff8effbb4c63f1f4e7169aa425567dd41
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ test/utest.cpp
@@ -0,0 +1,64 @@
+#include "version.hpp"
+#include <gtest/gtest.h>
+#include <experimental/filesystem>
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace phosphor::software::manager;
+namespace fs = std::experimental::filesystem;
+
+
+class VersionTest : public testing::Test
+{
+    protected:
+
+        virtual void SetUp()
+        {
+            char versionDir[] = "./versionXXXXXX";
+            _directory = mkdtemp(versionDir);
+
+            if (_directory.empty())
+            {
+                throw std::bad_alloc();
+            }
+        }
+
+        virtual void TearDown()
+        {
+            fs::remove_all(_directory);
+        }
+
+        std::string _directory;
+};
+
+/** @brief Make sure we correctly get the version from getVersion()*/
+TEST_F(VersionTest, TestGetVersion)
+{
+    auto manifestFilePath = _directory + "/" + "MANIFEST";
+    auto version = "test-version";
+
+    std::ofstream file;
+    file.open(manifestFilePath, std::ofstream::out);
+    ASSERT_TRUE(file.is_open());
+
+    file << "version=" << version << std::endl;
+    file.close();
+
+    EXPECT_EQ(Version::getVersion(manifestFilePath), version);
+}
+
+/** @brief Make sure we correctly get the Id from getId()*/
+TEST_F(VersionTest, TestGetId)
+{
+    std::stringstream hexId;
+    auto version = "test-id";
+
+    hexId << std::hex << ((std::hash<std::string> {}(
+                               version)) & 0xFFFFFFFF);
+
+    EXPECT_EQ(Version::getId(version), hexId.str());
+
+}

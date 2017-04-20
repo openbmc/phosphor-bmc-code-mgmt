6da77a6ee0456636d1fde41913fa6fe9ed888491
Gunnar Mills <gmills@us.ibm.com>
2017-04-20 16:07:20 -0500

    Create image dir if not there
    Change-Id: If1c1a62292027ae98d1ba1c6d62bd72d0ae8a307
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- download_manager.cpp
+++ download_manager.cpp
@@ -2,7 +2,9 @@
 #include <string>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <phosphor-logging/log.hpp>
+#include <experimental/filesystem>
 #include "config.h"
 #include <phosphor-logging/elog.hpp>
 #include <phosphor-logging/elog-errors.hpp>
@@ -18,6 +20,7 @@ namespace manager
 
 using namespace sdbusplus::xyz::openbmc_project::Common::Error;
 using namespace phosphor::logging;
+namespace fs = std::experimental::filesystem;
 
 void Download::downloadViaTFTP(const  std::string fileName,
                                const  std::string serverAddress)
@@ -25,26 +28,33 @@ void Download::downloadViaTFTP(const  std::string fileName,
     if (fileName.empty())
     {
         log<level::ERR>("Error FileName is empty");
-        elog<InvalidArgument>(xyz::openbmc_project::Common::InvalidArgument::
-                              ARGUMENT_NAME("FileName"),
-                              xyz::openbmc_project::
-                              Common::InvalidArgument::ARGUMENT_VALUE(""));
+        // elog<InvalidArgument>(xyz::openbmc_project::Common::InvalidArgument::
+        //      ARGUMENT_NAME("FileName"),
+        //      xyz::openbmc_project::
+        //     Common::InvalidArgument::ARGUMENT_VALUE(""));
         return;
     }
 
     if (serverAddress.empty())
     {
         log<level::ERR>("Error ServerAddress is empty");
-        elog<InvalidArgument>(xyz::openbmc_project::Common::InvalidArgument::
-                              ARGUMENT_NAME("ServerAddress"),
-                              xyz::openbmc_project::
-                              Common::InvalidArgument::ARGUMENT_VALUE(""));
+        // elog<InvalidArgument>(xyz::openbmc_project::Common::InvalidArgument::
+        //   ARGUMENT_NAME("ServerAddress"),
+        //   xyz::openbmc_project::
+        //   Common::InvalidArgument::ARGUMENT_VALUE(""));
         return;
     }
 
     log<level::INFO>("Downloading via TFTP",
                      entry("FILENAME=%s", fileName),
                      entry("SERVERADDRESS=%s", serverAddress));
+
+    // Check if IMAGE DIR present. If not try to create.
+    fs::path imgDirPath(IMG_UPLOAD_DIR);
+    if (!fs::is_directory(imgDirPath))
+    {
+        fs::create_directory(imgDirPath);
+    }
 
     pid_t pid = fork();
 
@@ -56,12 +66,12 @@ void Download::downloadViaTFTP(const  std::string fileName,
               (char*)0);
         // execl only returns on fail
         log<level::ERR>("Error occurred during the TFTP call");
-        elog<InternalFailure>();
+        //elog<InternalFailure>();
     }
     else if (pid < 0)
     {
         log<level::ERR>("Error occurred during fork");
-        elog<InternalFailure>();
+        // elog<InternalFailure>();
     }
 
     return;

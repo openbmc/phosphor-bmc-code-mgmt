36403a014707ec2df2c0f8bad0786da8086785b0
Gunnar Mills <gmills@us.ibm.com>
2017-04-27 11:24:19 -0500

    Untar tarball
    Untar the tarball to the image dir. The image dir is
    <IMG_UPLOAD_DIR>/<id>/. The MANIFEST is first extracted,
    the version is read from the MANIFEST and used to compute the id.
    
    Change-Id: Ifed01e980202404e104ce7c20beb85ab23527913
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- image_manager.cpp
+++ image_manager.cpp
@@ -1,4 +1,14 @@
 #include <string>
+#include <experimental/filesystem>
+#include <stdlib.h>
+#include <cstring>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <phosphor-logging/log.hpp>
+#include "config.h"
+#include "version.hpp"
 #include "image_manager.hpp"
 
 namespace phosphor
@@ -8,11 +18,141 @@ namespace software
 namespace manager
 {
 
+using namespace phosphor::logging;
+namespace fs = std::experimental::filesystem;
+
 int processImage(const std::string& tarFilePath)
 {
+    // Need tmp dir to write MANIFEST file to.
+    fs::path tmpDirPath(std::string{IMG_UPLOAD_DIR} + "/imageXXXXXX");
+    fs::path manifestPath;
+    fs::path imageDirPath;
+
+    int status;
+    int rc;
+    pid_t pid;
+    std::string version;
+    std::string id;
+
+    auto tmpDir = mkdtemp(const_cast<char*>(tmpDirPath.c_str()));
+    if (tmpDir == NULL)
+    {
+        goto removeDir;
+    }
+    manifestPath /= std::string{tmpDir} + "/" + MANIFEST_FILE_NAME;
+    pid = fork();
+
+    // Get the MANIFEST FILE
+    if (pid == 0)
+    {
+        // child process
+        execl("/bin/tar", "tar", "-xf", tarFilePath.c_str(), MANIFEST_FILE_NAME,
+              "-C", tmpDir, (char*)0);
+        // execl only returns on fail
+        log<level::ERR>("Failed to untar file",
+                        entry("FILENAME=%s", tarFilePath));
+        goto removeDir;
+    }
+    else if (pid > 0)
+    {
+        waitpid(pid, &status, 0);
+    }
+    else
+    {
+        log<level::ERR>("fork() failed.");
+        goto removeDir;
+    }
+
+    // Verify the manifest file
+    if (!fs::is_regular_file(manifestPath))
+    {
+        log<level::ERR>("Error No manifest file");
+        goto removeDir;
+    }
+
+    // Get version
+    version = Version::getValue(manifestPath.string(), "version");
+    if (version.empty())
+    {
+        log<level::ERR>("Error unable to read version from manifest file");
+        goto removeDir;
+    }
+
+    // Compute id
+    id = Version::getId(version);
+
+    fs::remove_all(tmpDir);
+    imageDirPath /= std::string{IMG_UPLOAD_DIR} + '/' + id;
+
+    if (mkdir(imageDirPath.c_str(), S_IRWXU) != 0)
+    {
+        log<level::ERR>("Error occured during mkdir");
+        goto removeTar;
+    }
+
+    // Untar tarball
+    rc = unTar(tarFilePath, imageDirPath.string());
+    if (rc < 0)
+    {
+        log<level::ERR>("Error occured during untar");
+        goto removeTar;
+    }
+    // Remove tarball
+    fs::remove_all(tarFilePath);
+
     return 0;
+
+removeDir:
+
+    fs::remove_all(tmpDir);
+
+removeTar:
+
+    fs::remove_all(tarFilePath);
+    return -1;
 }
 
+int unTar(const std::string& tarFilePath, const std::string& extractDirPath)
+{
+    if (tarFilePath.empty())
+    {
+        log<level::ERR>("Error TarFilePath is empty");
+        return -1;
+    }
+    if (extractDirPath.empty())
+    {
+        log<level::ERR>("Error ExtractDirPath is empty");
+        return -1;
+    }
+
+    log<level::INFO>("Untaring",
+                     entry("FILENAME=%s", tarFilePath),
+                     entry("EXTRACTIONDIR=%s", extractDirPath));
+    int status;
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        // child process
+        execl("/bin/tar", "tar", "-xf", tarFilePath.c_str(),
+              "-C", extractDirPath.c_str(), (char*)0);
+        // execl only returns on fail
+        log<level::ERR>("Failed to untar file",
+                        entry("FILENAME=%s", tarFilePath));
+        return -1;
+    }
+    else if (pid > 0)
+    {
+        waitpid(pid, &status, 0);
+    }
+    else
+    {
+        log<level::ERR>("fork() failed.");
+        return -1;
+    }
+
+    return 0;
+}
 } // namespace manager
 } // namespace software
 } // namepsace phosphor

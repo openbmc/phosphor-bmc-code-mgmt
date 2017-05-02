62097ce3a1b3e506c48dcf0d55ba20c1be542645
Gunnar Mills <gmills@us.ibm.com>
2017-05-02 12:20:36 -0500

    Create initial BMC Item Updater
    This is the initial BMC Item Updater.
    
    Change-Id: I1c4f8ec94d7d35a0e03a363007b79414b20058ac
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ item_updater.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include "item_updater.hpp"
+#include "config.h"
+
+namespace phosphor
+{
+namespace software
+{
+namespace updater
+{
+
+int ItemUpdater::createActivation(sd_bus_message* msg,
+                                  void* userData,
+                                  sd_bus_error* retErr)
+{
+    auto versionId = 1;
+    auto* updater = static_cast<ItemUpdater*>(userData);
+    updater->activations.insert(std::make_pair(
+            std::to_string(versionId),
+            std::make_unique<Activation>(
+                    updater->bus,
+                    SOFTWARE_OBJPATH)));
+    return 0;
+}
+
+} // namespace updater
+} // namespace software
+} // namespace phosphor

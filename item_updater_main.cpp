d89dde143f066e0aee0e717fffb3d6a49e9e4607
Gunnar Mills <gmills@us.ibm.com>
2017-05-02 12:20:36 -0500

    Create initial BMC Item Updater
    This is the initial BMC Item Updater.
    
    Change-Id: I1c4f8ec94d7d35a0e03a363007b79414b20058ac
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ item_updater_main.cpp
@@ -0,0 +1,23 @@
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/manager.hpp>
+#include "config.h"
+#include "item_updater.hpp"
+
+int main(int argc, char* argv[])
+{
+    auto bus = sdbusplus::bus::new_default();
+
+    // Add sdbusplus ObjectManager.
+    sdbusplus::server::manager::manager objManager(bus, SOFTWARE_OBJPATH);
+
+    phosphor::software::updater::ItemUpdater updater(bus);
+
+    bus.request_name(BUSNAME_UPDATER);
+
+    while (true)
+    {
+        bus.process_discard();
+        bus.wait();
+    }
+    return 0;
+}

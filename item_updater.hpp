43b8b51c9fa4df16b53b81d8ae905669fbbfce63
Saqib Khan <khansa@us.ibm.com>
2017-05-22 11:37:32 -0500

    ItemUpdater: Validate BMC squashfs image
    - Validate the image dir by checking its content.
    - Set Activation to "Active" if current image,
      "Ready" if validation passed and "Invalid" if
      validation fails
    
    Resolves openbmc/openbmc#1629
    
    Change-Id: I5f1c42fb2ba93c849ea8653c9e81c737ae27c814
    Signed-off-by: Saqib Khan <khansa@us.ibm.com>

--- item_updater.hpp
+++ item_updater.hpp
@@ -50,6 +50,17 @@ class ItemUpdater
          */
         void createActivation(sdbusplus::message::message& msg);
 
+        /**
+         * @brief Validates the presence of SquashFS iamge in the image dir.
+         *
+         * @param[in]  versionId - The software version ID.
+         * @param[out] result    - 0 --> Image is READY for activation
+         *                       - 1 --> image is already Active
+         *                       - -1--> Image is Invalid
+         *
+         */
+        static int validateSquashFSImage(const std::string& versionId);
+
         /** @brief Persistent sdbusplus DBus bus connection. */
         sdbusplus::bus::bus& bus;
 

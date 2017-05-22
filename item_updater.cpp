d897b8cc14367ad25affba9896108a97f0ab644d
Saqib Khan <khansa@us.ibm.com>
2017-05-22 11:37:32 -0500

    ItemUpdater: Validate BMC squashfs image
    - Validate the image dir by checking its content.
    - Set Activation to "Active" if current image,
      "Ready" if validation passed and "Invalid" if
      validation fails
    
    Resolves openbmc/openbmc#1629
    
    Change-Id: I5f1c42fb2ba93c849ea8653c9e81c737ae27c814
    Signed-off-by: Saqib Khan <khansa@us.ibm.com>

--- item_updater.cpp
+++ item_updater.cpp
@@ -1,8 +1,10 @@
+#include <fstream>
 #include <string>
 #include <phosphor-logging/log.hpp>
 #include "config.h"
 #include "item_updater.hpp"
 #include "xyz/openbmc_project/Software/Version/server.hpp"
+#include <experimental/filesystem>
 
 namespace phosphor
 {
@@ -15,6 +17,9 @@ namespace updater
 namespace server = sdbusplus::xyz::openbmc_project::Software::server;
 
 using namespace phosphor::logging;
+namespace fs = std::experimental::filesystem;
+
+constexpr auto bmcImage = "image-rofs";
 
 void ItemUpdater::createActivation(sdbusplus::message::message& msg)
 {
@@ -63,17 +68,53 @@ void ItemUpdater::createActivation(sdbusplus::message::message& msg)
 
     if (activations.find(versionId) == activations.end())
     {
-        // For now set all BMC code versions to active
-        constexpr auto activationState =
-                server::Activation::Activations::Active;
-
-        activations.insert(
-                std::make_pair(
-                        versionId,
-                        std::make_unique<Activation>(
-                                bus, path, versionId, activationState)));
+        // Determine the Activation state by processing the given image dir.
+        auto activationState = server::Activation::Activations::Invalid;
+        auto result = ItemUpdater::validateSquashFSImage(versionId);
+        if (result == 0)
+        {
+            activationState = server::Activation::Activations::Ready;
+        }
+        else if (result == 1)
+        {
+            activationState = server::Activation::Activations::Active;
+        }
+        updater->activations.insert(std::make_pair(
+                                        versionId,
+                                        std::make_unique<Activation>(
+                                            updater->bus,
+                                            path,
+                                            versionId,
+                                            activationState)));
     }
     return;
+}
+
+int ItemUpdater::validateSquashFSImage(const std::string& versionId)
+{
+
+    // TODO: Remove the validation for active image when
+    //       active image is implemented in the MANAGER class
+    fs::path imageDirPath(IMG_UPLOAD_DIR);
+    imageDirPath /= versionId;
+    if (!fs::is_directory(imageDirPath))
+    {
+        return 1;
+    }
+
+    fs::path file(imageDirPath);
+    file /= bmcImage;
+    std::ifstream efile(file.c_str());
+
+    if (efile.good() == 1)
+    {
+        return 0;
+    }
+    else
+    {
+        log<level::ERR>("Failed to find the SquashFS image.");
+        return -1;
+    }
 }
 
 } // namespace updater

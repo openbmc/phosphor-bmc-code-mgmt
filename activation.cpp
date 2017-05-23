c5511fb8556bf6f59b9701b945759557d41211eb
Saqib Khan <khansa@us.ibm.com>
2017-05-23 16:02:41 -0500

    Create Constructor for BMC Activation
    - Create Activation Block Transition
    - Create override functions so that actions can be taken
      based on the value that the properties are being set to.
    - Create ActivationBlockTransition when the Activating
      property is set to Activating. Remove it when the property
      is set to anything else.
    
    Change-Id: Id48d049cca54ae0bcbed6afe41e67e20e6a5e44c
    Signed-off-by: Saqib Khan <khansa@us.ibm.com>

---
+++ activation.cpp
@@ -0,0 +1,54 @@
+#include "activation.hpp"
+
+namespace phosphor
+{
+namespace software
+{
+namespace updater
+{
+
+namespace softwareServer = sdbusplus::xyz::openbmc_project::Software::server;
+
+auto Activation::activation(Activations value) ->
+        Activations
+{
+    if (value == softwareServer::Activation::Activations::Activating)
+    {
+        if (!activationBlocksTransition)
+        {
+            activationBlocksTransition =
+                      std::make_unique<ActivationBlocksTransition>(
+                                bus,
+                                path);
+        }
+    }
+    else
+    {
+        activationBlocksTransition.reset(nullptr);
+    }
+    return softwareServer::Activation::activation(value);
+}
+
+auto Activation::requestedActivation(RequestedActivations value) ->
+        RequestedActivations
+{
+    if ((value == softwareServer::Activation::RequestedActivations::Active) &&
+        (softwareServer::Activation::requestedActivation() !=
+                  softwareServer::Activation::RequestedActivations::Active))
+    {
+        if ((softwareServer::Activation::activation() ==
+                    softwareServer::Activation::Activations::Ready) ||
+            (softwareServer::Activation::activation() ==
+                    softwareServer::Activation::Activations::Failed))
+        {
+            Activation::activation(
+                    softwareServer::Activation::Activations::Activating);
+
+        }
+    }
+    return softwareServer::Activation::requestedActivation(value);
+}
+
+} // namespace updater
+} // namespace software
+} // namespace phosphor

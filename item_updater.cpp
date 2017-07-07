bc21d04badc4b4c0b7c664f5fb3def6e867e0566
Leonel Gonzalez <lgonzalez@us.ibm.com>
2017-07-07 14:38:25 -0500

    item_updater: Added erase method
    Implements Object.Delete, so item_updater deletes
    ReadWrite, ReadOnly partitions, and removes
    Version, and Activation from maps.
    
    Resolves openbmc/openbmc#1550
    
    Change-Id: If6d08bc7d380d043c28a26570e4698201cd7f5b0
    Signed-off-by: Leonel Gonzalez <lgonzalez@us.ibm.com>

--- item_updater.cpp
+++ item_updater.cpp
@@ -152,6 +152,36 @@ void ItemUpdater::processBMCImage()
     return;
 }
 
+void ItemUpdater::erase(std::string entryId)
+{
+    auto it = std::find_if(versions.begin(), versions.end(),
+        [&](std::pair<const std::string, std::unique_ptr<
+        phosphor::software::manager::Version>>& t)
+        {
+            return (*(t.second)).version() == entryId;
+        });
+    if (it == versions.end())
+    {
+        return;
+    }
+    this->versions.erase(it->first);
+
+    auto ita = activations.find(it->first);
+    if (ita == activations.end())
+    {
+        return;
+    }
+    // TODO: openbmc/openbmc#1986
+    //       Test if this is the currently running image
+    //       If not, don't continue.
+
+    // Delete ReadWrite and ReadOnly partitions
+    // and remove from map
+    removeReadWritePartition(it->first);
+    removeReadOnlyPartition(it->first);
+    this->activations.erase(it->first);
+}
+
 ItemUpdater::ActivationStatus ItemUpdater::validateSquashFSImage(
              const std::string& filePath)
 {
@@ -200,6 +230,35 @@ void ItemUpdater::reset()
     log<level::INFO>("BMC factory reset will take effect upon reboot.");
 
     return;
+}
+
+void ItemUpdater::removeReadOnlyPartition(std::string versionId)
+{
+    auto serviceFile = "obmc-flash-bmc-ubiro-remove@" + versionId +
+            ".service";
+
+    // Remove the read-only partitions.
+    auto method = bus.new_method_call(
+            SYSTEMD_BUSNAME,
+            SYSTEMD_PATH,
+            SYSTEMD_INTERFACE,
+            "StartUnit");
+    method.append(serviceFile, "replace");
+    bus.call_noreply(method);
+}
+
+void ItemUpdater::removeReadWritePartition(std::string versionId)
+{
+    auto serviceFile = "obmc-flash-bmc-ubirw-remove.service";
+
+    // Remove the read-write partitions.
+    auto method = bus.new_method_call(
+            SYSTEMD_BUSNAME,
+            SYSTEMD_PATH,
+            SYSTEMD_INTERFACE,
+            "StartUnit");
+    method.append(serviceFile, "replace");
+    bus.call_noreply(method);
 }
 
 } // namespace updater

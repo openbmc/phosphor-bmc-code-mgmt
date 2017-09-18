bc1bf3af74d1ecac11dbb763369651bc6bf43db1
Michael Tritz <mtritz@us.ibm.com>
2017-09-18 16:38:23 -0500

    BMC Updater: Add function to delete all versions
    This commit extends the item updater with a function to delete all
    non-current versions uploaded to the machine.
    
    Resolves openbmc/openbmc#2264
    
    Change-Id: I4d835d5020766f8bb5e9653b28535acfb191fe53
    Signed-off-by: Michael Tritz <mtritz@us.ibm.com>

--- item_updater.cpp
+++ item_updater.cpp
@@ -297,6 +297,33 @@ void ItemUpdater::erase(std::string entryId)
     this->activations.erase(entryId);
 }
 
+void ItemUpdater::deleteAll()
+{
+    std::vector<std::string> deletableVersions;
+
+    for (const auto& versionIt : versions)
+    {
+        if (!versionIt.second->isFunctional())
+        {
+            deletableVersions.push_back(versionIt.first);
+        }
+    }
+
+    for (const auto& deletableIt : deletableVersions)
+    {
+        ItemUpdater::erase(deletableIt);
+    }
+
+    // Remove any volumes that do not match current versions.
+    auto method = bus.new_method_call(
+            SYSTEMD_BUSNAME,
+            SYSTEMD_PATH,
+            SYSTEMD_INTERFACE,
+            "StartUnit");
+    method.append("obmc-flash-bmc-cleanup.service", "replace");
+    bus.call_noreply(method);
+}
+
 ItemUpdater::ActivationStatus ItemUpdater::validateSquashFSImage(
         const std::string& filePath)
 {

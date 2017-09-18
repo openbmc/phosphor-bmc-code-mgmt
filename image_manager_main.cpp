9b200b01112faf395fa484c57ebfb5b95b6a7998
Gunnar Mills <gmills@us.ibm.com>
2017-09-18 11:16:37 -0500

    Style changes in image_manager_main.cpp
    Style only changes
    
    Change-Id: I7cec0ac37bc7c72f0dfeedf77811f5036820019f
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- image_manager_main.cpp
+++ image_manager_main.cpp
@@ -8,23 +8,24 @@
 
 int main(int argc, char* argv[])
 {
+    using namespace phosphor::software::manager;
     auto bus = sdbusplus::bus::new_default();
 
     sd_event* loop = nullptr;
     sd_event_default(&loop);
 
     sdbusplus::server::manager::manager objManager(bus,
-            SOFTWARE_OBJPATH);
+                                                   SOFTWARE_OBJPATH);
     bus.request_name(VERSION_BUSNAME);
 
     try
     {
         phosphor::software::manager::Manager imageManager(bus);
-        phosphor::software::manager::Watch watch(loop,
-                std::bind(
-                    std::mem_fn(
-                        &phosphor::software::manager::Manager::processImage),
-                    &imageManager, std::placeholders::_1));
+        phosphor::software::manager::Watch watch(
+                loop,
+                std::bind(std::mem_fn(&Manager::processImage),
+                          &imageManager,
+                          std::placeholders::_1));
         bus.attach_event(loop, SD_EVENT_PRIORITY_NORMAL);
         sd_event_loop(loop);
     }

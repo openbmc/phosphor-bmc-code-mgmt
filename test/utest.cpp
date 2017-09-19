57dd90a30f7c044f011b9c3479d2969141a29db4
Saqib Khan <khansa@us.ibm.com>
2017-09-19 14:23:28 -0500

    Use sha512 to calculate the versionID for images.
    - We needed a hash algorithm that can be replicated on
      multiple platforms and across multiple programming languages.
    - This would allow the user to calculate the versionID by
      executing the following command to obtain correct versionID.
      echo -n "<versionID>" | sha512sum | cut -b 1-8
    - This is part of the change required for openbmc/openbmc#2323
    
    Change-Id: Ibc96a2871f07cb549c482c06dec7090e6ba3a766
    Signed-off-by: Saqib Khan <khansa@us.ibm.com>

--- test/utest.cpp
+++ test/utest.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <openssl/sha.h>
 
 using namespace phosphor::software::manager;
 namespace fs = std::experimental::filesystem;
@@ -56,12 +57,18 @@ TEST_F(VersionTest, TestGetValue)
 /** @brief Make sure we correctly get the Id from getId()*/
 TEST_F(VersionTest, TestGetId)
 {
-    std::stringstream hexId;
     auto version = "test-id";
-
-    hexId << std::hex << ((std::hash<std::string> {}(
-                               version)) & 0xFFFFFFFF);
-
-    EXPECT_EQ(Version::getId(version), hexId.str());
-
+    unsigned char digest[SHA512_DIGEST_LENGTH];
+    SHA512_CTX ctx;
+    SHA512_Init(&ctx);
+    SHA512_Update(&ctx, version, strlen(version));
+    SHA512_Final(digest, &ctx);
+    char mdString[SHA512_DIGEST_LENGTH*2+1];
+    for (int i = 0; i < SHA512_DIGEST_LENGTH; i++)
+    {
+        snprintf(&mdString[i*2], 3, "%02x", (unsigned int)digest[i]);
+    }
+    std::string hexId = std::string(mdString);
+    hexId = hexId.substr(0, 8);
+    EXPECT_EQ(Version::getId(version), hexId);
 }

f82ab1efe61b7480cf03ccbd84418bd100a480fe
Marri Devender Rao <devenrao@in.ibm.com>
2018-02-20 06:54:22 -0600

    Add unit tests for testing key management functionality
    Change-Id: I709748b6e0fde375dd588bd9eb3cbf7d63394db2
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- test/utest.cpp
+++ test/utest.cpp
@@ -1,4 +1,5 @@
 #include "version.hpp"
+#include "key_manager.hpp"
 #include <gtest/gtest.h>
 #include <experimental/filesystem>
 #include <stdlib.h>
@@ -9,8 +10,6 @@
 #include <openssl/sha.h>
 
 using namespace phosphor::software::manager;
-namespace fs = std::experimental::filesystem;
-
 
 class VersionTest : public testing::Test
 {
@@ -34,6 +33,135 @@ class VersionTest : public testing::Test
 
         std::string _directory;
 };
+
+class KeyManagerTest : public testing::Test
+{
+    protected:
+
+        virtual void SetUp()
+        {
+            char keyManagerDir[] = "./keyManagerXXXXXX";
+            _directory = mkdtemp(keyManagerDir);
+            if (_directory.empty())
+            {
+                throw std::bad_alloc();
+            }
+            extractPath = _directory;
+
+            hashFilePath /= extractPath;
+            hashFilePath /= "hash";
+
+            publicKeyPath = extractPath;
+            publicKeyPath /= "key";
+
+            keyManager = std::make_unique<KeyManager>(extractPath,
+                                hashFilePath, publicKeyPath);
+
+            fs::path manifestFilePath = extractPath;
+            manifestFilePath /= "MANIFEST";
+
+            std::ofstream manFile;
+            manFile.open(manifestFilePath, std::ofstream::out);
+            ASSERT_TRUE(manFile.is_open());
+            manFile << "HashType=" << "RSA-SHA512" << std::endl;
+            manFile << "KeyType=" << "OpenBMC" << std::endl;
+            manFile.close();
+        }
+
+        virtual void TearDown()
+        {
+            try
+            {
+                fs::remove_all(_directory);
+            }
+            catch(const std::exception& e)
+            {
+                std::cout << "failed to remove files" << std::endl;
+            }
+        }
+
+        fs::path _directory;
+        fs::path extractPath;
+        fs::path hashFilePath;
+        fs::path publicKeyPath;
+
+        std::unique_ptr<KeyManager> keyManager;
+};
+
+TEST_F(KeyManagerTest, TestGetKeyTypeFromManifest)
+{
+    EXPECT_EQ(keyManager->getKeyTypeFromManifest(), "OpenBMC");
+}
+
+TEST_F(KeyManagerTest, TestGetHashTypeFromManifest)
+{
+    EXPECT_EQ(keyManager->getHashTypeFromManifest(), "RSA-SHA512");
+}
+
+TEST_F(KeyManagerTest, TestSavePublicKeyToSystem)
+{
+    fs::path path(extractPath);
+    path /= PUBLIC_KEY_FILE_NAME;
+
+    std::ofstream file;
+    file.open(path.string(), std::ofstream::out);
+    ASSERT_TRUE(file.is_open());
+    file << "-----BEGIN PUBLIC KEY-----" << std::endl;
+    file << "a133#2a&*91ad~}" << std::endl;
+    file << "-----END PUBLIC KEY-----" << std::endl;
+    file.close();
+
+    keyManager->savePublicKeyToSystem();
+
+    //check if the file is copied
+    fs::path pubpath = publicKeyPath;
+    pubpath /= "OpenBMC";
+    pubpath /= PUBLIC_KEY_FILE_NAME;
+
+    EXPECT_EQ(fs::exists(pubpath), true);
+    fs::remove(path);
+}
+
+TEST_F(KeyManagerTest, TestGetAllKeyFilePath)
+{
+    //create public key file
+    fs::path pubpath(publicKeyPath);
+    pubpath /= "OpenBMC";
+
+    fs::create_directories(pubpath);
+    pubpath /= PUBLIC_KEY_FILE_NAME;
+
+    std::ofstream keyfile;
+    keyfile.open(pubpath.string(), std::ofstream::out);
+    keyfile << "-----BEGIN PUBLIC KEY-----" << std::endl;
+    keyfile << "a133#2a&*91ad~}" << std::endl;
+    keyfile << "-----END PUBLIC KEY-----" << std::endl;
+    keyfile.close();
+
+    PubKeyPathList keyFile = keyManager->getAllPubKeyFilePath();
+    EXPECT_EQ(keyFile.size(), 1);
+
+    fs::remove(pubpath);
+}
+
+TEST_F(KeyManagerTest, TestGetAllHashFilePath)
+{
+    //create hash key file
+    fs::path hashpath(hashFilePath);
+    hashpath /= "OpenBMC";
+    fs::create_directories(hashpath);
+    hashpath /= HASH_FILE_NAME;
+    std::ofstream hashfile;
+    hashfile.open(hashpath.string(), std::ofstream::out);
+    hashfile << "HashType=" << "SHA512" << std::endl;
+    hashfile.close();
+
+    HashFilePathList hashFiles = keyManager->getAllHashFilePath();
+    EXPECT_EQ(hashFiles.size(), 1);
+
+    fs::remove(hashpath);
+}
+
 
 /** @brief Make sure we correctly get the version and purpose from getValue()*/
 TEST_F(VersionTest, TestGetValue)

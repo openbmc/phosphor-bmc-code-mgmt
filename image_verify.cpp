7b27145099ec744a262e8752c02f45543ae97a12
Jayanth Othayoth <ojayanth@in.ibm.com>
2018-02-21 05:27:47 -0600

    Add support for signature verification routines
    Enabled high level logic flow for the signed image
    signature validation routines.
    
    Change-Id: I9b0213042bb15882f351e7937fd17fb0a3e9fb33
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>

--- image_verify.cpp
+++ image_verify.cpp
@@ -1,4 +1,8 @@
 #include "image_verify.hpp"
+#include "config.h"
+
+#include <phosphor-logging/log.hpp>
+#include <phosphor-logging/elog.hpp>
 
 namespace phosphor
 {
@@ -7,7 +11,115 @@ namespace software
 namespace image
 {
 
+using namespace phosphor::logging;
+
 bool Signature::verify()
+{
+    bool valid = false;
+
+    // Verify the MANIFEST file using available public keys and hash
+    // on the system.
+    if (false == primaryValidation(MANIFEST_FILE_NAME))
+    {
+        //TODO Add Error handler
+        log<level::ERR>("Manifest file Signature Validation failed");
+        return false;
+    }
+
+    //  Verify the publickey file using available public keys and hash
+    //  on the system
+    if (false == primaryValidation(PUBLICKEY_FILE_NAME))
+    {
+        //TODO Add Error handler
+        log<level::ERR>("Public Key file Signature Validation failed");
+        return false;
+    }
+
+    //Get hash function information from the Manifest file
+    auto hashFunction = keyManager.getHashTypeFromManifest();
+
+    //Get Image specific public Key file.
+    fs::path publicKey(imageDirPath);
+    publicKey /= PUBLICKEY_FILE_NAME;
+
+    //Validate the BMC image files.
+    for (const auto& bmcImage : bmcImages)
+    {
+        //Build Image File name
+        fs::path file(imageDirPath);
+        file /= bmcImage;
+
+        //Build Signature File name
+        fs::path sigFile(imageDirPath);
+        sigFile /= bmcImage;
+        sigFile.replace_extension(SIGNATURE_FILE_EXT);
+
+        //Verify the signature.
+        valid = verifyFile(file,
+                           sigFile,
+                           publicKey,
+                           hashFunction);
+        if (valid == false)
+        {
+            //TODO Add Error handler
+            log<level::ERR>("Image file Signature Validation failed",
+                            entry("IMAGE=%s", bmcImage.c_str()));
+            return false;
+        }
+    }
+
+    log<level::INFO>("Sucessfully completed Signature vaildation.");
+
+    return true;
+}
+
+
+bool Signature::primaryValidation(const std::string& name)
+{
+    //Build File complete path
+    fs::path file(imageDirPath);
+    file /= name;
+
+    //Build Signature File name
+    fs::path sigFile(imageDirPath);
+    sigFile /= name;
+    sigFile.replace_extension(SIGNATURE_FILE_EXT);
+
+    //Get available key types from the system.
+    auto keyTypes = keyManager.getAvailableKeyTypesFromSystem();
+    if (keyTypes.empty())
+    {
+        log<level::ERR>("Missing Signature configuration data in the system");
+        return false;
+    }
+
+    bool valid = false;
+
+    //Verify the file signature with available key types
+    //public keys and hash function.
+    for (const auto& keyType : keyTypes)
+    {
+        auto keyHashPair = keyManager.getKeyHashFileNames(keyType);
+
+        auto hashFunc = keyManager.getHashTypeFromFile(keyHashPair.first);
+
+        valid = verifyFile(file,
+                           sigFile,
+                           keyHashPair.second,
+                           hashFunc);
+        if (valid)
+        {
+            break;
+        }
+    }
+
+    return valid;
+}
+
+bool Signature::verifyFile(const fs::path& file,
+                           const fs::path& sigFile,
+                           const fs::path& publicKey,
+                           const std::string& hashFunc)
 {
     return true;
 }
--- image_verify.hpp
+++ image_verify.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <experimental/filesystem>
+#include "key_manager.hpp"
 
 namespace phosphor
 {
@@ -10,6 +11,11 @@ namespace image
 
 namespace fs = std::experimental::filesystem;
 
+const std::vector<std::string> bmcImages = { "image-kernel",
+                                             "image-rofs",
+                                             "image-rwfs",
+                                             "image-u-boot"
+                                           };
 /** @class Signature
  *  @brief Contains signature verification functions.
  *  @details The software image class that contains the signature
@@ -25,11 +31,17 @@ class Signature
         Signature& operator=(Signature&&) = delete;
         virtual ~Signature() = default;
 
-        /** @brief Constructs Verify Class
-         *
-         * @param[in]  imageDirPath - file path
+        /** @brief Constructs Signature.
+         * @param[in]  imageDirPath - image path
+         * @details Initlizes the KeyManager object based on the image
+         *          directory path. KeyManager object provides image
+         *          specific configurations information required for
+         *          signature vaidation.
          */
-        Signature(fs::path& imageDirPath) : imageDirPath(imageDirPath) {};
+        Signature(fs::path imageDirPath):
+            imageDirPath(imageDirPath),
+            keyManager(imageDirPath)
+        {};
 
         /**
          * @brief Image signature verification function.
@@ -44,10 +56,34 @@ class Signature
         bool verify();
 
     private:
+        /**
+         * @brief Wrapper function used for primary file validation
+         *        verify the file using the available public keys
+         *        and hash functions in in the system.
+         *
+         * @return true if signature verification was successful, false if not
+         */
+        bool primaryValidation(const std::string& name);
+
+        /**
+         * @brief Verify the file siganture using public key and hash function
+         *
+         * @param[in]  - Image file path
+         * @param[in]  - Signature file path
+         * @param[in]  - Public key
+         * @param[in]  - Hash function name
+         * @return true if signature verification was successful, false if not
+         */
+        bool verifyFile(const fs::path& file,
+                        const fs::path& signature,
+                        const fs::path& publicKey,
+                        const std::string& hashFunc);
 
         /** @brief Directory where software images are placed*/
         fs::path imageDirPath;
 
+        /** Key manager object */
+        KeyManager keyManager;
 };
 
 } // namespace image

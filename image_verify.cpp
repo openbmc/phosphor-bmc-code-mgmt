761bd2e5c921470ff510bf4ccdc5c6a9805d640a
Jayanth Othayoth <ojayanth@in.ibm.com>
2018-02-21 05:12:39 -0600

    Create initial Signature verification class
    Initial version of signature validation infrastructure
    for BMC signed image.
    
    Change-Id: I79d8ad10dbb7e3c4f0ffd21609b483be6734b4af
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>

---
+++ image_verify.cpp
@@ -0,0 +1,17 @@
+#include "image_verify.hpp"
+
+namespace phosphor
+{
+namespace software
+{
+namespace image
+{
+
+bool Signature::verify()
+{
+    return true;
+}
+
+} //namespace image
+} //namespace software
+} //namespace phosphor
---
+++ image_verify.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include <experimental/filesystem>
+
+namespace phosphor
+{
+namespace software
+{
+namespace image
+{
+
+namespace fs = std::experimental::filesystem;
+
+/** @class Signature
+ *  @brief Contains signature verification functions.
+ *  @details The software image class that contains the signature
+ *           verification functions for signed image.
+ */
+class Signature
+{
+    public:
+        Signature() = delete;
+        Signature(const Signature&) = default;
+        Signature& operator=(const Signature&) = delete;
+        Signature(Signature&&) = delete;
+        Signature& operator=(Signature&&) = delete;
+        virtual ~Signature() = default;
+
+        /** @brief Constructs Verify Class
+         *
+         * @param[in]  imageDirPath - file path
+         */
+        Signature(fs::path& imageDirPath) : imageDirPath(imageDirPath) {};
+
+        /**
+         * @brief Image signature verification function.
+         *        Verify the Manifest and public key file signature using the
+         *        public keys available in the system first. After successful
+         *        validation, continue the whole image files signature
+         *        validation using the image specific public key and the
+         *        hash function.
+         *
+         * @return true if signature verification was successful, false if not
+         */
+        bool verify();
+
+    private:
+
+        /** @brief Directory where software images are placed*/
+        fs::path imageDirPath;
+
+};
+
+} // namespace image
+} // namespace software
+} // namespace phosphor

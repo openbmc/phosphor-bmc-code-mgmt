3eae6c9e0959c07844f11e14df518aad742d267a
Jayanth Othayoth <ojayanth@in.ibm.com>
2018-02-21 05:27:47 -0600

    Add support for signature verification routines
    Enabled high level logic flow for the signed image
    signature validation routines.
    
    Inncludes reading hash type, key type from Manifest file.
    
    Change-Id: I9b0213042bb15882f351e7937fd17fb0a3e9fb33
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>

--- image_verify.cpp
+++ image_verify.cpp
@@ -1,4 +1,13 @@
+#include <set>
+
 #include "image_verify.hpp"
+#include "config.h"
+#include "version.hpp"
+
+#include <phosphor-logging/log.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <phosphor-logging/elog-errors.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
 
 namespace phosphor
 {
@@ -7,7 +16,178 @@ namespace software
 namespace image
 {
 
+using namespace phosphor::logging;
+using namespace phosphor::software::manager;
+using InternalFailure =
+    sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+
+constexpr auto keyTypeTag = "KeyType";
+constexpr auto hashFunctionTag = "HashType";
+
+Signature::Signature(const fs::path& imageDirPath,
+                     const fs::path& signedConfPath) :
+    imageDirPath(imageDirPath),
+    signedConfPath(signedConfPath),
+    publicKeyFile(imageDirPath / PUBLICKEY_FILE_NAME)
+{
+    fs::path file(imageDirPath / MANIFEST_FILE_NAME);
+
+    keyType = Version::getValue(file, keyTypeTag);
+    hashType = Version::getValue(file, hashFunctionTag);
+}
+
+inline const AvailableKeyTypes Signature::getAvailableKeyTypesFromSystem()
+{
+    AvailableKeyTypes keyTypes{};
+
+    // Find the path of all the files
+    if (!fs::is_directory(signedConfPath))
+    {
+        log<level::ERR>("Hash file path not found in the system");
+        elog<InternalFailure>();
+    }
+
+    // Look for all the hash and public key files and then get the parent paths
+    // and store it in set. Using set to remove duplicates.
+    // For example:
+    // /etc/activationdata/OpenBMC/publickey
+    // /etc/activationdata/OpenBMC/hashfunc
+    // Set will have /etc/activationdata/OpenBMC/
+
+    std::string key{};
+    std::size_t pos{};
+
+    for (const auto& p : fs::recursive_directory_iterator(signedConfPath))
+    {
+        if ((p.path().filename() == HASH_FILE_NAME) ||
+            (p.path().filename() == PUBLICKEY_FILE_NAME))
+        {
+            // extract the key types
+            // /etc/activationdata/OpenBMC/  -> get OpenBMC from the path
+            key = p.path().parent_path();
+            pos = key.find_last_of("/\\");
+            if ((key.length() > 0) && (pos != key.length() - 1))
+            {
+                keyTypes.emplace_back(key.substr(pos + 1));
+            }
+        }
+    }
+
+    return keyTypes;
+}
+
+inline const KeyHashPathPair Signature::getKeyHashFileNames(const Key_t& key)
+{
+    fs::path hashpath(signedConfPath / key / HASH_FILE_NAME);
+    fs::path keyPath(signedConfPath / key / PUBLICKEY_FILE_NAME);
+
+    return std::make_pair(hashpath, keyPath);
+}
+
+inline const Hash_t Signature::getHashTypeFromFile(const HashFilePath& file)
+{
+    return Version::getValue(file, hashFunctionTag);
+}
+
 bool Signature::verify()
+{
+    try
+    {
+        // Verify the MANIFEST file using available public keys and hash
+        // on the system.
+        if (false == systemLevelVerify(MANIFEST_FILE_NAME))
+        {
+            log<level::ERR>("Manifest file Signature Validation failed");
+            return false;
+        }
+
+        //  Verify the publickey file using available public keys and hash
+        //  on the system
+        if (false == systemLevelVerify(PUBLICKEY_FILE_NAME))
+        {
+            log<level::ERR>("Public Key file Signature Validation failed");
+            return false;
+        }
+
+        //Validate the BMC image files.
+        for (const auto& bmcImage : bmcImages)
+        {
+            //Build Image File name
+            fs::path file(imageDirPath);
+            file /= bmcImage;
+
+            //Build Signature File name
+            fs::path sigFile(file);
+            sigFile.replace_extension(SIGNATURE_FILE_EXT);
+
+            //Verify the signature.
+            auto valid = verifyFile(file,
+                                    sigFile,
+                                    publicKeyFile,
+                                    hashType);
+            if (valid == false)
+            {
+                log<level::ERR>("Image file Signature Validation failed",
+                                entry("IMAGE=%s", bmcImage.c_str()));
+                return false;
+            }
+        }
+
+        log<level::INFO>("Sucessfully completed Signature vaildation.");
+
+        return true;
+    }
+    catch (const InternalFailure& e)
+    {
+        return false;
+    }
+}
+
+
+bool Signature::systemLevelVerify(const std::string& name)
+{
+    //Build File complete path
+    fs::path file(imageDirPath);
+    file /= name;
+
+    //Build Signature File name
+    fs::path sigFile(file);
+    sigFile.replace_extension(SIGNATURE_FILE_EXT);
+
+    //Get available key types from the system.
+    auto keyTypes = getAvailableKeyTypesFromSystem();
+    if (keyTypes.empty())
+    {
+        log<level::ERR>("Missing Signature configuration data in system");
+        elog<InternalFailure>();
+    }
+
+    auto valid = false;
+
+    //Verify the file signature with available key types
+    //public keys and hash function.
+    for (const auto& keyType : keyTypes)
+    {
+        auto keyHashPair = getKeyHashFileNames(keyType);
+
+        auto hashFunc = getHashTypeFromFile(keyHashPair.first);
+
+        valid = verifyFile(file,
+                           sigFile,
+                           keyHashPair.second,
+                           hashFunc);
+        if (valid)
+        {
+            break;
+        }
+    }
+    return valid;
+}
+
+bool Signature::verifyFile(const fs::path& file,
+                           const fs::path& sigFile,
+                           const fs::path& publicKey,
+                           const std::string& hashFunc)
 {
     return true;
 }
--- image_verify.hpp
+++ image_verify.hpp
@@ -9,7 +9,19 @@ namespace image
 {
 
 namespace fs = std::experimental::filesystem;
+using Key_t = std::string;
+using Hash_t = std::string;
+using PublicKeyPath = fs::path;
+using HashFilePath = fs::path;
+using KeyHashPathPair = std::pair<HashFilePath, PublicKeyPath>;
+using AvailableKeyTypes = std::vector<Key_t>;
 
+//BMC flash image file name list.
+const std::vector<std::string> bmcImages = { "image-kernel",
+                                             "image-rofs",
+                                             "image-rwfs",
+                                             "image-u-boot"
+                                           };
 /** @class Signature
  *  @brief Contains signature verification functions.
  *  @details The software image class that contains the signature
@@ -25,11 +37,14 @@ class Signature
         Signature& operator=(Signature&&) = default;
         ~Signature() = default;
 
-        /** @brief Constructs Verify Class
-         *
-         * @param[in]  imageDirPath - file path
+        /**
+         * @brief Constructs Signature.
+         * @param[in]  imageDirPath - image path
+         * @param[in]  signedConfPath - Path of public key
+         *                              hash function files
          */
-        Signature(const fs::path& imageDirPath) : imageDirPath(imageDirPath) {};
+        Signature(const fs::path& imageDirPath,
+                  const fs::path& signedConfPath);
 
         /**
          * @brief Image signature verification function.
@@ -45,10 +60,69 @@ class Signature
         bool verify();
 
     private:
+        /**
+         * @brief Function used for system level file signature validation
+         *        using the available public keys and hash functions
+         *        in the system.
+         *        @param[in]  name - file name
+         */
+        bool systemLevelVerify(const std::string& name);
 
-        /** @brief Directory where software images are placed*/
+        /**
+         *  @brief Return all key types stored in the BMC based on the
+         *         public key and hashfunc files stored in the BMC.
+         *
+         *  @return list
+         */
+        inline const AvailableKeyTypes getAvailableKeyTypesFromSystem();
+
+        /**
+         *  @brief Return public key and hash function file names for the
+         *  corresponding key type
+         *
+         *  @param[in]  key - key type
+         *  @return Pair of hash and public key file names
+         */
+        inline const KeyHashPathPair getKeyHashFileNames(const Key_t& key);
+
+        /**
+         * @brief Read the hash algorithm type defined in the specified file
+         *
+         * @param[in]  file - hash type file.
+         *
+         * @return Hash type
+         */
+        inline const Hash_t getHashTypeFromFile(const HashFilePath& file);
+
+        /**
+         * @brief Verify the file signature using public key and hash function
+         *
+         * @param[in]  - Image file path
+         * @param[in]  - Signature file path
+         * @param[in]  - Public key
+         * @param[in]  - Hash function name
+         * @return true if signature verification was successful, false if not
+         */
+        bool verifyFile(const fs::path& file,
+                        const fs::path& signature,
+                        const fs::path& publicKey,
+                        const std::string& hashFunc);
+
+
+        /** @brief Directory where software images are placed */
         fs::path imageDirPath;
 
+        /** @brief Path of public key and hash function files */
+        fs::path signedConfPath;
+
+        /** @brief Path of the public key file extracted from the image */
+        fs::path publicKeyFile;
+
+        /** @brief key type defined in mainfest file */
+        Key_t keyType;
+
+        /** @brief Hash type defined in mainfest file */
+        Hash_t hashType;
 };
 
 } // namespace image

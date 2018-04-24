c41a9902d1c150b06ae80d379335680ed52e21c8
Adriana Kobylak <anoo@us.ibm.com>
2018-04-24 11:44:28 -0500

    sync_manager: Add callback to do rsync
    Call rsync when the subscribed file or directory is modified or
    deleted. Don't create error logs as syncing is used for backup
    and does not affect the system's operation. Any errors will be
    logged into the journal.
    
    Change-Id: I2671f0afd2924c15ea883d4d037c641c6e9680b4
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- sync_manager.cpp
+++ sync_manager.cpp
@@ -1,3 +1,8 @@
+#include <phosphor-logging/log.hpp>
+#include <sys/inotify.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include "config.h"
 #include "sync_manager.hpp"
 
 namespace phosphor
@@ -7,8 +12,45 @@ namespace software
 namespace manager
 {
 
-int Sync::processEntry()
+using namespace phosphor::logging;
+
+int Sync::processEntry(int mask, const std::string& entryPath)
 {
+    int status{};
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        auto dst = std::string{ALT_RWFS} + entryPath;
+        if (mask & IN_CLOSE_WRITE)
+        {
+            execl("/usr/bin/rsync", "rsync", "-a", entryPath.c_str(),
+                  dst.c_str(), nullptr);
+            log<level::ERR>("Error occurred during the rsync call",
+                            entry("ERRNO=%d", errno),
+                            entry("PATH=%s", entryPath.c_str()));
+            return -1;
+        }
+        else if (mask & IN_DELETE)
+        {
+            execl("/usr/bin/rsync", "rsync", "-a", "--delete",
+                  entryPath.c_str(), dst.c_str(), nullptr);
+            log<level::ERR>("Error occurred during the rsync delete call",
+                            entry("ERRNO=%d", errno),
+                            entry("PATH=%s", entryPath.c_str()));
+            return -1;
+        }
+    }
+    else if (pid > 0)
+    {
+        waitpid(pid, &status, 0);
+    }
+    else
+    {
+        log<level::ERR>("Error occurred during fork", entry("ERRNO=%d", errno));
+        return -1;
+    }
+
     return 0;
 }
 

fa078e556c3ddb5d68dd3429164f7a2cac355d96
Lei YU <mine260309@gmail.com>
2018-06-21 16:09:44 +0800

    item_updater: sort ubi specific code
    Add item_updater_helper and implement it in ubi and static layouts.
    
    Tested: Build this repo on both Romulus and Witherspoon OK;
            Tested on Romulus and Witherspoon that code update works fine.
    
    Change-Id: I706cfe63900f89bb41672dcc745b3483e06838c7
    Signed-off-by: Lei YU <mine260309@gmail.com>

---
+++ static/item_updater_helper.cpp
@@ -0,0 +1,47 @@
+#include "item_updater_helper.hpp"
+
+namespace phosphor
+{
+namespace software
+{
+namespace updater
+{
+
+void Helper::clearEntry(const std::string& entryId)
+{
+    // Empty
+}
+
+void Helper::cleanup()
+{
+    // Empty
+}
+
+void Helper::factoryReset()
+{
+    // TODO
+}
+
+void Helper::removeVersion(const std::string& versionId)
+{
+    // Empty
+}
+
+void Helper::updateUbootVersionId(const std::string& versionId)
+{
+    // Empty
+}
+
+void Helper::enableFieldMode()
+{
+    // TODO
+}
+
+void Helper::mirrorAlt()
+{
+    // Empty
+}
+
+} // namespace updater
+} // namespace software
+} // namespace phosphor

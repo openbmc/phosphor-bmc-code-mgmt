eb4082aaff1a86a075ef714cb9380f3a9efafeb6
Lei YU <mine260309@gmail.com>
2018-07-17 17:04:34 +0800

    Use separated images for fixed flash layout
    For fixed flash layout code update, it was using the while image-bmc.
    For future features like image verification during boot, it needs to use
    separated images and separated keys.
    
    So change the image-bmc to separated images.
    
    Change-Id: I6cc56f82c786125bb4386af5ea1d931afed7901d
    Tested: Do code update on Romulus without issue.
    Signed-off-by: Lei YU <mine260309@gmail.com>

--- images.hpp
+++ images.hpp
@@ -11,12 +11,8 @@ namespace image
 {
 
 // BMC flash image file name list.
-#ifdef UBIFS_LAYOUT
 const std::vector<std::string> bmcImages = {"image-kernel", "image-rofs",
                                             "image-rwfs", "image-u-boot"};
-#else
-const std::vector<std::string> bmcImages = {"image-bmc"};
-#endif
 
 } // namespace image
 } // namespace software

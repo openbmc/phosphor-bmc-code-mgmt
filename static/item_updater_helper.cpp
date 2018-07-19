ae67602ca18bf112f1635bea94d089971f6f0ec8
Lei YU <mine260309@gmail.com>
2018-07-19 15:41:44 +0800

    static layout: support factory reset
    Implement factory reset for static flash layout, which deletes all files
    in the rwfs.
    
    Tested: Invoke factory reset on /xyz/openbmc_project/software and verify
            everything in rwfs is deleted, and after reboot, the BMC acts
            like a fresh new one.
    
    Change-Id: I20e361519af6c3c12703315070debabd8cf88060
    Signed-off-by: Lei YU <mine260309@gmail.com>

--- static/item_updater_helper.cpp
+++ static/item_updater_helper.cpp
@@ -1,11 +1,19 @@
 #include "item_updater_helper.hpp"
 
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/log.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+
+#include <experimental/filesystem>
+
 namespace phosphor
 {
 namespace software
 {
 namespace updater
 {
+using namespace phosphor::logging;
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
 
 void Helper::clearEntry(const std::string& entryId)
 {
@@ -19,7 +27,23 @@ void Helper::cleanup()
 
 void Helper::factoryReset()
 {
-    // TODO
+    // Factory reset cleans up all the contents in rwfs
+    namespace fs = std::experimental::filesystem;
+    constexpr auto rwDir = "/run/initramfs/rw/cow";
+
+    if (!fs::is_directory(rwDir))
+    {
+        // The rwDir is expected to be a dir,
+        // otherwise we have problem and let it throw
+        log<level::ERR>("Unpexted rw dir");
+        elog<InternalFailure>();
+    }
+
+    for (const auto& iter : fs::directory_iterator(rwDir))
+    {
+        fs::remove_all(iter);
+    }
+    log<level::INFO>("rwfs is cleaned up");
 }
 
 void Helper::removeVersion(const std::string& versionId)

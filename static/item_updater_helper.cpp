6de476b927c26aa54fcde5739084d105015b6703
Lei YU <mine260309@gmail.com>
2018-07-19 15:41:44 +0800

    static layout: support factory reset
    Implement factory reset for static flash layout, that set the
    factory-reset u-boot env and let init to do the cleanup.
    
    Tested: Invoke factory reset on /xyz/openbmc_project/software and verify
            everything in rwfs is deleted, and after reboot, the BMC acts
            like a fresh new one.
    
    Change-Id: I20e361519af6c3c12703315070debabd8cf88060
    Signed-off-by: Lei YU <mine260309@gmail.com>

--- static/item_updater_helper.cpp
+++ static/item_updater_helper.cpp
@@ -1,11 +1,16 @@
 #include "item_updater_helper.hpp"
 
+#include <phosphor-logging/log.hpp>
+
+#include <cstdlib>
+
 namespace phosphor
 {
 namespace software
 {
 namespace updater
 {
+using namespace phosphor::logging;
 
 void Helper::clearEntry(const std::string& entryId)
 {
@@ -19,7 +24,19 @@ void Helper::cleanup()
 
 void Helper::factoryReset()
 {
-    // TODO
+    // Set openbmconce=factory-reset env in u-boot,
+    // The init will cleanup rwfs during boot.
+    constexpr auto cmd = "fw_setenv openbmconce factory-reset";
+
+    // Set the variable twice so that it is written to both environments of
+    // the u-boot redundant environment variables since initramfs can only
+    // read one of the environments.
+    auto r = std::system(cmd);
+    r |= std::system(cmd);
+    if (r != 0)
+    {
+        log<level::ERR>("Failed to set factory-reset uboot env");
+    }
 }
 
 void Helper::removeVersion(const std::string& versionId)

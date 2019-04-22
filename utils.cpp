3fb2c1592cbd9f02e8d378dc97835da1df74a3a1
Jayashankar Padath <jayashankar.padath@in.ibm.com>
2019-04-22 16:22:58 +0530

    Code Update :  ApplyTime software manager support
    Get the requested image apply time value provided through the
    UpdateService redfish schema. If the apply time value is Immediate, then
    BMC reboot will be triggered just after the new image activation.
    
    Tested: Verified that BMC is getting rebooted if the apply time value is
    Immediate. OnReset scenario was also tested.
    
    Signed-off-by: Jayashankar Padath <jayashankar.padath@in.ibm.com>
    Change-Id: I5efb51f7f36e196d6113cfca6d37c8c6bef70aa2

--- utils.cpp
+++ utils.cpp
@@ -1,5 +1,7 @@
 #include "utils.hpp"
 
+#include <phosphor-logging/log.hpp>
+
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
 
 #include <string.h>
@@ -27,3 +29,44 @@ void EVP_MD_CTX_free(EVP_MD_CTX* ctx)
 }
 
 #endif // OPENSSL_VERSION_NUMBER < 0x10100000L
+
+namespace utils
+{
+
+using namespace phosphor::logging;
+
+std::string getService(sdbusplus::bus::bus& bus, const std::string& path,
+                       const std::string& interface)
+{
+    auto method = bus.new_method_call(MAPPER_BUSNAME, MAPPER_PATH,
+                                      MAPPER_BUSNAME, "GetObject");
+
+    method.append(path);
+    method.append(std::vector<std::string>({interface}));
+
+    std::map<std::string, std::vector<std::string>> response;
+
+    try
+    {
+        auto reply = bus.call(method);
+        reply.read(response);
+        if (response.empty())
+        {
+            log<level::ERR>("Error in mapper response for getting service name",
+                            entry("PATH=%s", path.c_str()),
+                            entry("INTERFACE=%s", interface.c_str()));
+            return std::string{};
+        }
+    }
+    catch (const sdbusplus::exception::SdBusError& e)
+    {
+        log<level::ERR>("Error in mapper method call",
+                        entry("ERROR=%s", e.what()),
+                        entry("PATH=%s", path.c_str()),
+                        entry("INTERFACE=%s", interface.c_str()));
+        return std::string{};
+    }
+    return response.begin()->first;
+}
+
+} // namespace utils
--- utils.hpp
+++ utils.hpp
@@ -13,3 +13,22 @@ void EVP_MD_CTX_free(EVP_MD_CTX* ctx);
 }
 
 #endif // OPENSSL_VERSION_NUMBER < 0x10100000L
+
+#include "config.h"
+
+#include <map>
+#include <sdbusplus/server.hpp>
+#include <string>
+
+namespace utils
+{
+
+/**
+ * @brief Get the bus service
+ *
+ * @return the bus service as a string
+ **/
+std::string getService(sdbusplus::bus::bus& bus, const std::string& path,
+                       const std::string& interface);
+
+} // namespace utils

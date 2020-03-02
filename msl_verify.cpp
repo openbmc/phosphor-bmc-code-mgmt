dd48e6e23e4010729f44a954df48259e0e77fb5a
Miguel Gomez <mgomez@mx1.ibm.com>
2020-03-02 20:50:16 +0000

    added msl_verify for BMC minimmum ship level
    Contains the new function getBMCVersion
    Added regex expresion to capture all formats of fw
    Tested: The binary was integrated as a service on romulus and did not crash.
    Address Makefile.am and configure.ac
    
    Signed-off-by: Miguel Gomez <mgomez@mx1.ibm.com>
    Change-Id: I0ab0eba7c7c89f38ca698aa3e369aa50797edb07

---
+++ msl_verify.cpp
@@ -0,0 +1,134 @@
+#include "config.h"
+#include "msl_verify.hpp"
+#include "version.hpp"
+
+#include <experimental/filesystem>
+#include <fstream>
+#include <phosphor-logging/log.hpp>
+#include <regex>
+
+namespace MinimumShipLevel
+{
+
+namespace fs = std::experimental::filesystem;
+namespace variant_ns = sdbusplus::message::variant_ns;
+using namespace phosphor::logging;
+using AssociationList =
+    std::vector<std::tuple<std::string, std::string, std::string>>;
+
+int compare(const Version& a, const Version& b)
+{
+    if (a.major < b.major)
+    {
+        return -1;
+    }
+    else if (a.major > b.major)
+    {
+        return 1;
+    }
+
+    if (a.minor < b.minor)
+    {
+        return -1;
+    }
+    else if (a.minor > b.minor)
+    {
+        return 1;
+    }
+
+    if (a.rev < b.rev)
+    {
+        return -1;
+    }
+    else if (a.rev > b.rev)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+void parse(const std::string& versionStr, Version& version)
+{
+    std::smatch match;
+    version = {0, 0, 0};
+
+    // Match for vX.Y.Z or v-X.Y.Z
+    std::regex regex{"([a-z]+-?)([0-9]+)\\.([0-9]+)-([0-9]+)-([0-9]+)", 
+	             std::regex::extended};
+
+    if (!std::regex_search(versionStr, match, regex))
+    {
+            log<level::ERR>("Unable to parse BMC version",
+                            entry("VERSION=%s", versionStr.c_str()));
+            return;
+    }
+    else
+    {
+        // Populate Z
+        version.rev = std::stoi(match[3]);
+    }
+    version.major = std::stoi(match[1]);
+    version.minor = std::stoi(match[2]);
+}
+
+bool verify()
+{
+    if (minShipLevel.empty())
+    {
+        return true;
+    }
+
+    auto actual = getBMCVersion(OS_RELEASE_FILE);
+    if (actual.empty())
+    {
+        return true;
+    }
+
+    // Multiple min versions separated by a space can be specified, parse them
+    // into a vector, then sort them in ascending order
+    std::istringstream minStream(minShipLevel);
+    std::vector<std::string> mins(std::istream_iterator<std::string>{minStream},
+                                  std::istream_iterator<std::string>());
+    std::sort(mins.begin(), mins.end());
+
+    // In order to handle non-continuous multiple min versions, need to compare
+    // the major.minor section first, then if they're the same, compare the rev.
+    // Ex: the min versions specified are 2.0.10 and 2.2. We need to pass if
+    // actual is 2.0.11 but fail if it's 2.1.x.
+    // 1. Save off the rev number to compare later if needed.
+    // 2. Zero out the rev number to just compare major and minor.
+    Version actualVersion = {0, 0, 0};
+    parse(actual, actualVersion);
+    Version actualRev = {0, 0, actualVersion.rev};
+    actualVersion.rev = 0;
+
+    auto rc = 0;
+    std::string tmpMin{};
+
+    for (auto const& min : mins)
+    {
+        tmpMin = min;
+
+        Version minVersion = {0, 0, 0};
+        parse(min, minVersion);
+        Version minRev = {0, 0, minVersion.rev};
+        minVersion.rev = 0;
+
+        rc = compare(actualVersion, minVersion);
+        if (rc < 0)
+        {
+            break;
+        }
+        else if (rc == 0)
+        {
+            // Same major.minor version, compare the rev
+            rc = compare(actualRev, minRev);
+            break;
+        }
+    }
+    
+    return true;
+}
+
+} // namespace MinimumShipLevel

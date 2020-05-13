f894bfe974db08621c11ccc084150abcceb2efb0
Adriana Kobylak <anoo@us.ibm.com>
2020-05-13 14:08:14 -0500

    mmc: Update and remove functions for the kernel and rootfs
    Add support for update and remove of eMMC images. The remove is
    needed to avoid leaving kernel files behind. It also prevents
    possible rootfs corruption since the device would be cleared before
    doing an update.
    
    The kernel file (fitImage) is stored in the /boot partition. From
    there u-boot can read it. Store the kernel as kernel-<versionid>.
    A subsequent commit that implements pointing to the updated image
    will create a symlink fitImage -> kernel-<versionid> so that u-boot
    always reads the file 'fitImage'.
    
    The BMC rootfs can be flashed directly to the device. The image-rofs
    is a compressed zstd file:
    https://gerrit.openbmc-project.xyz/c/openbmc/meta-phosphor/+/30781
    
    Tested: Verified the kernel file and non-running device were updated.
    
    Change-Id: Ic983dec1df389d56f11f12dc2e82589d1a2b9dcc
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- mmc/flash.cpp
+++ mmc/flash.cpp
@@ -1,3 +1,5 @@
+#include "config.h"
+
 #include "flash.hpp"
 
 #include "activation.hpp"
@@ -9,14 +11,48 @@ namespace software
 namespace updater
 {
 
+namespace softwareServer = sdbusplus::xyz::openbmc_project::Software::server;
+
 void Activation::flashWrite()
 {
-    // Empty
+    auto method = bus.new_method_call(SYSTEMD_BUSNAME, SYSTEMD_PATH,
+                                      SYSTEMD_INTERFACE, "StartUnit");
+    auto serviceFile = "obmc-flash-mmc@" + versionId + ".service";
+    method.append(serviceFile, "replace");
+    bus.call_noreply(method);
 }
 
-void Activation::onStateChanges(sdbusplus::message::message& /*msg*/)
+void Activation::onStateChanges(sdbusplus::message::message& msg)
 {
-    // Empty
+    uint32_t newStateID{};
+    sdbusplus::message::object_path newStateObjPath;
+    std::string newStateUnit{};
+    std::string newStateResult{};
+
+    // Read the msg and populate each variable
+    msg.read(newStateID, newStateObjPath, newStateUnit, newStateResult);
+
+    auto mmcServiceFile = "obmc-flash-mmc@" + versionId + ".service";
+
+    if (newStateUnit == mmcServiceFile && newStateResult == "done")
+    {
+        roVolumeCreated = true;
+    }
+
+    if (newStateUnit == mmcServiceFile)
+    {
+        if (newStateResult == "failed" || newStateResult == "dependency")
+        {
+            Activation::activation(
+                softwareServer::Activation::Activations::Failed);
+        }
+        else if (roVolumeCreated)
+        {
+            Activation::onFlashWriteSuccess();
+        }
+    }
+
+    return;
 }
 
 } // namespace updater

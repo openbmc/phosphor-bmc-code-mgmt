86013f3918b352e42b8e737dc3c96a1d99bb33c6
Adriana Kobylak <anoo@us.ibm.com>
2020-05-13 12:12:38 -0500

    Add initial support for eMMC layout
    Initial commit to add an mmc layout option to update eMMC chips.
    
    Tested: Compiled with each of the three supported options:
            'meson build -Dbmc-layout=<option>' and verified a build/<option>
            subdir was created.
    
    Change-Id: Idfc9c7f0380daff2d865663dacba23c919386d4f
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

---
+++ mmc/flash.cpp
@@ -0,0 +1,24 @@
+#include "flash.hpp"
+
+#include "activation.hpp"
+
+namespace phosphor
+{
+namespace software
+{
+namespace updater
+{
+
+void Activation::flashWrite()
+{
+    // Empty
+}
+
+void Activation::onStateChanges(sdbusplus::message::message& /*msg*/)
+{
+    // Empty
+}
+
+} // namespace updater
+} // namespace software
+} // namespace phosphor
---
+++ mmc/item_updater_helper.cpp
@@ -0,0 +1,47 @@
+#include "item_updater_helper.hpp"
+
+namespace phosphor
+{
+namespace software
+{
+namespace updater
+{
+
+void Helper::setEntry(const std::string& /* entryId */, uint8_t /* value */)
+{
+    // Empty
+}
+
+void Helper::clearEntry(const std::string& /* entryId */)
+{
+    // Empty
+}
+
+void Helper::cleanup()
+{
+    // Empty
+}
+
+void Helper::factoryReset()
+{
+    // Empty
+}
+
+void Helper::removeVersion(const std::string& /* versionId */)
+{
+    // Empty
+}
+
+void Helper::updateUbootVersionId(const std::string& /* versionId */)
+{
+    // Empty
+}
+
+void Helper::mirrorAlt()
+{
+    // Empty
+}
+
+} // namespace updater
+} // namespace software
+} // namespace phosphor

0c3a3fe6ca4332f611984b169d4fc2e5e50c573c
Adriana Kobylak <anoo@us.ibm.com>
2021-01-20 10:57:05 -0600

    utils: Add execute command
    As issue openbmc/phosphor-bmc-code-mgmt#6 describes, the code
    makes use of the systemd services to execute commands via a
    script, but calls to systemd service files are async, so if
    there is a need to wait for the service to finish executing,
    workarounds like sleep commands have been added to the code, ex:
    https://github.com/openbmc/phosphor-bmc-code-mgmt/commit/60f5ccfd5ab0fc8cedc3a2bf5f5adcab77318b7d
    
    Create a function that would execute a command in a child
    process so that it's possible to wait for it to finish. In
    addition, errors can be more obvious by checking the return
    of the execute function and taking action to notify the caller
    of the error instead of relying on system unit dependencies
    to run recovery actions on error.
    
    Tested: Called new execute function from the code and verified
            it was successful. Ex:
            utils::execute("/bin/mkdir", "/tmp/test-execute");
    
    Change-Id: I81a6aa0a50276abb6aba40196a214629ec9baa13
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- utils.hpp
+++ utils.hpp
@@ -25,4 +25,48 @@ std::string getService(sdbusplus::bus::bus& bus, const std::string& path,
  * @return
  **/
 void mergeFiles(std::vector<std::string>& srcFiles, std::string& dstFile);
+
+namespace internal
+{
+
+/**
+ * @brief Construct an argument vector to be used with an exec command, which
+ *        requires the name of the executable to be the first argument, and a
+ *        null terminator to be the last.
+ * @param[in] name - Name of the executable
+ * @param[in] args - Optional arguments
+ * @return char* vector
+ */
+template <typename... Arg>
+constexpr auto constructArgv(const char* name, Arg&&... args)
+{
+    std::vector<char*> argV{
+        {const_cast<char*>(name), const_cast<char*>(args)..., nullptr}};
+    return argV;
+}
+
+/**
+ * @brief Helper function to execute command in child process
+ * @param[in] path - Fully qualified name of the executable to run
+ * @param[in] args - Optional arguments
+ * @return 0 on success
+ */
+int executeCmd(const char* path, char** args);
+
+} // namespace internal
+
+/**
+ * @brief Execute command in child process
+ * @param[in] path - Fully qualified name of the executable to run
+ * @param[in] args - Optional arguments
+ * @return 0 on success
+ */
+template <typename... Arg>
+int execute(const char* path, Arg&&... args)
+{
+    auto argArray = internal::constructArgv(path, std::forward<Arg>(args)...);
+
+    return internal::executeCmd(path, argArray.data());
+}
+
 } // namespace utils

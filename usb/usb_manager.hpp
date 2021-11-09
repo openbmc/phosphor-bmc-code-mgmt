4f9f65feaa5987fa089d6e41a4dd4997642f546c
George Liu <liuxiwei@inspur.com>
2021-11-09 20:06:31 +0800

    usb: Update the RequestedActivation property
    Subscribe to the add software interface, when it detects one,
    it will call RequestedActivation, in that way when the USB application
    copies the tarball to /tmp/images/ then it gets a signal to start
    updating.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: Ic650e34f8ec61d3c826332d28275db90f9ef348e

--- usb/usb_manager.hpp
+++ usb/usb_manager.hpp
@@ -1,9 +1,16 @@
 #pragma once
 
+#include "utils.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdeventplus/event.hpp>
+
 namespace phosphor
 {
 namespace usb
 {
+
+namespace MatchRules = sdbusplus::bus::match::rules;
 
 class USBManager
 {
@@ -15,14 +22,58 @@ class USBManager
     USBManager& operator=(const USBManager&) = delete;
     USBManager& operator=(USBManager&&) = default;
 
-    explicit USBManager(const char* path) : usbPath(path)
-    {}
+    explicit USBManager(sdeventplus::Event& event, const char* path) :
+        event(event), usbPath(path), isUSBCodeUpdate(false),
+        fwUpdateMatcher(phosphor::usb::utils::DBusHandler::getBus(),
+                        MatchRules::interfacesAdded() +
+                            MatchRules::path("/xyz/openbmc_project/software"),
+                        std::bind(std::mem_fn(&USBManager::updateActivation),
+                                  this, std::placeholders::_1))
+    {
+        if (!run())
+        {
+            lg2::error("Failed to FW Update via USB, usbPath:{USBPATH}",
+                       "USBPATH", usbPath);
+            event.exit(0);
+        }
 
-    bool copyTar(const char* path);
-    bool run();
+        isUSBCodeUpdate = true;
+    }
 
   private:
+    bool copyTar(const char* path);
+    bool run();
+    /** @brief Callback function for USB code update match.
+     *  @details Creates an Activation D-Bus object.
+     *
+     * @param[in]  msg       - Data associated with subscribed signal
+     */
+    void updateActivation(sdbusplus::message::message& msg);
+    void setRequestedActivation(const std::string& path);
+
+  private:
+    /**
+     * @brief sd event handler.
+     */
+    sdeventplus::Event& event;
+
+    /**
+     * @brief The USB path.
+     */
     const char* usbPath;
+
+    /**
+     * @brief Indicates whether USB codeupdate is going on.
+     */
+    bool isUSBCodeUpdate;
+
+    /**
+     * @brief sdbusplus signal match for new image.
+     */
+    sdbusplus::bus::match_t fwUpdateMatcher;
+
+    /** DBusHandler class handles the D-Bus operations */
+    utils::DBusHandler dBusHandler;
 };
 
 } // namespace usb

9832c9651f1d553330599cad853c1895ae95f6dc
George Liu <liuxiwei@inspur.com>
2021-11-10 17:20:04 +0800

    usb: Add disable USB Code Update function
    Verify whether the USB Code Update daemon has been disabled after
    this function is started. exit directly if it has been disabled.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I46c4c73626cee7eefd4555ecaf12efc3fe35503e

--- usb/usb_manager_main.cpp
+++ usb/usb_manager_main.cpp
@@ -4,11 +4,45 @@
 #include <phosphor-logging/lg2.hpp>
 #include <sdeventplus/event.hpp>
 
+bool isEnabled(sdbusplus::bus::bus& bus)
+{
+    constexpr auto OBJECT_PATH =
+        "/xyz/openbmc_project/control/service/phosphor_2dusb_2dcode_2dupdate";
+    constexpr auto OBJECT_INTF =
+        "xyz.openbmc_project.Control.Service.Attributes";
+    constexpr auto PRONAME = "Enabled";
+
+    try
+    {
+        auto value = utils::getProperty(bus, OBJECT_PATH, PRONAME, OBJECT_INTF);
+        const auto& running = std::get<bool>(value);
+
+        return running;
+    }
+    catch (const std::exception& e)
+    {
+        lg2::info(
+            "Failed to get property value, enable is allowed by default: "
+            "OBJECT_PATH = {PATH}, OBJECT_INTF = {INTF}, PRONAME = {PRONAME}",
+            "PATH", OBJECT_PATH, "INTF", OBJECT_INTF, "PRONAME", PRONAME);
+    }
+
+    return true;
+}
+
 int main(int argc, char** argv)
 {
     namespace fs = std::filesystem;
     // Dbus constructs
     auto bus = sdbusplus::bus::new_default();
+
+    // Verify that USB Code Update is disabled
+    if (!isEnabled(bus))
+    {
+        lg2::info("Disabled USB Code Update.");
+
+        return 0;
+    }
 
     // Get a default event loop
     auto event = sdeventplus::Event::get_default();

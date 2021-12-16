a1e4d2a9448e5eb0b1ad54a34e74957ca3d8242a
Lei YU <yulei.sh@bytedance.com>
2021-12-16 16:08:30 +0800

    dual-image: Implement BMC update when running on secondary
    Implement the BMC code update when it's running on the secondary image.
    It will update the alt flash by obmc-flash-bmc-alt@.service and
    wait for the completion.
    After the update is done, it needs reboot to take effect.
    
    Note if the BMC is running on the secondary, it requires a following
    commit to reset the CS to make the SoC to boot from the primary flash.
    
    This commit only flashes the alt image when it's running on the
    secondary image, there will be future commits to support other cases,
    e.g. flashing both images.
    
    Tested: Verify the code update process is successful and it flashes the
            whole primary chip when the BMC is running on the secondary
            chip.
    
    Signed-off-by: Lei YU <yulei.sh@bytedance.com>
    Change-Id: Ifa849e55c28f17b46d7f999ff43a7ad7e73f2ea1

--- static/flash.cpp
+++ static/flash.cpp
@@ -6,11 +6,14 @@
 #include "images.hpp"
 #include "item_updater.hpp"
 
+#include <phosphor-logging/lg2.hpp>
+
 #include <filesystem>
 
 namespace
 {
 constexpr auto PATH_INITRAMFS = "/run/initramfs";
+constexpr auto FLASH_ALT_SERVICE_TMPL = "obmc-flash-bmc-alt@";
 } // namespace
 
 namespace phosphor
@@ -20,11 +23,27 @@ namespace software
 namespace updater
 {
 
+PHOSPHOR_LOG2_USING;
+
 namespace fs = std::filesystem;
 using namespace phosphor::software::image;
 
 void Activation::flashWrite()
 {
+#ifdef BMC_STATIC_DUAL_IMAGE
+    if (parent.runningImageSlot != 0)
+    {
+        // It's running on the secondary chip, update the primary one
+        info("Flashing primary flash from secondary, id: {ID}", "ID",
+             versionId);
+        auto method = bus.new_method_call(SYSTEMD_BUSNAME, SYSTEMD_PATH,
+                                          SYSTEMD_INTERFACE, "StartUnit");
+        auto serviceFile = FLASH_ALT_SERVICE_TMPL + versionId + ".service";
+        method.append(serviceFile, "replace");
+        bus.call_noreply(method);
+        return;
+    }
+#endif
     // For static layout code update, just put images in /run/initramfs.
     // It expects user to trigger a reboot and an updater script will program
     // the image to flash during reboot.
@@ -38,9 +57,33 @@ void Activation::flashWrite()
     }
 }
 
-void Activation::onStateChanges(sdbusplus::message::message& /*msg*/)
+void Activation::onStateChanges(
+    [[maybe_unused]] sdbusplus::message::message& msg)
 {
-    // Empty
+#ifdef BMC_STATIC_DUAL_IMAGE
+    uint32_t newStateID;
+    auto serviceFile = FLASH_ALT_SERVICE_TMPL + versionId + ".service";
+    sdbusplus::message::object_path newStateObjPath;
+    std::string newStateUnit{};
+    std::string newStateResult{};
+    msg.read(newStateID, newStateObjPath, newStateUnit, newStateResult);
+
+    if (newStateUnit != serviceFile)
+    {
+        return;
+    }
+    if (newStateResult == "done")
+    {
+        activationProgress->progress(90);
+        onFlashWriteSuccess();
+    }
+    else
+    {
+        namespace softwareServer =
+            sdbusplus::xyz::openbmc_project::Software::server;
+        Activation::activation(softwareServer::Activation::Activations::Failed);
+    }
+#endif
 }
 
 } // namespace updater

88ba1f9ec712d82e8111952df0524bb460c2acb0
Adriana Kobylak <anoo@us.ibm.com>
2022-03-09 21:14:15 +0000

    updater: Add activations after versions
    When the interfaces added signal is created, it contains all the
    interfaces on that path. That means that when the activations map is
    created, there's a interfaces added signal with the Activation property
    in it. Then if versions is added after that, the interfaces added signal
    will list the new properties such as ExtendedVersion, plus the existing
    ones like Activation. This causes bmcweb to create 2 tasks for the
    update Redfish call because it watches for the Activation property in
    the interfaces added signal.
    
    By adding versions first to d-bus, the Version properties are exposed to
    d-bus first, then adding activations triggers interfaces added with the
    Version properties plus the new Activation ones, so the Activation
    property only appears once.
    
    By coincidence, processBMCImage() inserts versions first, then
    activations. It was likely not done on purpose but works out and there's
    no need to change it there.
    
    Tested: Verified Activation only showed up once in interfaces added with
    busctl monitor and verified only one task
    (GET https://$bmc/redfish/v1/TaskService/Tasks/) was created when
    calling the UpdateService Redfish API.
    
    Change-Id: I3a9e0c109b19ad7ad8d8041404b899a988e14368
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- item_updater.cpp
+++ item_updater.cpp
@@ -138,11 +138,6 @@ void ItemUpdater::createActivation(sdbusplus::message::message& msg)
                                 ACTIVATION_REV_ASSOCIATION, bmcInventoryPath));
         }
 
-        activations.insert(std::make_pair(
-            versionId,
-            std::make_unique<Activation>(bus, path, *this, versionId,
-                                         activationState, associations)));
-
         auto versionPtr = std::make_unique<VersionClass>(
             bus, path, version, purpose, extendedVersion, filePath,
             std::bind(&ItemUpdater::erase, this, std::placeholders::_1),
@@ -151,6 +146,11 @@ void ItemUpdater::createActivation(sdbusplus::message::message& msg)
             std::make_unique<phosphor::software::manager::Delete>(bus, path,
                                                                   *versionPtr);
         versions.insert(std::make_pair(versionId, std::move(versionPtr)));
+
+        activations.insert(std::make_pair(
+            versionId,
+            std::make_unique<Activation>(bus, path, *this, versionId,
+                                         activationState, associations)));
     }
     return;
 }

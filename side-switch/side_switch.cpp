82232e5f4e07a4ddf5990bcceb2301fee5ae3e5b
Andrew Geissler <geissonator@yahoo.com>
2022-03-24 11:04:40 -0500

    side-switch: check priority of running image
    If the running image does not have the highest priority (0), then assume
    a side switch boot is required.
    
    Tested:
    - Running image has priority 0:
    <6> Checking for side switch reboot
    <6> Running firmware version path is /xyz/openbmc_project/software/74575136
    <6> Running firmware version priority is 0
    <6> Side switch not needed
    
    - Running image has priority 1:
    <6> Checking for side switch reboot
    <6> Running firmware version path is /xyz/openbmc_project/software/74575136
    <6> Running firmware version priority is 1
    <6> Running image is at priority 1, execute side switch
    
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>
    Change-Id: Id05f7b7f624059f4e2d7181283577e0cbd10e88f

--- side-switch/side_switch.cpp
+++ side-switch/side_switch.cpp
@@ -1,12 +1,89 @@
 #include "side_switch.hpp"
 
 #include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/bus.hpp>
+
+#include <exception>
+#include <string>
+#include <variant>
+#include <vector>
 
 PHOSPHOR_LOG2_USING;
+
+bool sideSwitchNeeded()
+{
+    auto bus = sdbusplus::bus::new_default();
+    std::string fwRunningVersionPath;
+    uint8_t fwRunningPriority = 0;
+
+    // Get active image
+    try
+    {
+        auto method =
+            bus.new_method_call("xyz.openbmc_project.ObjectMapper",
+                                "/xyz/openbmc_project/software/functional",
+                                "org.freedesktop.DBus.Properties", "Get");
+        method.append("xyz.openbmc_project.Association", "endpoints");
+        std::variant<std::vector<std::string>> paths;
+        auto reply = bus.call(method);
+        reply.read(paths);
+        if (std::get<std::vector<std::string>>(paths).size() != 1)
+        {
+            info("side-switch only supports single image systems");
+            return (false);
+        }
+        fwRunningVersionPath = std::get<std::vector<std::string>>(paths)[0];
+        info("Running firmware version path is {FW_PATH}", "FW_PATH",
+             fwRunningVersionPath);
+    }
+    catch (const std::exception& e)
+    {
+        error("failed to retrieve active firmware version: {ERROR}", "ERROR",
+              e);
+        return (false);
+    }
+
+    // Check if active image has highest priority (0)
+    try
+    {
+        auto method =
+            bus.new_method_call("xyz.openbmc_project.Software.BMC.Updater",
+                                fwRunningVersionPath.c_str(),
+                                "org.freedesktop.DBus.Properties", "Get");
+        method.append("xyz.openbmc_project.Software.RedundancyPriority",
+                      "Priority");
+        std::variant<uint8_t> priority;
+        auto reply = bus.call(method);
+        reply.read(priority);
+        fwRunningPriority = std::get<uint8_t>(priority);
+        info("Running firmware version priority is {FW_PRIORITY}",
+             "FW_PRIORITY", fwRunningPriority);
+    }
+    catch (const std::exception& e)
+    {
+        error("failed to read priority from active image: {ERROR}", "ERROR", e);
+        return (false);
+    }
+
+    if (fwRunningPriority != 0)
+    {
+        info("Running image is at priority {FW_PRIORITY}, execute side switch",
+             "FW_PRIORITY", fwRunningPriority);
+        return (true);
+    }
+
+    return (false);
+}
 
 int main()
 {
     info("Checking for side switch reboot");
 
-    // TODO - Future commits in series to fill in logic
+    if (!sideSwitchNeeded())
+    {
+        info("Side switch not needed");
+        return 0;
+    }
+
+    // TODO - Future commits in series to fill in rest of logic
 }
--- side-switch/side_switch.hpp
+++ side-switch/side_switch.hpp
@@ -1,1 +1,7 @@
 #pragma once
+
+/** @brief Determine if a side switch is needed
+ *
+ *  @return True if side switch needed, false otherwise
+ */
+bool sideSwitchNeeded();

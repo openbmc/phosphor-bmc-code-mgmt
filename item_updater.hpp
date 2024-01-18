41d395cadb66aaf18ad0968c7148e77d2d2c5671
Tim Chao <Tim.Chao@quantatw.com>
2024-01-18 10:59:12 +0800

    item-updater: Implement sequencer version
    This commit create the object which only to provide
    the DBus access so that an external service could get
    the sequencer version.
    
    Use "-Ddual-sequencer=enabled" in phosphor-software-manager
    bbappend to enable dual sequencers support.
    
    Use "-Dsequencer0-version-file-path=/run/sequencer0.version"
    in phosphor-software-manager bbappend to determine the
    sequencer version file path.
    
    Tested:
    ```
    ~# curl -k localhost/redfish/v1/UpdateService/FirmwareInventory/sequencer0
    {
      "@odata.id": "/redfish/v1/UpdateService/FirmwareInventory/sequencer0",
      "@odata.type": "#SoftwareInventory.v1_3_0.SoftwareInventory",
      "Description": "Other image",
      "Id": "sequencer0",
      "Name": "Software Inventory",
      "RelatedItem": [],
      "RelatedItem@odata.count": 0,
      "Status": {
        "Health": "OK",
        "HealthRollup": "OK",
        "State": "Enabled"
      },
      "Updateable": false,
      "Version": "0.1.0"
    }
    ```
    
    Change-Id: I5caf37b17402bf2f26ce07699f19aa310e5b837a
    Signed-off-by: Tim Chao <Tim.Chao@quantatw.com>

--- item_updater.hpp
+++ item_updater.hpp
@@ -65,6 +65,12 @@ class ItemUpdater : public ItemUpdaterInherit
         setBMCInventoryPath();
         processBMCImage();
         restoreFieldModeStatus();
+        createSequencerObject(SEQUENCER0_RELEASE_FILE, seq0Activations,
+                              seq0Version);
+#ifdef DUAL_SEQUENCER
+        createSequencerObject(SEQUENCER1_RELEASE_FILE, seq1Activations,
+                             seq1Version);
+#endif
 #ifdef HOST_BIOS_UPGRADE
         createBIOSObject();
 #endif
@@ -278,6 +284,17 @@ class ItemUpdater : public ItemUpdaterInherit
   public:
     /** @brief Persistent Version D-Bus object for BIOS */
     std::unique_ptr<VersionClass> biosVersion;
+#endif
+
+    /** @brief Create the Sequencer object */
+    void createSequencerObject(const std::string& sequencerPath,
+                               std::unique_ptr<Activation>& seqActivations,
+                               std::unique_ptr<VersionClass>& seqVersion);
+    std::unique_ptr<Activation> seq0Activations;
+    std::unique_ptr<VersionClass> seq0Version;
+#ifdef DUAL_SEQUENCER
+    std::unique_ptr<Activation> seq1Activations;
+    std::unique_ptr<VersionClass> seq1Version;
 #endif
 
     /** @brief Get the slot number of running image */

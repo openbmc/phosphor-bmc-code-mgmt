d5c50f7b5428c9d5513d82ea6295b75a282cf5e6
Jagpal Singh Gill <paligill@gmail.com>
2024-04-07 23:56:48 -0700

    start-update-interface: implement software manager
    Implement the software manager module for the new daemon inheriting from
    the Update D-Bus interface. Link with new interface APIs from
    item_updater and software utils. New daemon will continue to exist at
    runtime alongwith old flow, until old flow is deprecated and removed.
    This change is based on -
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/65738
    https://gerrit.openbmc.org/c/openbmc/docs/+/65739
    
    Change-Id: I0ecbbb8fc5340de7f66f8870ae389b405a2debee
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ software_manager.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "config.h"
+
+#include "item_updater.hpp"
+
+#include <sdbusplus/async.hpp>
+#include <xyz/openbmc_project/Software/Update/server.hpp>
+
+#include <random>
+#include <string>
+#include <tuple>
+
+namespace phosphor::software::manager
+{
+
+using UpdateIntf = sdbusplus::xyz::openbmc_project::Software::server::Update;
+using ItemUpdaterIntf = phosphor::software::updater::ItemUpdater;
+using ActivationIntf = phosphor::software::updater::Activation;
+using ApplyTimeIntf =
+    sdbusplus::common::xyz::openbmc_project::software::ApplyTime;
+
+/** @class Manager
+ *  @brief Processes the image file from update D-Bus interface.
+ *  @details The software manager class handles software updates and manages
+ * software info through version and activation objects.
+ */
+class Manager : public UpdateIntf
+{
+  public:
+    /** @brief Constructs Manager Class
+     *
+     * @param[in] bus - The Dbus bus object
+     */
+    explicit Manager(sdbusplus::async::context& ctx, std::string& path) :
+        UpdateIntf(ctx.get_bus(), path.c_str()), ctx(ctx),
+        itemUpdater(std::make_unique<ItemUpdaterIntf>(ctx, path, true))
+    {}
+
+  private:
+    /** @brief Implementation for StartUpdate
+     *  Start a firware update to be performed asynchronously.
+     */
+    virtual sdbusplus::message::object_path
+        startUpdate(sdbusplus::message::unix_fd image,
+                    ApplyTimeIntf::RequestedApplyTimes applyTime) override;
+
+    /* @brief Process the image supplied via image fd */
+    auto processImage(sdbusplus::message::unix_fd image,
+                      ApplyTimeIntf::RequestedApplyTimes applyTime,
+                      std::string id, std::string objPath)
+        -> sdbusplus::async::task<>;
+
+    /* @brief The handler for the image processing failure  */
+    void processImageFailed(sdbusplus::message::unix_fd image, std::string& id);
+
+    /** @brief The random generator for the software id */
+    std::mt19937 randomGen{static_cast<unsigned>(
+        std::chrono::system_clock::now().time_since_epoch().count())};
+
+    /** @brief D-Bus context */
+    sdbusplus::async::context& ctx;
+    /** @brief item_updater object */
+    std::unique_ptr<ItemUpdaterIntf> itemUpdater;
+    /** @brief State whether update is in progress */
+    bool updateInProgress = false;
+};
+
+} // namespace phosphor::software::manager

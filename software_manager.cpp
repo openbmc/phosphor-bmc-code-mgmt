41a23f9a756bc658ad32381341395bcd18ba35fb
Jagpal Singh Gill <paligill@gmail.com>
2024-06-23 15:20:35 -0700

    fix for phoshphor-software-manager start issue
    Since, the earlier fix adds an empty app, it keeps on restarting because
    it exits soon. Add logic for ctx run so app doesn't exit.
    
    Change-Id: I0783af4121cd84044b2800742decef9634f2032f
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

--- software_manager.cpp
+++ software_manager.cpp
@@ -1,9 +1,23 @@
+#include "config.h"
+
 #include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async.hpp>
+#include <xyz/openbmc_project/Software/Update/server.hpp>
 
 PHOSPHOR_LOG2_USING;
 
 int main()
 {
     info("Creating Software Manager");
+
+    auto path = std::string(SOFTWARE_OBJPATH) + "/bmc";
+    sdbusplus::async::context ctx;
+    sdbusplus::server::manager_t manager{ctx, path.c_str()};
+    constexpr auto serviceName = "xyz.openbmc_project.Software.Manager";
+
+    ctx.request_name(serviceName);
+
+    ctx.run();
+
     return 0;
 }

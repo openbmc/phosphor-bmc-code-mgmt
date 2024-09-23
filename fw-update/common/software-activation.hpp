983a66de32022d6b00cde5a0bf8d4719009da86e
Alexander Hansen <alexander.hansen@9elements.com>
2024-09-23 17:46:16 +0200

    new fw update daemons for bios and vr
    new daemons to implement the flow as described in
    https://github.com/openbmc/docs/blob/master/designs/code-update.md
    
    - component/bios-spi/ : spi device bios update daemon
    - common/             : common code folder
                            - pldm image verification logic
                            - common update flow
                            - base class for the device specific
                              update daemons
    
    The new daemons are all following the generic template of Code Updater
    daemon as outlined in the design.
    
    The idea is that they are separate daemons (per device, as outlined in
    the design) but share all the code that's not device specific.
    
    Change-Id: If2438b8506aceb8c5313ec13a0bf7cb68f3cc279
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ fw-update/common/software-activation.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <boost/asio/steady_timer.hpp>
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <xyz/openbmc_project/Software/Activation/aserver.hpp>
+
+class SoftwareActivation :
+    public sdbusplus::aserver::xyz::openbmc_project::software::Activation<
+        SoftwareActivation>
+{
+  public:
+    SoftwareActivation(sdbusplus::async::context& ctx, const char* objPath);
+
+    sdbusplus::server::manager_t manager;
+};

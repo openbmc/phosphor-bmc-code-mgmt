63b1ad29e6232c9de250325cff5ce0c1cd112a62
Alexander Hansen <alexander.hansen@9elements.com>
2024-09-23 17:46:16 +0200

    new fw update daemons for bios and vr
    new daemons to implement the flow as described in
    https://github.com/openbmc/docs/blob/master/designs/code-update.md
    
    - component/bios-spi/
          spi device bios update daemon
    
    - component/pldm-update-pkg-tool/
          tool for create / view package
    
    - common/
          common code folder
          - pldm image verification logic
          - common update flow
          - base class for the device specific update daemons
    
    The new daemons are all following the generic template of Code Updater
    daemon as outlined in the design.
    
    The idea is that they are separate daemons (per device, as outlined in
    the design) but share all the code that's not device specific.
    
    Change-Id: If2438b8506aceb8c5313ec13a0bf7cb68f3cc279
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ fw-update/component/bios-spi/spi_device_code_updater.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "fw-update/common/fw_manager.hpp"
+#include "sdbusplus/async/context.hpp"
+
+#include <boost/asio/steady_timer.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/server.hpp>
+#include <xyz/openbmc_project/Software/Activation/server.hpp>
+#include <xyz/openbmc_project/Software/Update/server.hpp>
+#include <xyz/openbmc_project/Software/Version/server.hpp>
+
+class SPIDeviceCodeUpdater : public FWManager
+{
+  public:
+    SPIDeviceCodeUpdater(sdbusplus::async::context& io, bool isDryRun);
+
+    // override code updater functions
+
+    // misc
+    auto getInitialConfiguration() -> sdbusplus::async::task<>;
+    void getInitialConfigurationSingleDevice(
+        const std::string& service, const std::string& path, size_t ngpios);
+
+    std::string getConfigurationInterface() override;
+
+  private:
+};

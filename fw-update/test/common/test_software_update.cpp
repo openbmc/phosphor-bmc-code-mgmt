62942b8d50f6c4ef441dd07a69f81af3af542aaf
Alexander Hansen <alexander.hansen@9elements.com>
2024-09-23 17:46:16 +0200

    fw update: common code
    new daemons to implement the flow as described in
    https://github.com/openbmc/docs/blob/master/designs/code-update.md
    
    - common/
          common code folder
          - common update flow
          - base class for the device specific update daemons
    
    The new daemons are all following the generic template of Code Updater
    daemon as outlined in the design.
    
    The idea is that they are separate daemons (per device, as outlined in
    the design) but share all the code that's not device specific.
    
    Change-Id: If2438b8506aceb8c5313ec13a0bf7cb68f3cc279
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ fw-update/test/common/test_software_update.cpp
@@ -0,0 +1,118 @@
+
+#include "fw-update/tool/create_package/create_pldm_fw_package.hpp"
+#include "nopdevice/nopdevice.hpp"
+
+#include <fcntl.h>
+#include <inttypes.h>
+#include <unistd.h>
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async/context.hpp>
+#include <xyz/openbmc_project/Software/Update/client.hpp>
+#include <xyz/openbmc_project/Software/Version/client.hpp>
+
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+
+#include <gtest/gtest.h>
+
+// NOLINTBEGIN
+sdbusplus::async::task<>
+    testSoftwareUpdateSuccess(sdbusplus::async::context& ctx, int fd)
+// NOLINTEND
+{
+    NopCodeUpdater nopcu(ctx);
+    NopCodeUpdater* cu = &nopcu;
+
+    nopcu.setupBusName();
+
+    auto device = std::make_unique<NopDevice>(ctx, cu);
+
+    device->softwareCurrent =
+        std::make_unique<Software>(ctx, "/", "v12.345", *device);
+
+    std::unique_ptr<SoftwareActivationProgress> sap =
+        std::make_unique<SoftwareActivationProgress>(ctx, "/");
+
+    const auto applyTimeImmediate = sdbusplus::common::xyz::openbmc_project::
+        software::ApplyTime::RequestedApplyTimes::Immediate;
+
+    device->softwareCurrent->enableUpdate({applyTimeImmediate});
+
+    const std::string service = "xyz.openbmc_project.Software.Nop";
+
+    std::string objPathCurrentSoftware =
+        device->softwareCurrent->getObjectPath();
+
+    // go via dbus to call the dbus method to start the update
+    auto client =
+        sdbusplus::client::xyz::openbmc_project::software::Update<>(ctx)
+            .service(service)
+            .path(objPathCurrentSoftware);
+
+    sdbusplus::message::object_path objPathNewSoftware =
+        co_await client.start_update(fd, applyTimeImmediate);
+
+    // TODO: somehow remove this sleep
+    co_await sdbusplus::async::sleep_for(ctx, std::chrono::seconds(2));
+
+    assert(objPathNewSoftware != objPathCurrentSoftware);
+
+    // assert that update function was called
+    assert(device->deviceSpecificUpdateFunctionCalled);
+
+    auto clientNewVersion =
+        sdbusplus::client::xyz::openbmc_project::software::Version<>(ctx)
+            .service(service)
+            .path(objPathNewSoftware.str);
+
+    const std::string newVersion = co_await clientNewVersion.version();
+
+    // assert the new version is not the old version
+    assert(newVersion != "v12.345");
+    assert(newVersion == "VersionString1");
+
+    ctx.request_stop();
+
+    co_return;
+}
+
+TEST(SoftwareUpdate, TestSoftwareUpdate)
+{
+    sdbusplus::async::context ctx;
+
+    int fd = memfd_create("test_memfd", 0);
+
+    assert(fd >= 0);
+
+    lg2::debug("create fd {FD}", "FD", fd);
+
+    uint8_t component_image[] = {0x12, 0x34, 0x83, 0x21};
+
+    size_t size_out = 0;
+    std::shared_ptr<uint8_t[]> buf = create_pldm_package_buffer(
+        component_image, sizeof(component_image),
+        std::optional<uint32_t>(exampleVendorIANA),
+        std::optional<std::string>(exampleCompatible), &size_out);
+
+    ssize_t bytes_written = write(fd, (void*)buf.get(), size_out);
+    if (bytes_written == -1)
+    {
+        std::cerr << "Failed to write to memfd: " << strerror(errno)
+                  << std::endl;
+        close(fd);
+        assert(false);
+    }
+    if (lseek(fd, 0, SEEK_SET) != 0)
+    {
+        lg2::error("could not seek to the beginning of the file");
+        assert(false);
+    }
+
+    ctx.spawn(testSoftwareUpdateSuccess(ctx, fd));
+
+    ctx.run();
+
+    close(fd);
+}

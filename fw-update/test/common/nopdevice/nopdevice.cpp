8c842bae4b4f9cb7f90eca3ac50f5a17c9d5a84c
Alexander Hansen <alexander.hansen@9elements.com>
2024-09-23 17:46:16 +0200

    fw update: common code
    new daemons to implement the flow as described in
    https://github.com/openbmc/docs/blob/master/designs/code-update.md
    
    - common/
          common code folder
          - common update flow
          - base class for the device specific update daemons
    
    The new daemons are all following the generic template of Code Updater
    daemon as outlined in the design.
    
    The idea is that they are separate daemons (per device, as outlined in
    the design) but share all the code that's not device specific.
    
    Change-Id: If2438b8506aceb8c5313ec13a0bf7cb68f3cc279
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ fw-update/test/common/nopdevice/nopdevice.cpp
@@ -0,0 +1,100 @@
+#include "nopdevice.hpp"
+
+#include "fw-update/common/include/device.hpp"
+#include "fw-update/common/include/device_config.hpp"
+#include "fw-update/common/include/software_manager.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/async.hpp>
+#include <sdbusplus/server.hpp>
+#include <xyz/openbmc_project/Association/Definitions/server.hpp>
+#include <xyz/openbmc_project/Software/Update/server.hpp>
+
+#include <memory>
+
+static long getRandomId()
+{
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    unsigned int seed = ts.tv_nsec ^ getpid();
+    srandom(seed);
+    return random() % 10000;
+}
+
+// nop code updater needs unique suffix on dbus for parallel unit testing
+NopCodeUpdater::NopCodeUpdater(sdbusplus::async::context& ctx) :
+    NopCodeUpdater(ctx, getRandomId())
+{}
+
+NopCodeUpdater::NopCodeUpdater(sdbusplus::async::context& ctx,
+                               long uniqueSuffix) :
+    SoftwareManager(ctx, "Nop" + std::to_string(uniqueSuffix), true)
+{}
+
+std::set<std::string> NopCodeUpdater::getEMConfigTypes()
+{
+    return {"Nop"};
+}
+
+// NOLINTBEGIN
+sdbusplus::async::task<> NopCodeUpdater::getInitialConfigurationSingleDevice(
+    const std::string& /*unused*/, DeviceConfig& /*unused*/)
+// NOLINTEND
+{
+    co_return;
+}
+
+NopDevice::NopDevice(sdbusplus::async::context& ctx, const DeviceConfig& config,
+                     SoftwareManager* parent) :
+    Device(ctx, true, config, parent)
+{}
+NopDevice::NopDevice(sdbusplus::async::context& ctx, SoftwareManager* parent) :
+    Device(ctx, true,
+           DeviceConfig(exampleVendorIANA, exampleCompatible, "Nop",
+                        exampleConfigObjPath, exampleName),
+           parent)
+{}
+
+// NOLINTBEGIN
+sdbusplus::async::task<bool> NopDevice::deviceSpecificUpdateFunction(
+    const uint8_t* /*unused*/, size_t compImageSize,
+    std::unique_ptr<SoftwareActivationProgress>& activationProgress)
+// NOLINTEND
+{
+    lg2::debug(
+        "[nop device] called device specific update function with image size {SIZE}",
+        "SIZE", compImageSize);
+
+    deviceSpecificUpdateFunctionCalled = true;
+
+    // Setting this property for demonstration purpose.
+    // For a real device, this could represent the
+    // percentage completion of writing the firmware,
+    // and any progress made in the update process within this function.
+    activationProgress->progress(30);
+
+    // There is no hard constraint on the values here,
+    // we do not have to reach any specific percentage.
+    // The percentage should be monotonic and increasing.
+    activationProgress->progress(90);
+
+    co_return true;
+}
+
+std::string NopDevice::getObjPathConfig()
+{
+    return this->config.objPathConfig;
+}
+
+// NOLINTBEGIN
+sdbusplus::async::task<std::string> NopDevice::getInventoryItemObjectPath()
+// NOLINTEND
+{
+    // for a real device, it could find its inventory item via associations and
+    // the configuration interface, or go via the mapper to find the
+    // specific inventory item interface and then get the path from there.
+
+    co_return "/xyz/openbmc_project/inventory/item/my_nop_item";
+}
---
+++ fw-update/test/common/test_device_config.cpp
@@ -0,0 +1,66 @@
+
+#include "fw-update/common/include/device_config.hpp"
+
+#include <fcntl.h>
+#include <inttypes.h>
+#include <unistd.h>
+
+#include <phosphor-logging/lg2.hpp>
+
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+
+#include <gtest/gtest.h>
+
+TEST(DeviceConfig, success)
+{
+    DeviceConfig config(0x0324, "com.example.SampleCorp", "ConfigType",
+                        "/xyz/openbmc_project/inventory/system/s1/mycomponent",
+                        "SampleComponent");
+
+    ASSERT_EQ(config.vendorIANA, 0x0324);
+    ASSERT_EQ(config.compatible, "com.example.SampleCorp");
+    ASSERT_EQ(config.objPathConfig,
+              "/xyz/openbmc_project/inventory/system/s1/mycomponent");
+}
+
+TEST(DeviceConfig, failureCompatibleNoDot)
+{
+    try
+    {
+        DeviceConfig config(
+            0x0324, "comexamplesamplecorp", "ConfigType",
+            "/xyz/openbmc_project/inventory/system/s1/mycomponent",
+            "SampleComponent");
+        ASSERT_FALSE(true);
+    }
+    catch (std::exception& /*unused*/)
+    {}
+}
+
+TEST(DeviceConfig, failureCompatibleInvalidChar)
+{
+    try
+    {
+        DeviceConfig config(
+            0x0324, "com-examplesamplecorp#", "ConfigType",
+            "/xyz/openbmc_project/inventory/system/s1/mycomponent",
+            "SampleComponent");
+        ASSERT_FALSE(true);
+    }
+    catch (std::exception& /*unused*/)
+    {}
+}
+
+TEST(DeviceConfig, failureObjPath)
+{
+    try
+    {
+        DeviceConfig config(0x0324, "com.example.samplecorp", "ConfigType",
+                            "noslash", "ExampleComponent");
+        ASSERT_FALSE(true);
+    }
+    catch (std::exception& /*unused*/)
+    {}
+}

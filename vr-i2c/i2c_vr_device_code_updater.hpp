84c3f268f9e0cfd1616b164b1f3aad90b70e0e91
Christopher Meis <christopher.meis@9elements.com>
2024-10-22 09:36:41 +0200

    Add i2c voltage regulator update specific classes
    - component/vr_i2c : add voltage regulator update process
    
    The configuration of a voltage regulator relies on the EM-Schema in [1]
    
    1: https://gerrit.openbmc.org/c/openbmc/entity-manager/+/77326
    
    Change-Id: I2e11a6c10ae40ed7719ceb86dfd6a38dd5b27017
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

---
+++ vr-i2c/i2c_vr_device_code_updater.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "common/include/software_manager.hpp"
+#include "sdbusplus/async/context.hpp"
+
+#include <sdbusplus/async.hpp>
+
+class I2CVRDeviceCodeUpdater : public SoftwareManager
+{
+  public:
+    I2CVRDeviceCodeUpdater(sdbusplus::async::context& ctx, bool isDryRun);
+
+    std::set<std::string> getEMConfigTypes() override;
+
+    sdbusplus::async::task<> getInitialConfigurationSingleDevice(
+        const std::string& service, DeviceConfig& config) final;
+
+  private:
+};

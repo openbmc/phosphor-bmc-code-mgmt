c4bdc329355e8c60422be47e32f2e4eb6ff465b9
Christopher Meis <christopher.meis@9elements.com>
2024-10-22 09:36:41 +0200

    Add i2c voltage regulator updater
    - /i2c-vr: Classes following the design in [1]
    - /i2c-vr/vr.cpp, vr.hpp: General representation of a voltage regulators
      communication interface
    - /vr-i2c/xdpe1x2xx: Support for Infineon XDPE1x2xx class of voltage
      regulators
    - /common/i2c/: Basic I2C communication interface
    
    The configuration of a voltage regulator relies on the EM-Schema in [2]
    
    1: https://github.com/openbmc/docs/blob/master/designs/code-update.md
    2: https://gerrit.openbmc.org/c/openbmc/entity-manager/+/77326
    
    Change-Id: I2e11a6c10ae40ed7719ceb86dfd6a38dd5b27017
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

---
+++ i2c-vr/vr.cpp
@@ -0,0 +1,31 @@
+#include "vr.hpp"
+
+#include "xdpe1x2xx/xdpe1x2xx.hpp"
+
+#include <map>
+
+
+std::unique_ptr<VoltageRegulator> create(enum VRType vrType, uint8_t bus,
+                                         uint8_t address)
+{
+    std::unique_ptr<phosphor::i2c::I2C> inf = std::make_unique<phosphor::i2c::I2C>(bus, address);
+    std::unique_ptr<VoltageRegulator> ret;
+    switch (vrType)
+    {
+        case VRType::XDPE1X2XX:
+            ret = std::make_unique<XDPE152XX>(std::move(inf));
+            break;
+        default:
+            return NULL;
+    }
+    return ret;
+}
+
+enum VRType stringToEnum(std::string& vrType)
+{
+    std::map<std::string, enum VRType> VRTypeToString{
+        {"XDPE1X2XX", VRType::XDPE1X2XX},
+    };
+    return VRTypeToString[vrType];
+}
+

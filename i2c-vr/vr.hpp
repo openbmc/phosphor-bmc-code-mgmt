363f666ea35bc1b30b6d3858150488525111eb10
Christopher Meis <christopher.meis@9elements.com>
2024-10-22 09:36:41 +0200

    Add i2c voltage regulator updater
    - /i2c-vr: Classes following the design in [1]
    - /i2c-vr/vr.cpp, vr.hpp: General representation of a voltage regulators
      communication interface
    - /vr-i2c/xdpe1x2xx: Support for Infineon XDPE1x2xx class of voltage
      regulators
    - /common/i2c/: Basic I2C communication interface
    
    The configuration of a voltage regulator relies on the EM-Schema in [2]
    
    1: https://github.com/openbmc/docs/blob/master/designs/code-update.md
    2: https://gerrit.openbmc.org/c/openbmc/entity-manager/+/77463
    
    Change-Id: I2e11a6c10ae40ed7719ceb86dfd6a38dd5b27017
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

---
+++ i2c-vr/vr.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <string>
+
+#include <sdbusplus/async.hpp>
+
+namespace SDBusAsync = sdbusplus::async;
+
+namespace phosphor::software::VR
+{
+
+enum class VRType
+{
+    XDPE1X2XX,
+};
+
+class VoltageRegulator
+{
+  public:
+    explicit VoltageRegulator(SDBusAsync::context& ctx) : ctx(ctx){}
+    virtual ~VoltageRegulator() = default;
+
+    // @brief Parses the firmware image into the configuration structure
+    //        and verifies its correctness.
+    // @return sdbusplus::async::task<bool> true indicates success.
+    virtual SDBusAsync::task<bool> verifyImage(const uint8_t* image, size_t imageSize) = 0;
+
+    // @brief Applies update to the voltage regulator
+    // @return sdbusplus::async::task<bool> true indicates success.
+    virtual SDBusAsync::task<bool> updateFirmware(bool force) = 0;
+
+    // @brief resets the voltage regulator for the update to take effect.
+    // @return sdbusplus::async::task<bool> true indicates success.
+    virtual SDBusAsync::task<bool> reset() = 0;
+
+    // @brief Requests the CRC value of the voltage regulator over I2C.
+    // @param pointer to write the result to.
+    // @returns < 0 on error
+    virtual bool getCRC(uint32_t* checksum) = 0;
+
+  protected:
+    SDBusAsync::context& ctx;
+};
+
+std::unique_ptr<VoltageRegulator> create(SDBusAsync::context& ctx, enum VRType vrType, uint8_t bus,
+                                         uint8_t address);
+
+bool stringToEnum(std::string& vrStr, VRType& vrType);
+
+}

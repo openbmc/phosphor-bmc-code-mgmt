d78129d388c7822fa018dab51444eff50bfdf507
Christopher Meis <christopher.meis@9elements.com>
2024-10-22 09:36:41 +0200

    Add i2c voltage regulator update specific classes
    - component/vr_i2c : i2c device fw update daemon
    
    Change-Id: I2e11a6c10ae40ed7719ceb86dfd6a38dd5b27017
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

---
+++ fw-update/component/vr-i2c/i2c_vr_device.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "fw-update/common/device.hpp"
+#include "fw-update/common/fw_manager.hpp"
+
+#include <boost/asio/steady_timer.hpp>
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/async/context.hpp>
+#include <sdbusplus/timer.hpp>
+#include <xyz/openbmc_project/Association/Definitions/server.hpp>
+#include <xyz/openbmc_project/Software/Activation/server.hpp>
+#include <xyz/openbmc_project/Software/Update/server.hpp>
+#include <xyz/openbmc_project/Software/Version/server.hpp>
+
+#include <string>
+
+class Software;
+
+class I2CVRDevice : public Device
+{
+  public:
+    I2CVRDevice(sdbusplus::async::context& io, bool dryRun, const uint32_t& bus,
+                const uint32_t& address, const std::string& vendorIANA,
+                const std::string& compatible, FWManager* parent);
+
+    sdbusplus::async::task<bool> startUpdateAsync() final;
+
+    sdbusplus::async::task<bool> deviceSpecificUpdateFunction(
+        sdbusplus::message::unix_fd image,
+        std::shared_ptr<SoftwareActivationProgress> activationProgress) final;
+
+  private:
+    uint32_t busNo;
+    uint32_t address;
+
+    sdbusplus::async::task<bool> writeUpdate(
+        sdbusplus::message::unix_fd image,
+        const std::shared_ptr<SoftwareActivationProgress>& activationProgreess);
+
+    // Does the host need to be powered off?
+    // Do we need function handling that?
+};

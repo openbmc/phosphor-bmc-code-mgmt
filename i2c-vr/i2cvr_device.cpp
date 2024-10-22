abb6e11d4e8b7aba8d9466688de9357ac17fae44
Christopher Meis <christopher.meis@9elements.com>
2024-10-22 09:36:41 +0200

    Add i2c voltage regulator updater
    - /i2c-vr: Classes following the design in [1]
    - /i2c-vr/vr.cpp, vr.hpp: General representation of a voltage regulators
      communication interface
    - /vr-i2c/xdpe1x2xx: Support for Infineon XDPE1x2xx class of voltage
      regulators
    - /common/i2c/: Basic I2C communication interface
    
    The configuration of a voltage regulator relies on the EM-Schema in [2]
    
    1: https://github.com/openbmc/docs/blob/master/designs/code-update.md
    2: https://gerrit.openbmc.org/c/openbmc/entity-manager/+/77463
    
    Change-Id: I2e11a6c10ae40ed7719ceb86dfd6a38dd5b27017
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

---
+++ i2c-vr/i2cvr_device.cpp
@@ -0,0 +1,43 @@
+#include "i2cvr_device.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async.hpp>
+#include <sdbusplus/async/context.hpp>
+
+namespace phosphor::software::i2c_vr::device{
+
+// NOLINTBEGIN(readability-static-accessed-through-instance)
+sdbusplus::async::task<bool> I2CVRDevice::updateDevice(
+    const uint8_t* image, size_t imageSize)
+// NOLINTEND(readability-static-accessed-through-instance)
+{
+
+    auto ret = co_await vrInterface->verifyImage(image, imageSize);
+    if (!ret)
+    {
+        co_return false;
+    }
+
+    // setUpdateProgress unused. Working on a solution to pass it through to the update function.
+    ret = co_await vrInterface->updateFirmware(false);
+    if (!ret)
+    {
+        co_return false;
+    }
+
+    ret = co_await vrInterface->reset();
+    if (!ret)
+    {
+        co_return false;
+    }
+
+    lg2::info("Successfully updated VR {NAME}", "NAME", config.configName);
+    co_return true;
+}
+
+int I2CVRDevice::getVersion(uint32_t* sum)
+{
+    return this->vrInterface->getCRC(sum);
+}
+
+}

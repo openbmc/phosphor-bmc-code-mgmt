786b4e64a8f9e3ea4429338c6ba37cd945d6c4f7
Christopher Meis <christopher.meis@9elements.com>
2024-11-05 06:59:05 +0100

    Add communication interface for updating voltage regulators
    Change-Id: Ie3476e7e364659c08c0f7369a6dc5def6487f31a
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

---
+++ fw-update/component/vr-i2c/regulators/main.cpp
@@ -0,0 +1,32 @@
+#include "vr.hpp"
+
+int main(int argc, char* argv[])
+{
+    uint8_t bus = 0;
+    uint8_t addr = 0;
+    std::string type = "XDPE152C4";
+
+    for (int i = 1; i < argc; i++)
+    {
+        std::string arg = std::string(argv[i]);
+        if (arg == "--type" && i < argc - 1)
+        {
+            type = std::string(argv[i + 1]);
+        }
+        if (arg == "--bus")
+        {
+            bus = std::stoul(std::string(argv[i + 1], 2, 10));
+        }
+        if (arg == "--addr")
+        {
+            addr = std::stoul(std::string(argv[i + 1], 2, 10));
+        }
+    }
+    std::unique_ptr<VR::VoltageRegulator> dev = VR::create(type, bus, addr);
+
+    std::cout << "Bus: " << bus << std::endl;
+    std::cout << "Address: " << addr << std::endl;
+    std::cout << "Type: " << type << std::endl;
+
+    return 0;
+}

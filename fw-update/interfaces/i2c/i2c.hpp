9f7beb7c628897fdb45aa4bd054bdbe9a40e53ab
Christopher Meis <christopher.meis@9elements.com>
2024-11-05 06:57:42 +0100

    Add I2C hardware interface
    Change-Id: I49f4c1f854b55915247b196905e92a5ff4a26605
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

---
+++ fw-update/interfaces/i2c/i2c.hpp
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <cstdint>
+#include <cstring>
+#include <exception>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <memory>
+
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+
+extern "C"
+{
+#include <i2c/smbus.h>
+#include <linux/i2c-dev.h>
+#include <linux/i2c.h>
+}
+
+namespace i2c
+{
+
+    class I2CException : public std::exception
+    {
+        public:
+            explicit I2CException(const std::string& info, const std::string& bus,
+                          uint8_t addr, int errorCode = 0) :
+            bus(bus), addr(addr), errorCode(errorCode)
+            {
+                std::stringstream ss;
+                    ss << "I2CException: " << info << ": bus " << bus << ", addr 0x"
+                        << std::hex << static_cast<int>(addr);
+                if (errorCode != 0)
+                {
+                    ss << std::dec << ", errno " << errorCode << ": "
+                    << strerror(errorCode);
+                }
+                errStr = ss.str();
+            }
+
+            virtual ~I2CException() = default;
+
+            const char* what() const noexcept override
+            {
+                return errStr.c_str();
+            }
+            std::string bus;
+            uint8_t addr;
+            int errorCode;
+            std::string errStr;
+    };
+
+    class I2C
+    {
+        private:
+            static constexpr int INVALID_FD = -1;
+            int fd = INVALID_FD;
+            std::string busStr;
+            uint8_t device_address;
+            int max_retries;
+
+            void open();
+
+            int checkIsOpen() const
+            {
+                if (!isOpen())
+                {
+                    return -1;
+                }
+                return 0;
+            }
+
+
+            void closeWithoutException() noexcept
+            {
+                try
+                {
+                    close();
+                }
+                catch (...)
+                {}
+            }
+
+        public:
+            enum class InitialState
+            {
+                OPEN,
+                CLOSE
+            };
+
+            explicit I2C(uint8_t bus, uint8_t address,
+                InitialState initialState, int retries ) :
+                device_address(address),
+                max_retries(retries),
+                busStr("/dev/i2c-" + std::to_string(bus))
+            {
+                if (initialState == InitialState::OPEN)
+                {
+                    open();
+                }
+            }
+
+            void send_receive(uint8_t addr, uint8_t* writeData,
+                    uint8_t writeSize, uint8_t* readData, uint8_t& readSize) const;
+
+            bool isOpen() const
+            {
+                return (fd != INVALID_FD);
+            }
+
+            void close();
+
+    }; // end class I2C
+
+
+    std::unique_ptr<I2C> create(uint8_t bus, uint8_t addr);
+
+} // end namespace i2c

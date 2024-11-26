4a053c8e069133436d490aaff12471ee6c9d0677
Alexander Hansen <alexander.hansen@9elements.com>
2024-11-26 17:02:39 +0100

    fw update: library to create pldm fw package
    Implement some helper functions to easily create a pldm fw package in a
    few lines of user code.
    
    This is helpful for unit testing the code update flow.
    
    The package created is for testing purpose. The library is not
    fully-featured or complete and should not be relied upon for anything
    other than the tests in this repo.
    
    https://github.com/openbmc/docs/blob/master/designs/code-update.md
    
    Tested: next few patches in series
    
    Change-Id: If899e5537a2e5ac641544ca80e8876d83549da28
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ test/create_package/component_image_info_area.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <unistd.h>
+
+#include <cstdint>
+
+/*
+ * componentLocationOffsetIndex  is for backfilling by the caller
+ */
+ssize_t create_pldm_component_image_info_area_v1_0_0(
+    uint8_t* b, ssize_t i, size_t component_image_size,
+    size_t& componentLocationOffsetIndex);

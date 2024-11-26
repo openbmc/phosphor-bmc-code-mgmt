4f694123d2139fc679bb5cd33062ffa6ae992ee9
Alexander Hansen <alexander.hansen@9elements.com>
2024-11-26 11:16:44 +0100

    fw update: spi device code updater
    - component/bios-spi/*
    
    This code updater is for updating spi flash devices.
    It can for example update the host firmware on different server boards
    and has following features:
    
    - power down the host before update
    - set mux gpios to access spi flash
    - (very limited) communication with ME (Management Engine)
    - use flashrom to utilize fw with IFD (Intel Flash Descriptor)
    - otherwise directly write to the flash chip.
    
    The behavior of this code updater can be configured via EM.
    
    Change-Id: I27803b7fded71af2364c2f55fad841a410603dec
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ fw-update/component/bios-spi/spi_device_code_updater.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "fw-update/common/include/software_manager.hpp"
+#include "sdbusplus/async/context.hpp"
+
+#include <sdbusplus/async.hpp>
+
+class SPIDeviceCodeUpdater : public SoftwareManager
+{
+  public:
+    SPIDeviceCodeUpdater(sdbusplus::async::context& ctx, bool isDryRun,
+                         bool debug);
+
+    sdbusplus::async::task<> getInitialConfigurationSingleDevice(
+        const std::string& service, DeviceConfig& config) final;
+
+    bool debug;
+
+  private:
+};

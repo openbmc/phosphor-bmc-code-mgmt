ef03e6422bb5d8a83674d1311960222fb267d74e
Alexander Hansen <alexander.hansen@9elements.com>
2025-01-14 14:17:19 +0100

    fw update: tests for common code
    Tests for the common code introduced in another commit.
    
    These tests should check that the common code behaves as outlined in the
    design [1]
    
    References:
    [1] https://github.com/openbmc/docs/blob/master/designs/code-update.md
    
    Tested: unit tests pass
    
    Change-Id: I8f12839afd47ef3403a80439af54fedcc00f10be
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ test/common/software/test_software_get_random_softwareid.cpp
@@ -0,0 +1,43 @@
+#include "../nopdevice/nopdevice.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/async.hpp>
+#include <sdbusplus/server.hpp>
+
+#include <memory>
+#include <regex>
+
+#include <gtest/gtest.h>
+
+class WrapSoftware : public Software
+{
+  public:
+    static std::string wrapGetRandomSoftwareId(Device& parent)
+    {
+        return Software::getRandomSoftwareId(parent);
+    };
+};
+
+TEST(SoftwareTest, TestSoftwareGetRandomSoftwareId)
+{
+    sdbusplus::async::context ctx;
+    NopCodeUpdater nopcu(ctx);
+    NopCodeUpdater* cu = &nopcu;
+
+    SoftwareConfig config(0x1234, "my.example.compatible", "Nop",
+                          "MB1NopComponent");
+
+    auto device = std::make_unique<NopDevice>(ctx, config, cu);
+
+    std::string swid = WrapSoftware::wrapGetRandomSoftwareId(*device);
+    lg2::debug("{SWID}", "SWID", swid);
+
+    std::regex re("[a-zA-Z0-9]+_[0-9]+");
+    std::cmatch m;
+
+    EXPECT_TRUE(std::regex_match(swid.c_str(), m, re));
+
+    EXPECT_TRUE(swid.starts_with("MB1NopComponent_"));
+}

54a356bc6112c92084e4f57483b53d42d47c9bd6
Alexander Hansen <alexander.hansen@9elements.com>
2025-02-25 10:24:21 +0100

    SPI device CU: unit test
    Add a unit test for spi device code updater.
    
    Change-Id: Ic01952d8da566d7b6ea3c61dafee8af009486f51
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ test/bios-spi/test_bios_spi.cpp
@@ -0,0 +1,58 @@
+#include "bios-spi/bios_software_manager.hpp"
+#include "bios-spi/spi_device.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/async.hpp>
+#include <sdbusplus/server.hpp>
+#include <xyz/openbmc_project/Association/Definitions/server.hpp>
+#include <xyz/openbmc_project/Software/Update/server.hpp>
+
+#include <iostream>
+#include <memory>
+
+#include "gtest/gtest.h"
+
+// NOLINTNEXTLINE(readability-static-accessed-through-instance)
+sdbusplus::async::task<void> start(sdbusplus::async::context& ctx)
+// NOLINTEND(readability-static-accessed-through-instance)
+{
+    BIOSSoftwareManager spidcu(ctx, true);
+    uint32_t vendorIANA = 0x0000a015;
+    std::string compatible = "com.testvendor.testcomponent";
+    auto* cu = &spidcu;
+    std::vector<std::string> gpioNames;
+    std::vector<uint64_t> gpioValues;
+
+    std::string objPath =
+        "/xyz/openbmc_project/inventory/system/board/ExampleBoard/ExampleDevice";
+
+    SoftwareConfig config(objPath, vendorIANA, compatible, configTypeBIOS,
+                          "HostSPI");
+
+    auto sd = std::make_unique<SPIDevice>(
+        ctx, 1, 0, true, true, gpioNames, gpioValues, config, cu,
+        flashLayoutFlat, flashToolNone, std::filesystem::current_path());
+
+    spidcu.devices.insert({objPath, std::move(sd)});
+
+    ctx.request_stop();
+    co_return;
+}
+
+TEST(BIOS, Constructor)
+{
+    sdbusplus::async::context ctx;
+
+    try
+    {
+        ctx.spawn(start(ctx));
+        ctx.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        assert(false);
+    }
+}

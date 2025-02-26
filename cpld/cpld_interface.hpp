171193932d15d6e255f22f0ae9b4127eb4e3a2d3
Daniel Hsu <Daniel-Hsu@quantatw.com>
2025-02-26 15:03:47 +0800

    fw-update: cpld code updater
    This commit introduces a code updater for CPLD.
    The Key features of this updater include:
    - configuring chip vendor and name
    - update CPLD through I2C
    
    Change-Id: Ife8e30a00bfbb307e6e4eb55838a397c8a8162bd
    Signed-off-by: Daniel Hsu <Daniel-Hsu@quantatw.com>

---
+++ cpld/cpld_interface.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <cstdint>
+#include <functional>
+#include <memory>
+#include <string>
+
+namespace phosphor::software::cpld
+{
+
+class CPLDInterface
+{
+  public:
+    explicit CPLDInterface() {}
+    virtual ~CPLDInterface() = default;
+
+    CPLDInterface(const CPLDInterface&) = delete;
+    CPLDInterface& operator=(const CPLDInterface&) = delete;
+    CPLDInterface(CPLDInterface&&) = delete;
+    CPLDInterface& operator=(CPLDInterface&&) = delete;
+
+    virtual bool updateFirmware(bool force, const uint8_t* image,
+                                size_t imageSize) = 0;
+};
+
+class CPLDFactory
+{
+  public:
+    using Creator = std::function<std::unique_ptr<CPLDInterface>(
+        const std::string& chipName, uint16_t bus, uint8_t address)>;
+
+    static CPLDFactory& instance();
+
+    void registerVendor(const std::string& vendorName, Creator creator);
+    std::unique_ptr<CPLDInterface> create(const std::string& vendorName,
+                                          const std::string& chipName,
+                                          uint16_t bus, uint8_t address) const;
+
+  private:
+    std::unordered_map<std::string, Creator> creators;
+};
+
+} // namespace phosphor::software::cpld

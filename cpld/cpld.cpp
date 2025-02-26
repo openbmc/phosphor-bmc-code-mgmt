e8e31506a472281d2e510f7288e88413d84945f9
Daniel Hsu <Daniel-Hsu@quantatw.com>
2025-02-26 15:03:47 +0800

    fw-update: cpld code updater
    This commit introduces a code updater for CPLD.
    The Key features of this updater include:
    - configuring chip vendor and name
    - update CPLD through I2C
    
    Change-Id: Ife8e30a00bfbb307e6e4eb55838a397c8a8162bd
    Signed-off-by: Daniel Hsu <Daniel-Hsu@quantatw.com>

---
+++ cpld/cpld.cpp
@@ -0,0 +1,34 @@
+#include "cpld.hpp"
+
+namespace phosphor::software::cpld
+{
+
+// NOLINTBEGIN(readability-static-accessed-through-instance)
+sdbusplus::async::task<bool> CPLDDevice::updateDevice(const uint8_t* image,
+                                                      size_t image_size)
+// NOLINTEND(readability-static-accessed-through-instance)
+{
+    if (cpldInterface == nullptr)
+    {
+        lg2::error("CPLD interface is not initialized");
+        co_return false;
+    }
+    else
+    {
+        setUpdateProgress(1);
+        if (!(co_await cpldInterface->updateFirmware(
+                false, image, image_size, [this](int percent) -> bool {
+                    return this->setUpdateProgress(percent);
+                })))
+        {
+            lg2::error("Failed to update CPLD firmware");
+            co_return false;
+        }
+
+        setUpdateProgress(100);
+        lg2::info("Successfully updated CPLD");
+        co_return true;
+    }
+}
+
+} // namespace phosphor::software::cpld

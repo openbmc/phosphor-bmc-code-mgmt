d357db9e2c78c4dafa739948b2c5c5c13665ee13
Daniel Hsu <Daniel-Hsu@quantatw.com>
2025-02-26 15:03:47 +0800

    fw-update: cpld code updater
    This commit introduces a code updater for CPLD.
    The Key features of this updater include:
    - configuring chip vendor and name
    - update CPLD through I2C
    
    Change-Id: Ife8e30a00bfbb307e6e4eb55838a397c8a8162bd
    Signed-off-by: Daniel Hsu <Daniel-Hsu@quantatw.com>

---
+++ cpld/lattice/lattice.hpp
@@ -0,0 +1,156 @@
+#pragma once
+#include "common/include/i2c/i2c.hpp"
+#include "cpld_fw_handler.hpp"
+
+#include <chrono>
+#include <string_view>
+
+constexpr uint8_t busyWaitmaxRetry = 30;
+constexpr uint8_t busyFlagBit = 0x80;
+constexpr uint8_t statusRegBusy = 0x10;
+constexpr uint8_t statusRegFail = 0x20;
+constexpr std::chrono::milliseconds waitBusyTime(200);
+
+static constexpr std::string_view TAG_QF = "QF";
+static constexpr std::string_view TAG_UH = "UH";
+static constexpr std::string_view TAG_CF_START = "L000";
+static constexpr std::string_view TAG_UFM = "NOTE TAG DATA";
+static constexpr std::string_view TAG_ROW = "NOTE FEATURE";
+static constexpr std::string_view TAG_CHECKSUM = "C";
+static constexpr std::string_view TAG_USERCODE = "NOTE User Electronic";
+static constexpr std::string_view TAG_EBR_INIT_DATA = "NOTE EBR_INIT DATA";
+static constexpr std::string_view TAG_CF_END = "NOTE END OF CFG";
+
+constexpr uint8_t isOK = 0;
+constexpr uint8_t isReady = 0;
+constexpr uint8_t busyOrReadyBit = 4;
+constexpr uint8_t failOrOKBit = 5;
+
+enum cpldI2cCmd
+{
+    CMD_ERASE_FLASH = 0x0E,
+    CMD_DISABLE_CONFIG_INTERFACE = 0x26,
+    CMD_READ_STATUS_REG = 0x3C,
+    CMD_RESET_CONFIG_FLASH = 0x46,
+    CMD_PROGRAM_DONE = 0x5E,
+    CMD_PROGRAM_PAGE = 0x70,
+    CMD_ENABLE_CONFIG_MODE = 0x74,
+    CMD_READ_FW_VERSION = 0xC0,
+    CMD_PROGRAM_USER_CODE = 0xC2,
+    CMD_READ_DEVICE_ID = 0xE0,
+    CMD_READ_BUSY_FLAG = 0xF0,
+};
+
+struct cpldI2cInfo
+{
+    unsigned long int QF;
+    unsigned int* UFM;
+    unsigned int Version;
+    unsigned int CheckSum;
+    std::vector<uint8_t> cfgData;
+    std::vector<uint8_t> ufmData;
+};
+
+class CpldLatticeManager : public CpldManager
+{
+  public:
+    std::vector<uint8_t> fwData;
+    cpldI2cInfo fwInfo{};
+    CpldLatticeManager(const uint8_t bus, const uint8_t address,
+                       const uint8_t* image, size_t imageSize,
+                       const std::string& chip, const std::string& interface,
+                       const std::string& target, const bool debugMode) :
+        CpldManager(bus, address, image, imageSize, chip, interface, target,
+                    debugMode),
+        i2cInterface(phosphor::i2c::I2C(bus, address))
+    {}
+    sdbusplus::async::task<bool> fwUpdate();
+    sdbusplus::async::task<bool> getVersion();
+    sdbusplus::async::task<bool> XO2XO3Family_update();
+    sdbusplus::async::task<bool> XO5Family_update();
+
+  protected:
+    phosphor::i2c::I2C i2cInterface;
+
+  private:
+    int indexof(const char* str, const char* ptn);
+    int jedFileParser();
+    sdbusplus::async::task<bool> readDeviceId();
+    sdbusplus::async::task<bool> enableProgramMode();
+    sdbusplus::async::task<bool> eraseFlash();
+    sdbusplus::async::task<bool> resetConfigFlash();
+    sdbusplus::async::task<bool> writeProgramPage();
+    sdbusplus::async::task<bool> programUserCode();
+    sdbusplus::async::task<bool> programDone();
+    int disableusyAndVerify();
+    sdbusplus::async::task<bool> disableConfigInterface();
+    sdbusplus::async::task<bool> readBusyFlag(uint8_t& busyFlag);
+    sdbusplus::async::task<bool> readStatusReg(uint8_t& statusReg);
+    sdbusplus::async::task<bool> waitBusyAndVerify();
+    sdbusplus::async::task<bool> readUserCode(uint32_t& userCode);
+};
+
+class XO5I2CManager : public CpldLatticeManager
+{
+    friend class CpldLatticeManager;
+
+  public:
+    // XO5 commands
+    static const uint8_t XO5_CMD_IDLE = 0x00;
+    static const uint8_t XO5_CMD_SET_PAGE = 0x01;
+    static const uint8_t XO5_CMD_ERASE_FLASH = 0x02;
+    static const uint8_t XO5_CMD_CFG_WRITE_PAGE = 0x11;
+    static const uint8_t XO5_CMD_CFG_RESET_ADDR = 0x12;
+    static const uint8_t XO5_CMD_CFG_READ_PAGE = 0x19;
+    static const uint8_t XO5_CMD_UFM_WRITE_PAGE = 0x20;
+    static const uint8_t XO5_CMD_UFM_READ_PAGE = 0x21;
+    static const uint8_t XO5_CMD_UFM_RESET_ADDR = 0x22;
+    static const uint8_t XO5_CMD_USERDATA_WRITE_PAGE = 0x30;
+    static const uint8_t XO5_CMD_USERDATA_READ_PAGE = 0x31;
+    static const uint8_t XO5_CMD_USERDATA_RESET_ADDR = 0x32;
+
+    // Flash partition (X25)
+    static const uint8_t XO5_PARTITION_CFG0 = 0x00;
+    static const uint8_t XO5_PARTITION_UFM0 = 0x01;
+    static const uint8_t XO5_PARTITION_CFG1 = 0x02;
+    static const uint8_t XO5_PARTITION_UFM1 = 0x03;
+    static const uint8_t XO5_PARTITION_CFG2 = 0x04;
+    static const uint8_t XO5_PARTITION_UFM2 = 0x05;
+    static const uint8_t XO5_PARTITION_USERDATA0 = 0x06;
+    static const uint8_t XO5_PARTITION_USERDATA1 = 0x07;
+    static const uint8_t XO5_PARTITION_USERDATA2 = 0x08;
+    static const uint8_t XO5_PARTITION_USERDATA3 = 0x09;
+    static const uint8_t XO5_PARTITION_USERDATA4 = 0x0A;
+    static const uint8_t XO5_PARTITION_USERDATA5 = 0x0B;
+    static const uint8_t XO5_PARTITION_USERDATA6 = 0x0C;
+    static const uint8_t XO5_PARTITION_USERDATA7 = 0x0D;
+    static const uint8_t XO5_PARTITION_USERDATA8 = 0x0E;
+
+    // Flash erase size
+    static const uint8_t XO5_ERASE_BLOCK = 0x0;
+    static const uint8_t XO5_ERASE_WHOLE = 0x1;
+
+    static const int XO5_PAGE_NUM = 256;
+    static const int XO5_PAGE_SIZE = 256;
+    static const int XO5_CFG_BLOCK_NUM = 11;
+
+    XO5I2CManager(const uint8_t bus, const uint8_t addr, const uint8_t* image,
+                  size_t imageSize, const std::string& chip,
+                  const std::string& interface, const std::string& target,
+                  const bool debugMode) :
+        CpldLatticeManager(bus, addr, image, imageSize, chip, interface, target,
+                           debugMode)
+    {}
+
+  private:
+    int XO5jedFileParser();
+    sdbusplus::async::task<bool> set_page(uint8_t partition_num,
+                                          uint32_t page_num);
+    sdbusplus::async::task<bool> erase_flash(uint8_t size);
+    sdbusplus::async::task<bool> cfg_reset_addr();
+    sdbusplus::async::task<std::vector<uint8_t>> cfg_read_page();
+    sdbusplus::async::task<bool> cfg_write_page(
+        const std::vector<uint8_t>& byte_list);
+    sdbusplus::async::task<bool> programCfgData(uint8_t cfg);
+    sdbusplus::async::task<bool> verifyCfgData(uint8_t cfg);
+};

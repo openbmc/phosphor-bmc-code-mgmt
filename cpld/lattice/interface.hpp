1c83f2c47761a3c38b0c248561a732fcd50a5523
Daniel Hsu <Daniel-Hsu@quantatw.com>
2025-02-26 15:03:47 +0800

    fw-update: cpld code updater
    This commit introduces a code updater for CPLD.
    The Key features of this updater include:
    - configuring chip vendor and name
    - update CPLD through I2C
    
    Change-Id: Ife8e30a00bfbb307e6e4eb55838a397c8a8162bd
    Signed-off-by: Daniel Hsu <Daniel-Hsu@quantatw.com>

---
+++ cpld/lattice/interface.hpp
@@ -0,0 +1,22 @@
+#include "cpld/cpld_interface.hpp"
+
+namespace phosphor::software::cpld
+{
+
+class LatticeCPLD : public CPLDInterface
+{
+  public:
+    LatticeCPLD(const std::string& chipname, uint16_t bus, uint8_t address) :
+        chipname(chipname), bus(bus), address(address)
+    {}
+
+    sdbusplus::async::task<bool> updateFirmware(
+        bool force, const uint8_t* image, size_t imageSize) final;
+
+  private:
+    std::string chipname;
+    uint16_t bus;
+    uint8_t address;
+};
+
+} // namespace phosphor::software::cpld

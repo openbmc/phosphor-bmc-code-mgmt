cdd762567a3b27ab0798bcd89d91356e7bea8611
Daniel Hsu <Daniel-Hsu@quantatw.com>
2025-02-26 15:03:47 +0800

    fw-update: cpld code updater
    This commit introduces a code updater for CPLD.
    The Key features of this updater include:
    - configuring chip vendor and name
    - update CPLD through I2C
    
    Change-Id: Ife8e30a00bfbb307e6e4eb55838a397c8a8162bd
    Signed-off-by: Daniel Hsu <Daniel-Hsu@quantatw.com>

---
+++ cpld/lattice/interface.cpp
@@ -0,0 +1,39 @@
+#include "interface.hpp"
+
+#include "lattice.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+
+namespace phosphor::software::cpld
+{
+
+bool LatticeCPLD::updateFirmware(bool /*force*/, const uint8_t* image,
+                                 size_t imageSize)
+{
+    // Implement the firmware update logic for Lattice CPLD here
+    // This is a placeholder implementation
+    lg2::info("Updating Lattice CPLD firmware");
+
+    auto cpldManager = CpldLatticeManager(bus, address, image, imageSize,
+                                          chipname, "i2c", "CFG0", false);
+
+    return static_cast<bool>(cpldManager.fwUpdate() >= 0);
+}
+
+} // namespace phosphor::software::cpld
+
+// Factory function to create lattice CPLD device
+namespace
+{
+using namespace phosphor::software::cpld;
+const bool registered = [] {
+    CPLDFactory::instance().registerVendor(
+        "lattice",
+        [](const std::string& chipname, uint16_t bus, uint8_t address) {
+            // Create and return a LatticeCPLD instance
+            // Pass the parameters to the constructor
+            return std::make_unique<LatticeCPLD>(chipname, bus, address);
+        });
+    return true;
+}();
+} // namespace

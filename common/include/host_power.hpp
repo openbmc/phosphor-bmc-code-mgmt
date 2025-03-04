298cb7e717050092f1bafd5fc017b0d9aafa5e62
Alexander Hansen <alexander.hansen@9elements.com>
2025-03-04 10:20:33 +0100

    common: host power utility
    Create this utility class to abstract host power operation with a single
    function call.
    
    This was refactored out of [1] (BIOS Code Updater)
    
    Tested: Has been tested as part of [1]
    
    References:
    
    [1] https://gerrit.openbmc.org/c/openbmc/phosphor-bmc-code-mgmt/+/76101
    
    Change-Id: I97dc8b1824f70f0aeede3b39683c2ee4ef9ca3c9
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ common/include/host_power.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <sdbusplus/async/context.hpp>
+#include <sdbusplus/async/match.hpp>
+#include <xyz/openbmc_project/State/Host/client.hpp>
+
+namespace phosphor::software::host_power
+{
+
+const auto stateOn =
+    sdbusplus::common::xyz::openbmc_project::state::Host::HostState::Running;
+const auto stateOff =
+    sdbusplus::common::xyz::openbmc_project::state::Host::HostState::Off;
+
+using HostState =
+    sdbusplus::common::xyz::openbmc_project::state::Host::HostState;
+
+class HostPower
+{
+  public:
+    HostPower(sdbusplus::async::context& ctx);
+
+    // @param state   desired powerstate (true means on)
+    // @returns       true on success
+    static sdbusplus::async::task<bool> setHostPowerstate(
+        sdbusplus::async::context& ctx, HostState state);
+
+    // @returns       true when powered
+    // @returns       std::nullopt on failure to query power state
+    static sdbusplus::async::task<HostState> getHostPowerstate(
+        sdbusplus::async::context& ctx);
+
+    sdbusplus::async::match hostStateChangedMatch;
+};
+
+}; // namespace phosphor::software::host_power

50f27daaf8217bc75ae7745f3440f31dac0486a4
Christopher Meis <christopher.meis@9elements.com>
2025-03-10 12:04:15 +0100

    Add I2CFW dummy device
    Change-Id: I9b3bb84c929849526b8870f4cee74af3c6faea2c
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

---
+++ i2c-vr/dummyDevice/dummyDevice.hpp
@@ -0,0 +1,23 @@
+#include "vr.hpp"
+
+#include <sdbusplus/async.hpp>
+
+namespace SDBusAsync = sdbusplus::async;
+
+namespace phosphor::software::VR
+{
+
+class DummyDevice : public VoltageRegulator
+{
+  public:
+    DummyDevice(SDBusAsync::context& ctx);
+
+    SDBusAsync::task<bool> verifyImage(const uint8_t* image,
+                                       size_t imageSize) final;
+    SDBusAsync::task<bool> updateFirmware(bool force) final;
+    SDBusAsync::task<bool> reset() final;
+    bool getCRC(uint32_t* checksum) final;
+    bool forcedUpdateAllowed() final;
+};
+
+} // namespace phosphor::software::VR

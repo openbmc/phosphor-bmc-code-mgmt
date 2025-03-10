3a1fccb22b87bcc66e369fc12533445357fb98f7
Christopher Meis <christopher.meis@9elements.com>
2025-03-10 12:04:15 +0100

    Add I2CFW dummy device
    Change-Id: I9b3bb84c929849526b8870f4cee74af3c6faea2c
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

---
+++ i2c-vr/dummyDevice/dummyDevice.cpp
@@ -0,0 +1,52 @@
+#include "dummyDevice.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async.hpp>
+
+PHOSPHOR_LOG2_USING;
+
+namespace SDBusAsync = sdbusplus::async;
+
+namespace phosphor::software::VR
+{
+
+DummyDevice::DummyDevice(SDBusAsync::context& ctx) : VoltageRegulator(ctx)
+{
+    info("DummyDevice Voltage Regulator created");
+}
+
+SDBusAsync::task<bool> DummyDevice::verifyImage(const uint8_t* image,
+                                                size_t imageSize)
+{
+    (void)*image;
+    (void)imageSize;
+    SDBusAsync::sleep_for(ctx, std::chrono::microseconds(5000));
+    co_return true;
+}
+
+SDBusAsync::task<bool> DummyDevice::updateFirmware(bool force)
+{
+    if (force)
+    {
+        info("DummyDevice Update will be forced!");
+    }
+
+    SDBusAsync::sleep_for(ctx, std::chrono::microseconds(5000));
+    info("DummyDevice Update done");
+
+    co_return true;
+}
+
+SDBusAsync::task<bool> DummyDevice::reset()
+{
+    SDBusAsync::sleep_for(ctx, std::chrono::microseconds(5000));
+    co_return true;
+}
+
+bool DummyDevice::getCRC(uint32_t* checksum)
+{
+    *checksum = 0x0000BEEF;
+    return true;
+}
+
+} // namespace phosphor::software::VR

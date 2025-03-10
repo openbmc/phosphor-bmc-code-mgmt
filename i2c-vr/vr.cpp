6706269640926bb04414561c7c51d3e670c1fdbe
Christopher Meis <christopher.meis@9elements.com>
2025-03-10 12:04:15 +0100

    Add I2VFW dummy device
    Change-Id: I9b3bb84c929849526b8870f4cee74af3c6faea2c
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- i2c-vr/vr.cpp
+++ i2c-vr/vr.cpp
@@ -1,6 +1,7 @@
 #include "vr.hpp"
 
 #include "xdpe1x2xx/xdpe1x2xx.hpp"
+#include "dummyDevice/dummyDevice.hpp"
 
 #include <map>
 
@@ -16,6 +17,9 @@ std::unique_ptr<VoltageRegulator> create(SDBusAsync::context& ctx, enum VRType v
         case VRType::XDPE1X2XX:
             ret = std::make_unique<XDPE1X2XX>(ctx, bus, address);
             break;
+        case VRType::DummyVR:
+            ret = std::make_unique<DummyDevice>(ctx);
+            break;
         default:
             return NULL;
     }
@@ -25,6 +29,7 @@ std::unique_ptr<VoltageRegulator> create(SDBusAsync::context& ctx, enum VRType v
 bool stringToEnum(std::string& vrStr, VRType& vrType)
 {
     std::map<std::string, enum VRType> VRTypeToString{
+        {"DummyDeviceFirmware", VRType::DummyVR},
         {"XDPE1X2XXFirmware", VRType::XDPE1X2XX},
     };
 

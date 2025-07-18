d7b72646615202dc66ab979517aa6a73d585c746
Alexander Hansen <alexander.hansen@9elements.com>
2025-07-18 16:30:32 +0200

    common: fix Software::getPurpose error path
    'version' cannot be dereferenced if it is not initialized.
    
    Instead of changing the API here, throw an exception to avoid the
    segmentation fault.
    
    Change-Id: Iac7badf145dad64e21ede5e81f0b4b761ff6c89d
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- common/src/software.cpp
+++ common/src/software.cpp
@@ -124,6 +124,11 @@ void Software::setVersion(const std::string& versionStr,
 
 SoftwareVersion::VersionPurpose Software::getPurpose()
 {
+    if (!version)
+    {
+        throw std::invalid_argument(
+            "no version present, cannot get version purpose");
+    }
     return version->purpose();
 }
 

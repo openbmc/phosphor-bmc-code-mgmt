740ab433ea2062e035ec7d2db7a44fd612e23609
Alexander Hansen <alexander.hansen@9elements.com>
2025-07-29 12:37:13 +0200

    common: split inventory association function
    Create Software::createInventoryAssociationWithEndpoint to split the
    object mapper call from actually creating the association.
    
    This makes unit testing the functions easier and provides a clean
    separation between the logic provided by common code versus the external
    information we are fetching.
    
    Tested: Inspection only.
    
    Change-Id: I0147480219b9c9fbc709699b9e50411fb11bea6e
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- common/src/software.cpp
+++ common/src/software.cpp
@@ -74,6 +74,12 @@ sdbusplus::async::task<> Software::createInventoryAssociations(bool isRunning)
         co_return;
     }
 
+    co_await createInventoryAssociationWithEndpoint(isRunning, endpoint);
+}
+
+sdbusplus::async::task<> Software::createInventoryAssociationWithEndpoint(
+    bool isRunning, std::string endpoint)
+{
     std::vector<std::tuple<std::string, std::string, std::string>> assocs;
 
     if (isRunning)

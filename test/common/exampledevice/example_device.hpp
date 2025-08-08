18b5e61bcd99b658aac04c021c5cee65c0b004a6
Alexander Hansen <alexander.hansen@9elements.com>
2025-08-08 11:35:40 +0200

    test: common: software
    Test API of `class Software`, focus on initial state on construction.
    
    Change-Id: I20350748d02f62cfb8371fee68d43ebbdb91609c
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- test/common/exampledevice/example_device.hpp
+++ test/common/exampledevice/example_device.hpp
@@ -51,6 +51,7 @@ class ExampleSoftware : public Software
 {
   public:
     using Software::createInventoryAssociation;
+    using Software::getPurpose;
     using Software::objectPath;
     ExampleSoftware(sdbusplus::async::context& ctx, ExampleDevice& parent);
 };

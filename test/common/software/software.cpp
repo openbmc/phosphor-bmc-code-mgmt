66182b6f9228d76cb6532c0d97f4511897302ab0
Alexander Hansen <alexander.hansen@9elements.com>
2025-08-08 11:35:40 +0200

    test: common: software
    Test API of `class Software`, focus on initial state on construction.
    
    Change-Id: I20350748d02f62cfb8371fee68d43ebbdb91609c
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ test/common/software/software.cpp
@@ -0,0 +1,76 @@
+#include "../exampledevice/example_device.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/async.hpp>
+#include <sdbusplus/server.hpp>
+
+#include <memory>
+
+#include <gtest/gtest.h>
+
+PHOSPHOR_LOG2_USING;
+
+using namespace phosphor::software;
+using namespace phosphor::software::example_device;
+
+class SoftwareTest : public testing::Test
+{
+  protected:
+    SoftwareTest() :
+        exampleUpdater(ctx, true, nullptr), device(exampleUpdater.getDevice())
+    {}
+    ~SoftwareTest() noexcept override {}
+
+    sdbusplus::async::context ctx;
+    ExampleCodeUpdater exampleUpdater;
+    std::unique_ptr<ExampleDevice>& device;
+
+  public:
+    SoftwareTest(const SoftwareTest&) = delete;
+    SoftwareTest(SoftwareTest&&) = delete;
+    SoftwareTest& operator=(const SoftwareTest&) = delete;
+    SoftwareTest& operator=(SoftwareTest&&) = delete;
+};
+
+TEST_F(SoftwareTest, TestSoftwareConstructor)
+{
+    // the software version is currently unknown
+    EXPECT_EQ(device->softwareCurrent, nullptr);
+
+    auto sw = std::make_unique<Software>(ctx, *device);
+
+    // since that software is not an update, there is no progress
+    EXPECT_EQ(sw->softwareActivationProgress, nullptr);
+}
+
+TEST_F(SoftwareTest, TestVersionPurpose)
+{
+    auto sw = std::make_unique<ExampleSoftware>(ctx, *device);
+
+    EXPECT_EQ(sw->getPurpose(), std::nullopt);
+
+    sw->setVersion("swVersion");
+    EXPECT_EQ(sw->getPurpose(), SoftwareVersion::VersionPurpose::Unknown);
+}
+
+TEST_F(SoftwareTest, TestSoftwareId)
+{
+    auto sw = std::make_unique<Software>(ctx, *device);
+
+    // design: Swid = <DeviceX>_<RandomId>
+    EXPECT_TRUE(sw->swid.starts_with("ExampleSoftware"));
+}
+
+TEST_F(SoftwareTest, TestSoftwareObjectPath)
+{
+    auto sw = std::make_unique<ExampleSoftware>(ctx, *device);
+
+    debug("{PATH}", "PATH", sw->objectPath);
+
+    // assert that the object path is as per the design
+    // design: /xyz/openbmc_project/Software/<SwId>
+    EXPECT_TRUE(std::string(sw->objectPath)
+                    .starts_with("/xyz/openbmc_project/software/"));
+}

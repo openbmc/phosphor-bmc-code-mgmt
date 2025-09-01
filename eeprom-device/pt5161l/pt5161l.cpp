e27405e1cdb00b6e7a0a0e6a459fa0148b43768d
Daniel Hsu <Daniel-Hsu@quantatw.com>
2025-09-01 16:12:43 +0800

    eeprom: update pt5161l getVersion()
    Add a step to read fw_load_status before fetching the firmware
    version. This ensures the service only proceeds when the device
    reports a normal status.
    
    Test on Harma:
    ```
    <7> Host state xyz.openbmc_project.State.Host.HostState.Running matches to retrieve the version
    <3> Status from file: /sys/kernel/debug/pt5161l/21-0024/fw_load_status is invalid: abnormal, retrying...
    <7> Harma_MB_Retimer_1_8983: set version 2.8.19
    <7> Host state xyz.openbmc_project.State.Host.HostState.Running matches to retrieve the version
    <7> Harma_MB_Retimer_0_3392: set version 2.8.19
    ```
    
    Change-Id: I7b665652d8bee20956da3e3d15ff1d222560a656
    Signed-off-by: Daniel Hsu <Daniel-Hsu@quantatw.com>

--- eeprom-device/pt5161l/pt5161l.cpp
+++ eeprom-device/pt5161l/pt5161l.cpp
@@ -12,12 +12,41 @@ PHOSPHOR_LOG2_USING;
 std::string PT5161LDeviceVersion::getVersion()
 {
     std::string version;
+    std::string status;
     std::ostringstream busOss;
     std::ostringstream addrOss;
+    bool isStatusNormal = false;
+    constexpr int maxRetries = 15;
+    constexpr auto retryDelay = std::chrono::seconds(2);
 
     busOss << std::setw(2) << std::setfill('0') << static_cast<int>(bus);
     addrOss << std::setw(4) << std::setfill('0') << std::hex << std::nouppercase
             << static_cast<int>(address);
+
+    std::string fw_load_status = "/sys/kernel/debug/pt5161l/" + busOss.str() +
+                                 "-" + addrOss.str() + "/fw_load_status";
+
+    for (int i = 0; i < maxRetries; ++i)
+    {
+        std::ifstream file(fw_load_status);
+
+        if (file && std::getline(file, status) && status == "normal")
+        {
+            isStatusNormal = true;
+            break;
+        }
+
+        error("Status from file: {PATH} is invalid: {STATUS}, retrying...",
+              "PATH", fw_load_status, "STATUS", status);
+
+        std::this_thread::sleep_for(retryDelay);
+    }
+
+    if (!isStatusNormal)
+    {
+        error("Failed to get version: firmware load status is not normal");
+        return version;
+    }
 
     // The PT5161L driver exposes the firmware version through the fw_ver node
     std::string path = "/sys/kernel/debug/pt5161l/" + busOss.str() + "-" +

94499b0abd1f1b0100c740d6f1a4e2339f1cb19c
Henry Wu <Henry_Wu@quantatw.com>
2025-10-03 10:32:17 +0800

    common: add GPIO controller function
    The CPLD update process requires toggling a GPIO pin to control the
    debug card mux. BIOS and EEPROM updates use the same logic, so refactor
    the duplicated code into a common GPIO controller function. Update BIOS,
    EEPROM, and CPLD code paths to use the new helper.
    
    Tested:
    - Verified CPLD update with GPIO mux toggling
    - Verified BIOS update with GPIO mux toggling
    
    Verified CPLD update with GPIO mux toggling
    ```
    Oct 02 19:00:53 bmc phosphor-cpld-software-update[897]: Requesting chip gpiochip0, GPIO line debug-card-mux to 1
    Oct 02 19:00:53 bmc phosphor-cpld-software-update[897]: Updating Lattice CPLD firmware
    Oct 02 19:01:12 bmc phosphor-cpld-software-update[897]: [48K blob data]
    Oct 02 19:01:25 bmc phosphor-cpld-software-update[897]: Successfully updated CPLD
    Oct 02 19:01:25 bmc phosphor-cpld-software-update[897]: Requesting chip gpiochip0, GPIO line debug-card-mux to 0
    ```
    
    Verified BIOS update with GPIO mux toggling
    ```
    Oct 02 19:11:55 bmc phosphor-bios-software-update[891]: Requesting chip gpiochip0, GPIO line FM_MUX1_SEL_R to 1
    Oct 02 19:13:36 bmc phosphor-bios-software-update[2212]: [14.8K blob data]
    Oct 02 19:13:44 bmc phosphor-bios-software-update[2212]: [47.9K blob data]
    Oct 02 19:13:54 bmc phosphor-bios-software-update[2212]: [48K blob data]
    Oct 02 19:14:04 bmc phosphor-bios-software-update[2212]: [48K blob data]
    Oct 02 19:14:17 bmc phosphor-bios-software-update[2212]: [48K blob data]
    Oct 02 19:14:26 bmc phosphor-bios-software-update[2212]: [47.9K blob data]
    Oct 02 19:14:27 bmc phosphor-bios-software-update[2212]: [4.5K blob data]
    Oct 02 19:14:30 bmc phosphor-bios-software-update[2212]: [48K blob data]
    Oct 02 19:14:32 bmc phosphor-bios-software-update[2212]: [48K blob data]
    Oct 02 19:14:35 bmc phosphor-bios-software-update[2212]: [48K blob data]
    Oct 02 19:14:37 bmc phosphor-bios-software-update[2212]: [48K blob data]
    Oct 02 19:14:40 bmc phosphor-bios-software-update[2212]: [48K blob data]
    Oct 02 19:14:41 bmc phosphor-bios-software-update[2212]: [20.5K blob data]
    Oct 02 19:14:43 bmc phosphor-bios-software-update[891]: Successfully updated SPI flash
    Oct 02 19:14:43 bmc phosphor-bios-software-update[891]: Requesting chip gpiochip0, GPIO line FM_MUX1_SEL_R to 0
    ```
    
    Change-Id: I8d3c128951fd833a0cd25e1440236f82d37b776f
    Signed-off-by: Henry Wu <Henry_Wu@quantatw.com>

---
+++ common/include/gpio_controller.hpp
@@ -0,0 +1,66 @@
+#pragma once
+#include <gpiod.hpp>
+
+#include <string>
+
+std::vector<std::unique_ptr<::gpiod::line_bulk>> requestMuxGPIOs(
+    const std::vector<std::string>& gpioLines,
+    const std::vector<bool>& gpioPolarities, bool inverted);
+
+/*
+ * @class GPIOGroup
+ * @brief A group of GPIO lines to be muxed together
+ */
+class GPIOGroup
+{
+  public:
+    GPIOGroup(std::vector<std::string> lines, std::vector<bool> polarities);
+
+    ~GPIOGroup();
+
+    // GPIOs set to route mux to the BMC
+    bool muxToBMC();
+    // GPIOs set to the opposite values to route mux to the external device
+    bool muxToDevice();
+
+    void releaseAll();
+
+    GPIOGroup(const GPIOGroup&) = delete;
+    GPIOGroup& operator=(const GPIOGroup&) = delete;
+    GPIOGroup(GPIOGroup&& /*other*/) noexcept;
+    GPIOGroup& operator=(GPIOGroup&& /*other*/) noexcept;
+
+  private:
+    bool mux(bool inverted);
+
+    std::vector<std::string> lines_;
+    std::vector<bool> polarities_;
+    std::vector<std::unique_ptr<::gpiod::line_bulk>> activeBulks_;
+};
+
+/*
+ * @class MuxGuard
+ * @brief A RAII guard to mux GPIO lines to BMC on construction
+ * and back to device on destruction
+ */
+class MuxGuard
+{
+  public:
+    explicit MuxGuard(GPIOGroup& group);
+    ~MuxGuard();
+
+    MuxGuard(const MuxGuard&) = delete;
+    MuxGuard& operator=(const MuxGuard&) = delete;
+
+    MuxGuard(MuxGuard&& /*other*/) = delete;
+    MuxGuard& operator=(MuxGuard&& /*other*/) = delete;
+
+    bool ok() const
+    {
+        return ok_;
+    }
+
+  private:
+    GPIOGroup& group_;
+    bool ok_{false};
+};

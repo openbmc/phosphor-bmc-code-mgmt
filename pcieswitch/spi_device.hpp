24bd64e912602b1c9d1deaeb056b30f52d605d10
Colin Huang <colin.huang2@amd.com>
2025-11-14 17:40:57 +0800

    pcieswitch: support PCIe switch firmware update
    Tested on the Anacapa platform.
    ```
    BMC_IP="10.44.176.138"
    USER="root"
    PASS="0penBmc"
    
    FW_FILE="/home/dirk/anacapa-rbb-pciesw_0.07.pldm"
    COMPONENT_NAME="Anacapa_RBB_PCIESW"
    
    target_num=$(curl -s -k -u ${USER}:${PASS} "https://${BMC_IP}/redfish/v1/UpdateService/FirmwareInventory" \
        | grep -o "${COMPONENT_NAME}_[0-9]\+" \
        | head -n1 \
        | grep -o '[0-9]\+')
    
    if [ -z "$target_num" ]; then
        echo "Error: Could not find firmware target for ${COMPONENT_NAME}."
        exit 1
    fi
    
    TARGET_URL="/redfish/v1/UpdateService/FirmwareInventory/${COMPONENT_NAME}_${target_num}"
    
    echo "Found firmware target: ${TARGET_URL}"
    echo "Uploading firmware file: ${FW_FILE}"
    
    curl -k -u ${USER}:${PASS} -X POST \
      -H "Content-Type: multipart/form-data" \
      -F "UpdateParameters={\"Targets\":[\"${TARGET_URL}\"],\"@Redfish.OperationApplyTime\":\"Immediate\"};type=application/json" \
      -F "UpdateFile=@${FW_FILE};type=application/octet-stream" \
      "https://${BMC_IP}/redfish/v1/UpdateService/update-multipart"
    
    echo "Firmware update request sent for target ${TARGET_URL}"
    
    Found firmware target: /redfish/v1/UpdateService/FirmwareInventory/Anacapa_RBB_PCIESW_2956
    Uploading firmware file: /home/dirk/anacapa-rbb-pciesw_0.07.pldm
    {
      "@odata.id": "/redfish/v1/TaskService/Tasks/3",
      "@odata.type": "#Task.v1_4_3.Task",
      "HidePayload": false,
      "Id": "3",
      "Messages": [
        {
          "@odata.type": "#Message.v1_1_1.Message",
          "Message": "The task with Id '3' has started.",
          "MessageArgs": [
            "3"
          ],
          "MessageId": "TaskEvent.1.0.TaskStarted",
          "MessageSeverity": "OK",
          "Resolution": "None."
        }
      ],
      "Name": "Task 3",
      "Payload": {
        "HttpHeaders": [],
        "HttpOperation": "POST",
        "TargetUri": "/redfish/v1/UpdateService/update-multipart"
      },
      "PercentComplete": 0,
      "StartTime": "2025-11-14T09:18:48+00:00",
      "TaskMonitor": "/redfish/v1/TaskService/TaskMonitors/3",
      "TaskState": "Running",
      "TaskStatus": "OK"
    }Firmware update request sent for target /redfish/v1/UpdateService/FirmwareInventory/Anacapa_RBB_PCIESW_2956
    ```
    
    Change-Id: I811f96ad4180deb5bd7bf55b03863c20a68e8a1b
    Signed-off-by: Colin Huang <colin.huang2@amd.com>

---
+++ pcieswitch/spi_device.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "common/include/device.hpp"
+#include "common/include/host_power.hpp"
+#include "common/include/software.hpp"
+#include "common/include/software_manager.hpp"
+
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/async/context.hpp>
+
+#include <string>
+
+class SPIDevice;
+
+using namespace phosphor::software;
+using namespace phosphor::software::manager;
+using namespace phosphor::software::host_power;
+
+const std::string versionUnknown = "Unknown";
+
+const std::string rbbPCIeSwitchSysPath =
+    "/sys/bus/usb/devices/2-2.4.6/idProduct";
+const std::string lbbPCIeSwitchSysPath =
+    "/sys/bus/usb/devices/2-2.5.6/idProduct";
+const std::string amcPCIeSwitchSysPath = "/sys/bus/usb/devices/2-2.2/idProduct";
+const std::string ft4222DID = "601c";
+
+enum FT4222
+{
+    LBB = 0,
+    RBB,
+    AMC,
+    FT4222_END
+};
+
+class SPIDevice : public Device
+{
+  public:
+    using Device::softwareCurrent;
+    SPIDevice(sdbusplus::async::context& ctx, std::string& chipName,
+              const std::vector<std::string>& gpioLines,
+              const std::vector<std::string>& gpioValues,
+              SoftwareConfig& config, SoftwareManager* parent) :
+        Device(ctx, config, parent,
+               {RequestedApplyTimes::Immediate, RequestedApplyTimes::OnReset}),
+        gpioLines(gpioLines), gpioValues(gpioValues), chipName(chipName)
+    {}
+
+    sdbusplus::async::task<bool> updateDevice(const uint8_t* image,
+                                              size_t imageSize) final;
+    static sdbusplus::async::task<bool> getVersion(std::string& version);
+    void getFt4222AdapterNum(const bool* devExist, std::string& adapterNum);
+
+  private:
+    std::vector<std::string> gpioLines;
+    std::vector<std::string> gpioValues;
+    std::string chipName;
+};

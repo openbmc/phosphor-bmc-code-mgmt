b1e8e3709e34fa02a408ce28c2e0efdd3263ccc3
Jishnu C M <jishnunambiarcm@duck.com>
2026-03-13 05:35:25 -0500

    Fix the MLDSA signature validation
    Fix the openssl APIs for MLDSA validation.
    Update the unit test with MLDSA signature validation.
    
    Tested:
        Backward compatibility of the existing code update
        Verified the new signatures are verified when present.
    
    Change-Id: I41927f108ab3d6a667ffec38c1932b25e60bc352
    Signed-off-by: Jishnu C M <jishnunambiarcm@duck.com>

--- bmc/test/utest.cpp
+++ bmc/test/utest.cpp
@@ -223,6 +223,7 @@ class SignatureTest : public testing::Test
             manifestFile);
         command("echo \"HashType=RSA-SHA256\" >> " + manifestFile);
         command("echo \"KeyType=OpenBMC\" >> " + manifestFile);
+        command("echo \"MLDSA_Hash_Type=SHA3-512,MLDSA\" >> " + manifestFile);
 
         std::string kernelFile = extractPath.string() + "/" + "image-kernel";
         command("echo \"image-kernel file \" > " + kernelFile);
@@ -257,6 +258,22 @@ class SignatureTest : public testing::Test
                 ubootFile);
         command(opensslCmd + pkeyFile + " -out " + pubkeyFile + ".sig " +
                 pubkeyFile);
+
+        std::string mldsaImageDir = extractPath.string() + "/MLDSA";
+        command("mkdir -p " + mldsaImageDir);
+
+        std::string mldsaCheckCmd =
+            "openssl genpkey -algorithm ML-DSA-87 -out /tmp/test_mldsa.pem 2>/dev/null";
+        int mldsaSupported = std::system(mldsaCheckCmd.c_str());
+        command("rm -f /tmp/test_mldsa.pem 2>/dev/null");
+        if (mldsaSupported == 0)
+        {
+            std::cout << "MLDSA supported" << std::endl;
+        }
+        else
+        {
+            std::cout << "MLDSA not supported" << std::endl;
+        }
 
 #ifdef WANT_SIGNATURE_VERIFY
         std::string fullFile = extractPath.string() + "/" + "image-full";

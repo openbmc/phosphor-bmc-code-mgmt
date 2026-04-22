66ae46601a6560fab3d8d18e78de95f5b6c294ee
Jae Hyun Yoo <jae.yoo@oss.qualcomm.com>
2026-04-21 22:11:49 -0700

    detect tar compression type via magic bytes to use correct tar options
    The existing unTar logic processes tar files through a pipe, which prevents
    automatic compression detection by tar. To address this, parse the magic bytes
    of the tar file to determine whether it is compressed and dynamically select
    -xzf for compressed tar files or -xf for plain tar files.
    
    With this change, the update service can accept both plain and compressed tar
    files, and each package will be processed appropriately.
    
    Change-Id: I1f49c6b029704bb58da406c8149b802d116a8882
    Signed-off-by: Jae Hyun Yoo <jae.yoo@oss.qualcomm.com>

--- bmc/software_utils.cpp
+++ bmc/software_utils.cpp
@@ -29,9 +29,47 @@ static bool writeToFile(int imageFd, FILE* outStream)
     return true;
 }
 
+bool isCompressedTar(int imageFd)
+{
+    unsigned char magic[6] = {0};
+    ssize_t bytesRead = read(imageFd, magic, sizeof(magic));
+    lseek(imageFd, 0, SEEK_SET);
+
+    if (bytesRead < 2)
+    {
+        return false;
+    }
+
+    // gzip
+    if (magic[0] == 0x1f && magic[1] == 0x8b)
+    {
+        return true;
+    }
+
+    // bzip2
+    if (bytesRead >= 3 && magic[0] == 0x42 && magic[1] == 0x5a &&
+        magic[2] == 0x68)
+    {
+        return true;
+    }
+
+    // xz
+    if (bytesRead >= 6 && magic[0] == 0xfd && magic[1] == 0x37 &&
+        magic[2] == 0x7a && magic[3] == 0x58 && magic[4] == 0x5a &&
+        magic[5] == 0x00)
+    {
+        return true;
+    }
+
+    return false;
+}
+
 bool unTar(int imageFd, const std::string& extractDirPath)
 {
-    std::string tarCmd = "tar -xf - -C " + extractDirPath + " --no-same-owner";
+    std::string tarOptions = isCompressedTar(imageFd) ? "-xzf" : "-xf";
+    std::string tarCmd =
+        "tar " + tarOptions + " - -C " + extractDirPath + " --no-same-owner";
+
     info("Executing command: {CMD}", "CMD", tarCmd);
     FILE* outStream = popen(tarCmd.c_str(), "w");
     if (outStream == nullptr)

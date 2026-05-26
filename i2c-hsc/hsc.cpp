416dc3348f8fb58834b91b4923d2ac4a4224b989
Jackson Liu <Jackson.Liu@quantatw.com>
2026-05-26 15:14:39 +0800

    Add i2c hot swap controller updater
    /i2c-hsc/hsc.cpp, hsc.hpp: General representation of a HSC communication interface
    /vr-hsc/tps25990: Support for Texas Instruments TPS25990 class of HSC
    
    The configuration of a HSC relies on the EM-Schema in [1]
    Support for the TPS25990 type in phosphor-dbus-interfaces was added
    in [2]
    
    [1] https://gerrit.openbmc.org/c/openbmc/entity-manager/+/90279
    [2] https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/90499
    
    Change-Id: Icd7b1905de0892de3706717aa61035e403d85033
    Signed-off-by: Jackson Liu <Jackson.Liu@quantatw.com>

---
+++ i2c-hsc/hsc.cpp
@@ -0,0 +1,41 @@
+#include "hsc.hpp"
+
+#include "tps25990/tps25990.hpp"
+#include "tps25990/rs31390.hpp"
+#include "tps25990/tps25990.hpp"
+
+#include <map>
+
+namespace phosphor::software::HSC
+{
+
+std::unique_ptr<HotSwapController> create(sdbusplus::async::context& ctx,
+                                         enum HSCType hscType, uint16_t bus,
+                                         uint16_t address)
+{
+    switch (hscType)
+    {
+        case HSCType::TPS25990:
+            return std::make_unique<TPS25990>(ctx, bus, address);
+        case HSCType::RS31390:
+            return std::make_unique<RS31390>(ctx, bus, address);
+        default:
+            return nullptr;
+    }
+}
+
+bool stringToEnum(std::string& hscStr, HSCType& hscType)
+{
+    std::map<std::string, enum HSCType> HSCTypeToString{
+        {"TPS25990Firmware", HSCType::TPS25990},
+        {"RS31390Firmware", HSCType::RS31390}};
+
+    if (HSCTypeToString.contains(hscStr))
+    {
+        hscType = HSCTypeToString[hscStr];
+        return true;
+    }
+    return false;
+}
+
+} // namespace phosphor::software::HSC

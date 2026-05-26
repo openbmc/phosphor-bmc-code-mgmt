15a6f2300d308c43fb72555709ac408a94daa83c
Jackson Liu <Jackson.Liu@quantatw.com>
2026-05-26 15:14:39 +0800

    Add i2c hot swap controller updater
    /i2c-hsc/hsc.cpp, hsc.hpp: General representation of a HSC communication
    interface
    /vr-hsc/tps25990: Support for Texas Instruments TPS25990 class of HSC
    
    The configuration of a HSC relies on the EM-Schema in [1]
    Support for the TPS25990 type in phosphor-dbus-interfaces was added
    in [2]
    The package config for HSC update in [3]
    
    [1] https://gerrit.openbmc.org/c/openbmc/entity-manager/+/90279
    [2] https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/
    +/90499
    [3] https://gerrit.openbmc.org/c/openbmc/openbmc/+/90500
    
    Change-Id: Icd7b1905de0892de3706717aa61035e403d85033
    Signed-off-by: Jackson Liu <Jackson.Liu@quantatw.com>

---
+++ i2c-hsc/tps25990/rs31390.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "common/include/pmbus.hpp"
+#include "tps25990.hpp"
+
+namespace phosphor::software::HSC
+{
+
+class RS31390 : public TPS25990
+{
+  public:
+    RS31390(sdbusplus::async::context& ctx, uint16_t bus, uint16_t address) :
+        TPS25990(ctx, bus, address)
+    {}
+
+    sdbusplus::async::task<bool> getCheckSum(uint32_t* sum) final;
+    sdbusplus::async::task<bool> parseImage(const uint8_t* image,
+                                            size_t imageSize) final;
+};
+
+} // namespace phosphor::software::HSC

dea3335de86bb795ed83b9c7b17bc0dbca27754d
Jackson Liu <Jackson.Liu@quantatw.com>
2026-05-26 15:14:39 +0800

    Add i2c hot swap controller updater
    /i2c-hsc/hsc.cpp, hsc.hpp: General representation of a HSC communication
    interface
    /vr-hsc/tps25990: Support for Texas Instruments TPS25990 class of HSC
    
    The configuration of a HSC relies on the EM-Schema in [1]
    Support for the TPS25990 type in phosphor-dbus-interfaces was added
    in [2]
    The package config for HSC update in [3]
    
    [1] https://gerrit.openbmc.org/c/openbmc/entity-manager/+/90279
    [2] https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/
    +/90499
    [3] https://gerrit.openbmc.org/c/openbmc/openbmc/+/90500
    
    Change-Id: Icd7b1905de0892de3706717aa61035e403d85033
    Signed-off-by: Jackson Liu <Jackson.Liu@quantatw.com>

---
+++ i2c-hsc/hsc.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <sdbusplus/async.hpp>
+
+#include <cstdint>
+#include <memory>
+#include <string>
+
+namespace phosphor::software::HSC
+{
+
+enum class HSCType
+{
+    TPS25990,
+    RS31390
+};
+
+class HotSwapController
+{
+  public:
+    explicit HotSwapController(sdbusplus::async::context& ctx) : ctx(ctx) {}
+    virtual ~HotSwapController() = default;
+
+    HotSwapController(HotSwapController& hsc) = delete;
+    HotSwapController& operator=(HotSwapController other) = delete;
+    HotSwapController(HotSwapController&& other) = delete;
+    HotSwapController& operator=(HotSwapController&& other) = delete;
+
+    // @brief Parses the firmware image into the configuration structure
+    //        and verifies its correctness.
+    // @return sdbusplus::async::task<bool> true indicates success.
+    virtual sdbusplus::async::task<bool> verifyImage(const uint8_t* image,
+                                                     size_t imageSize) = 0;
+
+    // @brief Applies update to the hot swap controller
+    // @return sdbusplus::async::task<bool> true indicates success.
+    virtual sdbusplus::async::task<bool> updateFirmware(bool force) = 0;
+
+    virtual sdbusplus::async::task<bool> getCRC(uint32_t* checksum) = 0;
+
+  protected:
+    sdbusplus::async::context& ctx;
+};
+
+std::unique_ptr<HotSwapController> create(sdbusplus::async::context& ctx,
+                                           enum HSCType hscType, uint16_t bus,
+                                           uint16_t address);
+
+bool stringToEnum(std::string& hscStr, HSCType& hscType);
+
+} // namespace phosphor::software::HSC

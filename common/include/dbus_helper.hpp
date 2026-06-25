58abd868a48d87f706fe3c16ddb3c08d3d4db8fa
Alexander Hansen <alexander.hansen@9elements.com>
2026-06-25 15:24:12 +0200

    deduplicate Mux Configuration parsing
    GPIO Mux configuration is common across all those updaters as they all
    use the same shared schema from EM side.
    
    Write a shared helper function to parse the mux config.
    
    Preserve the updater specific debug logging behavior w.r.t. the Mux
    GPIOs.
    
    Note: i would have preferred to use a structured binding directly
    instead of destructuring in a separate statement but that is apparently
    not supported in my GCC version [1].
    
    Also put the D-Bus helpers as explicit instantiations to prevent
    stdexec from giving us ODR violations because GCC cannot match symbols
    from anonymous namespaces across translation unit boundaries.
    Not to be confused, below is the yocto GCC (different version).
    
    ```
    | /usr/src/debug/phosphor-software-manager/1.0+git/common/include/dbus_helper.hpp:32:1: error: type 'struct _Z23dbusGetOptionalPropertyINSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEEEN4exec6__task10basic_taskISt8optionalIT_ENS7_27__default_task_context_implILNS7_20__scheduler_affinityE1EEEEERN9sdbusplus5async7contextES5_NSG_7message7details19string_path_wrapperES5_S5_.Frame' violates the C++ One Definition Rule [-Werror=odr]
    | /usr/src/debug/phosphor-software-manager/1.0+git/common/include/dbus_helper.hpp:32:1: note: a different type is defined in another translation unit
    | lto1: note: the first difference of corresponding definitions is field 'Aw0_3_4'
    | lto1: note: a field of same name but different type is defined in another translation unit
    | /usr/include/stdexec/__detail/__as_awaitable.hpp:159:14: note: type 'struct __t' defined in anonymous namespace cannot match across the translation unit boundary
    ```
    
    Tested: on anacapa qemu
    
    logs after patching are same as before (modulo swid) with the difference
    of the warning logs from the property access attempt also being printed.
    
    ```
    Mar 13 08:36:12 anacapa phosphor-bios-software-update[791]: method: org.freedesktop.DBus.Error.UnknownProperty: Unknown interface xyz.openbmc_project.Configuration.SPIFlash.MuxOutputs1 or property Name.
    ```
    
    Both before and after this patch this bug remains (error logs on valid
    config). This patch only solves the code duplication.
    
    ```
    root@anacapa:~# busctl tree xyz.openbmc_project.Software.BIOS
    └─ /xyz
      └─ /xyz/openbmc_project
        └─ /xyz/openbmc_project/software
          └─ /xyz/openbmc_project/software/Anacapa_BIOS_1145
    root@anacapa:~# journalctl -u  xyz.openbmc_project.Software.BIOS
    Mar 13 08:35:45 anacapa systemd[1]: Starting Host FW Code Update Daemon...
    Mar 13 08:35:48 anacapa systemd[1]: Started Host FW Code Update Daemon.
    Mar 13 08:36:12 anacapa phosphor-bios-software-update[791]: method: org.freedesktop.DBus.Error.UnknownProperty: Unknown interface xyz.openbmc_project.Configuration.SPIFlash.MuxOutputs1 or property Name.
    Mar 13 08:36:12 anacapa phosphor-bios-software-update[791]: Missing property Name on path /xyz/openbmc_project/inventory/system/board/Anacapa_SCM/Anacapa_BIOS, interface xyz.openbmc_project.Configuration.SPIFlash.MuxOutputs1
    Mar 13 08:36:12 anacapa phosphor-bios-software-update[791]: method: org.freedesktop.DBus.Error.UnknownProperty: Unknown interface xyz.openbmc_project.Configuration.SPIFlash.MuxOutputs1 or property Polarity.
    Mar 13 08:36:12 anacapa phosphor-bios-software-update[791]: Missing property Polarity on path /xyz/openbmc_project/inventory/system/board/Anacapa_SCM/Anacapa_BIOS, interface xyz.openbmc_project.Configuration.SPIFlash.MuxOutputs1
    ```
    
    References:
    [1] gcc (GCC) 16.1.1 20260430
    
    Change-Id: Ib4ecf67327712a94b071ea16664a1e8d85d33f87
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- common/include/dbus_helper.hpp
+++ common/include/dbus_helper.hpp
@@ -3,33 +3,18 @@
 #include <phosphor-logging/lg2.hpp>
 #include <sdbusplus/async.hpp>
 #include <sdbusplus/async/context.hpp>
+#include <sdbusplus/message/native_types.hpp>
 
 #include <optional>
 
 PHOSPHOR_LOG2_USING;
 
 template <typename T>
+sdbusplus::async::task<std::optional<T>> dbusGetOptionalProperty(
+    sdbusplus::async::context& ctx, std::string service,
+    sdbusplus::object_path path, std::string intf, std::string property);
+
+template <typename T>
 sdbusplus::async::task<std::optional<T>> dbusGetRequiredProperty(
-    sdbusplus::async::context& ctx, const std::string& service,
-    const std::string& path, const std::string& intf,
-    const std::string& property)
-{
-    auto client =
-        sdbusplus::async::proxy().service(service).path(path).interface(
-            "org.freedesktop.DBus.Properties");
-
-    std::optional<T> opt = std::nullopt;
-    try
-    {
-        std::variant<T> result =
-            co_await client.call<std::variant<T>>(ctx, "Get", intf, property);
-
-        opt = std::get<T>(result);
-    }
-    catch (std::exception& e)
-    {
-        error("Missing property {PROPERTY} on path {PATH}, interface {INTF}",
-              "PROPERTY", property, "PATH", path, "INTF", intf);
-    }
-    co_return opt;
-}
+    sdbusplus::async::context& ctx, std::string service,
+    sdbusplus::object_path path, std::string intf, std::string property);

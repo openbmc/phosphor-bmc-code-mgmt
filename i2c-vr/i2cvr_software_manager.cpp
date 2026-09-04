1fe0be608ba28a5fa89df866304b613dafc87086
Angela Cheng wiwynn <angelaptcheng@gmail.com>
2026-09-04 05:58:34 +0000

    i2c-vr: add MPQ82D00 firmware update support
    Add support for programming MPS MPQ82D00 VR devices over the I2C bus.
    
    The entity-manager configuration and dbus-sensors support for this
    device were added in [1] and [2].
    
    [1] https://gerrit.openbmc.org/c/openbmc/entity-manager/+/93815
    [2] https://gerrit.openbmc.org/c/openbmc/dbus-sensors/+/93818
    
    Change-Id: I100362b3c2fa4d42283f806537fe32ae1d5fa1c8
    Signed-off-by: Angela Cheng wiwynn <angelaptcheng@gmail.com>

--- i2c-vr/i2cvr_software_manager.cpp
+++ i2c-vr/i2cvr_software_manager.cpp
@@ -26,7 +26,8 @@ const std::vector<std::string> emConfigTypes = {
     "MP2X6XXFirmware",    "MP292XFirmware",       "MP297XFirmware",
     "MP5998Firmware",     "MPQ87XXFirmware",      "MP994XFirmware",
     "RAA22XGen2Firmware", "RAA22XGen3p5Firmware", "TDA38640AFirmware",
-    "XDP71XFirmware",     "TPS25990Firmware",     "RS31390Firmware"};
+    "XDP71XFirmware",     "TPS25990Firmware",     "RS31390Firmware",
+    "MPQ82D00Firmware"};
 
 I2CVRSoftwareManager::I2CVRSoftwareManager(sdbusplus::async::context& ctx) :
     ManagerInf::SoftwareManager(ctx, configDBusName)
